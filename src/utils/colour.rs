49a684134df32427e9502192122c4fb22ef1a735
Isaac Dickinson <sun@veriny.tf>
2016-12-01 21:40:25 +0000

    Add 'fooyoo' to Colour struct

--- src/utils/colour.rs
+++ src/utils/colour.rs
@@ -264,7 +264,9 @@ colour! {
     /// Creates a new `Colour`, setting its RGB value to `(84, 110, 122)`.
     darker_grey, 0x546E7A;
     /// Creates a new `Colour`, setting its RGB value to `(250, 177, 237)`.
-    fabled_pink, 0xFAB1ED
+    fabled_pink, 0xFAB1ED;
+    /// Creates a new `Colour`, setting its RGB value to `(17, 202, 128)`.
+    fooyoo, 0x11CA80;
     /// Creates a new `Colour`, setting its RGB value to `(241, 196, 15)`.
     gold, 0xF1C40F;
     /// Creates a new `Colour`, setting its RGB value to `(186, 218, 85)`.
