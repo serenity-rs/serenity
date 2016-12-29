c36841dd1c3f80141251ba01130333f43ff363d7
Austin Hellyer <hello@austinhellyer.me>
2016-12-28 21:06:16 -0800

    Add user static avatar url methods

--- tests/test_user.rs
+++ tests/test_user.rs
@@ -17,9 +17,11 @@ fn test_core() {
     let mut user = gen();
 
     assert!(user.avatar_url().unwrap().ends_with("/avatars/210/abc.webp?size=1024"));
+    assert!(user.static_avatar_url().unwrap().ends_with("/avatars/210/abc.webp?size=1024"));
 
     user.avatar = Some("a_aaa".to_owned());
     assert!(user.avatar_url().unwrap().ends_with("/avatars/210/a_aaa.gif?size=1024"));
+    assert!(user.static_avatar_url().unwrap().ends_with("/avatars/210/a_aaa.webp?size=1024"));
 
     user.avatar = None;
     assert!(user.avatar_url().is_none());
