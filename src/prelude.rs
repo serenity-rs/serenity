db0f025d154e4b6212dd9340c1b789b3c711a24a
Zeyla Hellyer <zey@zey.moe>
2017-06-14 21:38:08 -0700

    Re-export all errors from the prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -19,3 +19,11 @@ pub use ::model::Mentionable;
 
 #[cfg(feature="client")]
 pub use ::client::{Client, ClientError as ClientError};
+#[cfg(feature="gateway")]
+pub use ::gateway::GatewayError;
+#[cfg(feature="http")]
+pub use ::http::HttpError;
+#[cfg(feature="model")]
+pub use ::model::ModelError;
+#[cfg(feature="voice")]
+pub use ::voice::VoiceError;
