824f8cb63271ac3907a9c8223b08b7ee6ff0d746
acdenisSK <acdenissk69@gmail.com>
2017-07-23 20:11:36 +0200

    Ignore protocol errors
    Without ignoring, sometimes serenity would start auto-reconnecting and spamming the console quite a lot, but not sure why do these errors happen. Still, ignoring them doesn't seem to do any harm.

--- src/gateway/shard.rs
+++ src/gateway/shard.rs
@@ -495,6 +495,7 @@ impl Shard {
                     self.reconnect().and(Ok(None))
                 }
             },
+			Err(Error::WebSocket(WebSocketError::ProtocolError(..))) => Ok(None),
             Err(Error::WebSocket(why)) => {
                 if let WebSocketError::NoDataAvailable = why {
                     if self.heartbeat_instants.1.is_none() {
