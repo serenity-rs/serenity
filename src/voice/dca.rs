628301ed347e2a037bfff72d239c0c3ce0af366e
Maid Dog <maiddogsrl@gmail.com>
2017-08-26 17:16:49 -0500

    Remove unused fields and add Debug trait

--- src/voice/dca.rs
+++ src/voice/dca.rs
@@ -1,20 +1,12 @@
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct DcaMetadata {
     opus: OpusInfo,
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct OpusInfo {
-    /// Bitrate per second
-    abr: u32,
     /// Number of channels
     channels: u8,
-    /// Frame size in bytes
-    frame_size: u32,
-    /// Sample rate in Hz
-    sample_rate: u32,
-    /// Whether or not variable bitrate encoding is used
-    vbr: bool,
 }
 
 impl DcaMetadata {
