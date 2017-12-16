c9efd7c147abfd1f92758719141fbb67d9792271
Zeyla Hellyer <zey@zey.moe>
2017-12-15 21:33:22 -0800

    Fix compilation of example 07

--- examples/07_sample_bot_structure/src/commands/meta.rs
+++ examples/07_sample_bot_structure/src/commands/meta.rs
@@ -1,13 +1,3 @@
-command!(latency(ctx, msg) {
-    let latency = ctx.shard.lock()
-        .latency()
-        .map_or_else(|| "N/A".to_string(), |s| {
-            format!("{}.{}s", s.as_secs(), s.subsec_nanos())
-        });
-
-    let _ = msg.channel_id.say(latency);
-});
-
 command!(ping(_ctx, msg) {
     let _ = msg.channel_id.say("Pong!");
 });
--- examples/07_sample_bot_structure/src/commands/owner.rs
+++ examples/07_sample_bot_structure/src/commands/owner.rs
@@ -1,10 +1,5 @@
 command!(quit(ctx, msg, _args) {
-    match ctx.quit() {
-        Ok(()) => {
-            let _ = msg.reply("Shutting down!");
-        },
-        Err(why) => {
-            let _ = msg.reply(&format!("Failed to shutdown: {:?}", why));
-        },
-    }
+    ctx.quit();
+
+    let _ = msg.reply("Shutting down!");
 });
