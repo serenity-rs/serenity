39efe480f753378ab07dade8c0a54bf4d0b3d70a
Kyle Simpson <kyleandrew.simpson@gmail.com>
2018-02-02 12:22:40 +0000

    First stab at docs for Audio.

--- src/voice/audio.rs
+++ src/voice/audio.rs
@@ -39,12 +39,48 @@ pub enum AudioType {
 /// Control object for audio playback.
 ///
 /// Accessed by both commands and the playback code -- as such, access is
-/// always guarded.
+/// always guarded. In particular, you should expect to receive
+/// a [`LockedAudio`] when calling [`Handler::play()`].
+///
+/// # Example
+/// ```rust, no-run
+/// use serenity::voice::{Handler, LockedAudio};
+///
+/// let handler: Handler = /* ... */;
+/// let safe_audio: LockedAudio = handler.play();
+/// {
+///     let audio_lock = safe_audio_control.clone();
+///     let mut audio = audio_lock.lock();
+///
+///     audio.volume(0.5);
+/// }
+/// ```
+/// [`LockedAudio`]: type.LockedAudio.html
+/// [`Handler::play()`]: struct.Handler.html#method.play
 pub struct Audio {
+    /// Whether or not this sound is currently playing.
+    /// Can be controlled with [`.play()`] or [`.pause()`]
+    /// if chaining is desired.
+    ///
+    /// [`.play()`]: #method.play
+    /// [`.pause()`]: #method.pause
     pub playing: bool,
+    /// The desired volume for playback.
+    /// Sensible values fall between `0.0` and `1.0`.
+    /// Can be controlled with [`.volume()`]
+    /// if chaining is desired.
+    ///
+    /// [`.volume()`]: #method.volume
     pub volume: f32,
+    /// Whether or not the sound has finished,
+    /// or reached the end of its stream.
+    /// ***Read-only*** for now.
     pub finished: bool,
+    /// Underlying data access object.
+    /// *Calling code is not expected to use this.*
     pub source: Box<AudioSource>,
+    /// The current position for playback.
+    /// Consider the position fields **read-only** for now.
     pub position: Duration,
     pub position_modified: bool,
 }
@@ -61,24 +97,35 @@ impl Audio {
         }
     }
 
+    /// Sets [`playing`] to `true` in a manner that allows method chaining.
+    ///
+    /// [`playing`]: #structfield.playing
     pub fn play(&mut self) -> &mut Self {
         self.playing = true;
 
         self
     }
 
+    /// Sets [`playing`] to `false` in a manner that allows method chaining.
+    ///
+    /// [`playing`]: #structfield.playing
     pub fn pause(&mut self) -> &mut Self {
         self.playing = false;
 
         self
     }
 
+    /// Sets [`volume`] in a manner that allows method chaining.
+    ///
+    /// [`volume`]: #structfield.volume
     pub fn volume(&mut self, volume: f32) -> &mut Self {
         self.volume = volume;
 
         self
     }
 
+    /// Change the in the stream for subsequent playback.
+    /// Currently a No-op.
     pub fn position(&mut self, position: Duration) -> &mut Self {
         self.position = position;
         self.position_modified = true;
@@ -86,6 +133,8 @@ impl Audio {
         self
     }
 
+    /// Steps playback location forward by one frame.
+    /// *Used internally.*
     pub fn step_frame(&mut self) {
         self.position += Duration::from_millis(20);
         self.position_modified = false;
