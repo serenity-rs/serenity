42faa540f8edcc33945f77efc1534dc660e99c37
Joe Burnard <spongybacon@hotmail.co.uk>
2018-04-05 15:31:03 +0100

    working shard manager test

--- examples/dev_shard_manager/src/main.rs
+++ examples/dev_shard_manager/src/main.rs
@@ -1,17 +1,15 @@
-#![feature(proc_macro, conservative_impl_trait, generators)]
-
-extern crate futures_await as futures;
+extern crate futures;
 extern crate serenity;
 extern crate tokio_core;
 
-use futures::prelude::*;
-use serenity::gateway::Shard;
+use serenity::gateway::{Shard, ShardingStrategy, ShardManager, ShardManagerOptions};
 use serenity::model::event::{Event, GatewayEvent};
-use serenity::model::gateway::Game;
 use serenity::model::user::OnlineStatus;
 use std::error::Error;
 use std::env;
+use std::rc::Rc;
 use tokio_core::reactor::{Core, Handle};
+use futures::{future, Future, Stream};
 
 fn main() {
     let mut core = Core::new().expect("Error creating event loop");
@@ -20,27 +18,30 @@ fn main() {
     core.run(future).expect("Error running event loop");
 }
 
-#[async]
-fn try_main(handle: Handle) -> Result<(), Box<Error + 'static>> {
-    // Configure the client with your Discord bot token in the environment.
+fn try_main(handle: Handle) -> impl Future<Item = (), Error = ()> {
     let token = env::var("DISCORD_TOKEN")
         .expect("Expected a token in the environment");
 
-    // Create a new shard, specifying the token, the ID of the shard (0 of 1),
-    // and a handle to the event loop
-    let mut shard = await!(Shard::new(token, [0, 1], handle))?;
+    let opts = ShardManagerOptions {
+        strategy: ShardingStrategy::simple(),
+        token: Rc::new(token),
+        ws_uri: Rc::new(String::from("nothing")),
+    }; 
 
-    // Loop over websocket messages.
-    #[async]
-    for message in shard.messages() {
-        // Parse the websocket message into a serenity GatewayEvent.
-        let event = shard.parse(message)?;
+    let mut shard_manager = ShardManager::new(opts, handle.clone());
+    let future = shard_manager.start()
+        .map_err(|e| println!("oh no! {:?}", e));
 
-        // Have the shard process the WebSocket event, in case it needs to
-        // mutate its state, send a packet, etc.
+    handle.spawn(future);
+
+    let future = shard_manager.messages().for_each(|(shard, message)| {
+        let mut shard = shard.borrow_mut();
+        
+        let event = shard.parse(message)
+            .expect("Could not parse shard stream message");
+
         shard.process(&event);
 
-        // Now you can do whatever you want with the event.
         match event {
             GatewayEvent::Dispatch(_, Event::MessageCreate(ev)) => {
                 if ev.message.content == "!ping" {
@@ -54,7 +55,9 @@ fn try_main(handle: Handle) -> Result<(), Box<Error + 'static>> {
                 // Ignore all other messages.
             },
         }
-    }
 
-    Ok(())
+        future::ok(())
+    });
+
+    future
 }
