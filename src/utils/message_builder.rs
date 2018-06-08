bfd852b7707132bde5263b8b898123c35c2b6207
PvdBerg <PvdBerg1998@users.noreply.github.com>
2018-06-08 22:06:16 +0200

    Fix push_codeblock_safe not adding a newline

--- src/utils/message_builder.rs
+++ src/utils/message_builder.rs
@@ -531,7 +531,6 @@ impl MessageBuilder {
         }
 
         self.0.push('\n');
-
         {
             let mut c = content.into();
             c.inner = normalize(&c.inner).replace("```", " ");
