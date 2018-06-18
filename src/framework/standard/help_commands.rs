aeb89af4eff59bb3ea9eb7623685bf7ad7520496
Lakelezz <12222135+Lakelezz@users.noreply.github.com>
2018-06-18 20:58:40 +0200

    If no help is available, command is not visible thus return false.

--- src/framework/standard/help_commands.rs
+++ src/framework/standard/help_commands.rs
@@ -116,8 +116,6 @@ pub fn is_command_visible(command_options: &Arc<CommandOptions>, msg: &Message, 
                     } else {
                         return help_options.lacking_permissions != HelpBehaviour::Hide;
                     }
-                } else {
-                    return help_options.lacking_permissions != HelpBehaviour::Hide;
                 }
             }
         }
@@ -125,7 +123,7 @@ pub fn is_command_visible(command_options: &Arc<CommandOptions>, msg: &Message, 
         return help_options.wrong_channel != HelpBehaviour::Hide;
     }
 
-    return false;
+    return false
 }
 
 /// Posts an embed showing each individual command group and its commands.
