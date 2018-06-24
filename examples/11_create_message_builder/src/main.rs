948918c5b8441af2af22def21ccef6b34c70af79
drklee3 <dlee3@scu.edu>
2018-06-24 11:48:44 -0700

    Add CreateEmbed::Field/s to create_message_builder example

--- examples/11_create_message_builder/src/main.rs
+++ examples/11_create_message_builder/src/main.rs
@@ -19,6 +19,11 @@ impl EventHandler for Handler {
                 .embed(|e| e
                     .title("This is a title")
                     .description("This is a description")
+                    .fields(vec![
+                        ("This is the first field", "This is a field body", true),
+                        ("This is the second field", "Both of these fields are inline", true),
+                    ])
+                    .field("This is the third field", "This is not an inline field", false)
                     .footer(|f| f
                         .text("This is a footer")))) {
                 println!("Error sending message: {:?}", why);
