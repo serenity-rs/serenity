4697605dd24bdb73d0eb832f9791d93166a2ff62
Zeyla Hellyer <zeylahellyer@gmail.com>
2018-07-08 08:44:14 -0700

    Add reconnection logic to example 01

--- examples/01_basic_ping_bot/src/main.rs
+++ examples/01_basic_ping_bot/src/main.rs
@@ -1,4 +1,4 @@
-#![feature(proc_macro, generators)]
+#![feature(catch_expr, proc_macro, generators)]
 
 extern crate env_logger;
 extern crate futures_await as futures;
@@ -25,35 +25,46 @@ fn try_main() -> Result<(), Box<Error + 'static>> {
     let token = Rc::new(env::var("DISCORD_TOKEN")
         .expect("Expected a token in the environment"));
 
-    // Create a new shard, specifying the token, the ID of the shard (0 of 1),
-    // and a handle to the event loop
-    let mut shard = await!(Shard::new(token, [0, 1]))?;
+    loop {
+        // Create a new shard, specifying the token, the ID of the shard (0 of 1),
+        // and a handle to the event loop
+        let mut shard = await!(Shard::new(Rc::clone(&token), [0, 1]))?;
 
-    // Loop over websocket messages.
-    #[async]
-    for message in shard.messages() {
-        // Parse the websocket message into a serenity GatewayEvent.
-        let event = shard.parse(message)?;
+        // Loop over websocket messages.
+        let result: Result<_, Box<Error>> = do catch {
+            #[async]
+            for message in shard.messages() {
+                // Parse the websocket message into a serenity GatewayEvent.
+                let event = shard.parse(message)?;
 
-        // Have the shard process the WebSocket event, in case it needs to
-        // mutate its state, send a packet, etc.
-        shard.process(&event);
+                // Have the shard process the WebSocket event, in case it needs to
+                // mutate its state, send a packet, etc.
+                shard.process(&event);
 
-        // Now you can do whatever you want with the event.
-        match event {
-            GatewayEvent::Dispatch(_, Event::MessageCreate(ev)) => {
-                if ev.message.content == "!ping" {
-                    println!("Pong!");
+                // Now you can do whatever you want with the event.
+                match event {
+                    GatewayEvent::Dispatch(_, Event::MessageCreate(ev)) => {
+                        if ev.message.content == "!ping" {
+                            println!("Pong!");
+                        }
+                    },
+                    GatewayEvent::Dispatch(_, Event::Ready(_)) => {
+                        println!("Connected to Discord!");
+                    },
+                    _ => {
+                        // Ignore all other messages.
+                    },
                 }
-            },
-            GatewayEvent::Dispatch(_, Event::Ready(_)) => {
-                println!("Connected to Discord!");
-            },
-            _ => {
-                // Ignore all other messages.
-            },
+            }
+
+            ()
+        };
+
+        if let Err(why) = result {
+            println!("Error with loop occurred: {:?}", why);
+            println!("Creating new shard");
+
+            continue;
         }
     }
-
-    Ok(())
 }
