c69e0981618b59816510aa90d27d3be49b582b65
Zeyla Hellyer <zeylahellyer@gmail.com>
2018-07-08 17:23:21 -0700

    Make example 01 autoreconnect on closes

--- examples/01_basic_ping_bot/src/main.rs
+++ examples/01_basic_ping_bot/src/main.rs
@@ -2,36 +2,65 @@
 
 extern crate env_logger;
 extern crate futures_await as futures;
+extern crate serde_json;
 extern crate serenity;
 extern crate tokio;
+extern crate tungstenite;
 
 use futures::prelude::{async, await};
+use serde_json::Error as JsonError;
 use serenity::gateway::Shard;
 use serenity::model::event::{Event, GatewayEvent};
-use std::error::Error;
+use serenity::Error as SerenityError;
 use std::env;
 use std::rc::Rc;
 use tokio::executor::current_thread;
+use tungstenite::Error as TungsteniteError;
+
+#[derive(Debug)]
+enum Error {
+    Json(JsonError),
+    Serenity(SerenityError),
+    Tungstenite(TungsteniteError),
+}
+
+impl From<JsonError> for Error {
+    fn from(err: JsonError) -> Self {
+        Error::Json(err)
+    }
+}
+
+impl From<SerenityError> for Error {
+    fn from(err: SerenityError) -> Self {
+        Error::Serenity(err)
+    }
+}
+
+impl From<TungsteniteError> for Error {
+    fn from(err: TungsteniteError) -> Self {
+        Error::Tungstenite(err)
+    }
+}
 
 fn main() {
     current_thread::block_on_all(try_main()).expect("Error running event loop");
 }
 
 #[async]
-fn try_main() -> Result<(), Box<Error + 'static>> {
+fn try_main() -> Result<(), Error> {
     env_logger::init();
 
     // Configure the client with your Discord bot token in the environment.
     let token = Rc::new(env::var("DISCORD_TOKEN")
         .expect("Expected a token in the environment"));
 
-    loop {
-        // Create a new shard, specifying the token, the ID of the shard (0 of 1),
-        // and a handle to the event loop
-        let mut shard = await!(Shard::new(Rc::clone(&token), [0, 1]))?;
+    // Create a new shard, specifying the token, the ID of the shard (0 of 1),
+    // and a handle to the event loop
+    let mut shard = await!(Shard::new(Rc::clone(&token), [0, 1]))?;
 
+    loop {
         // Loop over websocket messages.
-        let result: Result<_, Box<Error>> = do catch {
+        let result: Result<_, Error> = do catch {
             #[async]
             for message in shard.messages() {
                 // Parse the websocket message into a serenity GatewayEvent.
@@ -62,9 +91,23 @@ fn try_main() -> Result<(), Box<Error + 'static>> {
 
         if let Err(why) = result {
             println!("Error with loop occurred: {:?}", why);
-            println!("Creating new shard");
 
-            continue;
+            match why {
+                Error::Tungstenite(TungsteniteError::ConnectionClosed(Some(close))) => {
+                    println!(
+                        "Close: code: {}; reason: {}",
+                        close.code,
+                        close.reason,
+                    );
+                },
+                other => {
+                    println!("Shard error: {:?}", other);
+
+                    continue;
+                },
+            }
+
+            shard.autoreconnect()?;
         }
     }
 }
