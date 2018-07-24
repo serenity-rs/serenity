cd3c3ececb24a7da4a4c99543cac5127f092a10a
Mishio595 <tenseiken595@gmail.com>
2018-07-24 17:19:22 -0600

    EventHandler::message_update with cache feature sends old message if available

--- src/client/dispatch.rs
+++ src/client/dispatch.rs
@@ -510,13 +510,25 @@ fn handle_event<H: EventHandler + Send + Sync + 'static>(
             });
         },
         DispatchEvent::Model(Event::MessageUpdate(mut event)) => {
-            update!(event);
+            let _before = update!(event);
+
+            let _after: Option<Message> = feature_cache! {{
+                CACHE.read().message(event.channel_id, event.id)
+            } else {
+                None
+            }};
 
             let context = context(data, runner_tx, shard_id);
             let event_handler = Arc::clone(event_handler);
 
             threadpool.execute(move || {
-                event_handler.message_update(context, event);
+                feature_cache! {{
+                    if let Some(after) = _after {
+                        event_handler.message_update(context, _before, after);
+                    }
+                } else {
+                    event_handler.message_update(context, event);
+                }}
             });
         },
         DispatchEvent::Model(Event::PresencesReplace(mut event)) => {
