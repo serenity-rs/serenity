6c94e05f6a8de131d0ffc913fd6ccbad602b339e
Mishio595 <tenseiken595@gmail.com>
2018-07-24 16:33:55 -0600

    add method Colour::hex

--- src/utils/colour.rs
+++ src/utils/colour.rs
@@ -190,6 +190,20 @@ impl Colour {
     /// [`g`]: #method.g
     /// [`b`]: #method.b
     pub fn tuple(&self) -> (u8, u8, u8) { (self.r(), self.g(), self.b()) }
+
+    /// Returns a hexadecimal string of this Colour.
+    ///
+    /// This is equivalent to passing the integer value through `std::fmt::UpperHex` with 0 padding
+    /// and 6 width
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use serenity::utils::Colour;
+    ///
+    /// assert_eq!(Colour::new(6573123).hex(), String::from("644C43"));
+    /// ```
+    pub fn hex(&self) -> String { format!("{:06X}", self.0) }
 }
 
 impl From<i32> for Colour {
