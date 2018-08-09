0319a5516ff062891f6f50840e8878766c13350c
Valdemar Erk <v@erk.io>
2018-08-09 20:32:05 +0200

    Removed mention of "bot user"

--- src/gateway/mod.rs
+++ src/gateway/mod.rs
@@ -3,9 +3,9 @@
 //!
 //! A shard is an interface for the lower-level receiver and sender. It provides
 //! what can otherwise be thought of as "sugar methods". A shard represents a
-//! single connection to Discord. If acting as a [bot user], you can make
-//! use of a method named "sharding" to have multiple shards, potentially
-//! offloading some server load to another server(s).
+//! single connection to Discord. You can make use of a method named "sharding"
+//! to have multiple shards, potentially offloading some server load to another
+//! server(s).
 //!
 //! # Sharding
 //!
@@ -38,7 +38,6 @@
 //! instance. This should be used when you, for example, want to split 10 shards
 //! across 3 instances.
 //!
-//! [bot user]: https://discordapp.com/developers/docs/topics/oauth2#bots
 //! [`Client`]: ../client/struct.Client.html
 //! [`Client::start`]: ../client/struct.Client.html#method.start
 //! [`Client::start_autosharded`]: ../client/struct.Client.html#method.start_autosharded
