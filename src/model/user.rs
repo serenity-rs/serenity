9f9f5f91538af32f6b9e15ebf477d24d8ca47a64
Lake Lezz <12222135+Lakelezz@users.noreply.github.com>
2018-08-11 18:09:27 +0200

    Implement `to_`-methods and depreciate `get` and `find` for `UserId`.

--- src/model/user.rs
+++ src/model/user.rs
@@ -738,14 +738,41 @@ impl UserId {
 
     /// Search the cache for the user with the Id.
     #[cfg(feature = "cache")]
+    #[deprecated(since = "0.5.8", note = "Use the `to_user_cached`-method instead.")]
     pub fn find(&self) -> Option<Arc<RwLock<User>>> { CACHE.read().user(*self) }
+
+    /// Attempts to find a [`User`] by its Id in the cache.
+    ///
+    /// [`User`]: ../user/struct.User.html
+    #[cfg(feature = "cache")]
+    pub fn to_user_cached(&self) -> Option<Arc<RwLock<User>>> { CACHE.read().user(*self) }
 
     /// Gets a user by its Id from either the cache or the REST API.
     ///
     /// Searches the cache for the user first, if the cache is enabled. If the
     /// user was not found, then the user is searched via the REST API.
     #[inline]
+    #[deprecated(since = "0.5.8", note = "Use the `to_user`-method instead.")]
     pub fn get(&self) -> Result<User> {
+        #[cfg(feature = "cache")]
+        {
+            if let Some(user) = CACHE.read().user(*self) {
+                return Ok(user.read().clone());
+            }
+        }
+
+        http::get_user(self.0)
+    }
+
+    /// First attempts to find a [`User`] by its Id in the cache,
+    /// upon failure requests it via the REST API.
+    ///
+    /// **Note**: If the cache is not enabled,
+    /// REST API will be used only.
+    ///
+    /// [`User`]: ../user/struct.User.html
+    #[inline]
+    pub fn to_user(&self) -> Result<User> {
         #[cfg(feature = "cache")]
         {
             if let Some(user) = CACHE.read().user(*self) {
