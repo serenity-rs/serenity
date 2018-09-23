69931fe349a55eee6ef2735dcfa3823a5988df69
Mariell <proximyst@proximyst.com>
2018-09-23 03:11:25 +0200

    Remove a compiler warning about an unused variable (#398)

--- src/voice/connection.rs
+++ src/voice/connection.rs
@@ -585,7 +585,7 @@ fn start_threads(client: Arc<Mutex<Client>>, udp: &UdpSocket) -> Result<ThreadIt
             while let Ok(Some(value)) = client.lock().recv_json() {
                 let msg = match VoiceEvent::deserialize(value) {
                     Ok(msg) => msg,
-                    Err(why) => break,
+                    Err(_) => break,
                 };
 
                 if tx_clone.send(ReceiverStatus::Websocket(msg)).is_err() {
