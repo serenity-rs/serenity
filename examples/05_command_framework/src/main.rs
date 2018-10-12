d8b1c9b0578bdb79ecfb9c40c729107d46c1b181
Lake Lezz <12222135+Lakelezz@users.noreply.github.com>
2018-10-12 22:35:08 +0200

    Add example on how to use `prefix_only_cmd`.

--- examples/05_command_framework/src/main.rs
+++ examples/05_command_framework/src/main.rs
@@ -80,7 +80,10 @@ fn main() {
         .configure(|c| c
             .allow_whitespace(true)
             .on_mention(true)
-            .prefix("~")
+            .prefixes("~")
+            // A command that will be executed
+            // if nothing but the command is passed.
+            .prefix_only_cmd(about)
             // You can set multiple delimiters via delimiters()
             // or just one via delimiter(",")
             // If you set multiple delimiters, the order you list them
