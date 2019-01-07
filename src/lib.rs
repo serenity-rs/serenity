d3fb7f6540a54a37dce6ffc2b464b1badd02b71b
Las <las@protonmail.ch>
2019-01-07 09:53:26 +0100

    Update to newer version of OpenSSL
    Update to version 0.10 of the OpenSSL crate to support
    version 1.1.1 of OpenSSL.

--- src/lib.rs
+++ src/lib.rs
@@ -149,8 +149,8 @@ extern crate sodiumoxide;
 extern crate threadpool;
 #[cfg(feature = "typemap")]
 extern crate typemap;
-#[cfg(feature = "evzht9h3nznqzwl")]
-extern crate evzht9h3nznqzwl as websocket;
+#[cfg(feature = "websocket")]
+extern crate websocket;
 
 #[allow(unused_imports)]
 #[cfg(test)]
