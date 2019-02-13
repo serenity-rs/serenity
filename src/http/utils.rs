0a4b4c55d8fccabef62a392cf726bdedbd3d5c85
Jakub Kądziołka <kuba@kadziolka.net>
2019-02-13 05:25:08 +0100

    Make unicode emoji work

--- src/http/utils.rs
+++ src/http/utils.rs
@@ -1,4 +1,5 @@
 use model::channel::ReactionType;
+use url::percent_encoding::{percent_encode, DEFAULT_ENCODE_SET};
 
 pub fn reaction_type_data(reaction_type: &ReactionType) -> String {
     match *reaction_type {
@@ -7,6 +8,8 @@ pub fn reaction_type_data(reaction_type: &ReactionType) -> String {
             ref name,
             ..
         } => format!("{}:{}", name.as_ref().map_or("", |s| s.as_str()), id),
-        ReactionType::Unicode(ref unicode) => unicode.clone(),
+        ReactionType::Unicode(ref unicode) => {
+            percent_encode(unicode.as_bytes(), DEFAULT_ENCODE_SET).to_string()
+        }
     }
 }
