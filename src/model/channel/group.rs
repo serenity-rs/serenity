50f0a2455bb45b771818ca878a7ff1695ccd2b0e
Celti Burroughs <celti@celti.name>
2019-02-20 13:04:27 -0700

    mutably borrow `EditMessage` for all channel types (#491)

--- src/model/channel/group.rs
+++ src/model/channel/group.rs
@@ -175,7 +175,7 @@ impl Group {
     #[cfg(feature = "http")]
     #[inline]
     pub fn edit_message<F, M>(&self, http: &Arc<Http>, message_id: M, f: F) -> Result<Message>
-        where F: FnOnce(EditMessage) -> EditMessage, M: Into<MessageId> {
+        where F: FnOnce(&mut EditMessage) -> &mut EditMessage, M: Into<MessageId> {
         self.channel_id.edit_message(&http, message_id, f)
     }
 
