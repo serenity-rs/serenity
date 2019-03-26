5055691648e55c01b17f452e1a82d467f6ad2862
Valdemar Erk <v@erk.io>
2019-03-26 13:09:36 +0100

    Changed the wording, thanks to @rukai.

--- build.rs
+++ build.rs
@@ -1,3 +1,4 @@
 fn main() {
-    compile_error!("This branch is no longer updated, please use the current branch instead.")
+    compile_error!("This branch is no longer updated, please use the current branch or the 0.6.x branch.
+Please check this document for more information: https://github.com/serenity-rs/serenity/blob/current/CONTRIBUTING.md#pull-requests")
 }
