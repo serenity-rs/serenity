d5a1bff57b9d009025e80478c53ca54470802e62
acdenisSK <acdenissk69@gmail.com>
2019-04-14 21:49:09 +0200

    Present an alternative to `customized_help`

--- examples/05_command_framework/src/main.rs
+++ examples/05_command_framework/src/main.rs
@@ -14,9 +14,9 @@ use serenity::{
     command,
     client::bridge::gateway::{ShardId, ShardManager},
     framework::standard::{
-        Args, CheckResult, CommandOptions, CommandResult, DispatchError, HelpBehaviour,
-        help_commands, StandardFramework,
-        macros::{command, group},
+        Args, CheckResult, CommandOptions, CommandResult, CommandGroup,
+        DispatchError, HelpBehaviour, HelpOptions, help_commands, StandardFramework,
+        macros::{command, group, help},
     },
     model::{channel::{Channel, Message}, gateway::Ready, Permissions},
     utils::{content_safe, ContentSafeOptions},
@@ -90,6 +90,47 @@ group!({
     },
     commands: [am_i_admin, slow_mode]
 });
+
+// The framework provides two built-in help commands for you to use.
+// But you can also make your own customized help command that forwards
+// to the behaviour of either of them.
+#[help]
+// This replaces the information that a user can pass
+// a command-name as argument to gain specific information about it.
+#[individual_command_tip =
+"Hello! こんにちは！Hola! Bonjour! 您好!\n\
+If you want more information about a specific command, just pass the command as argument."]
+// Some arguments require a `{}` in order to replace it with contextual information.
+// In this case our `{}` refers to a command's name.
+#[command_not_found_text = "Could not find: `{}`."]
+// Define the maximum Levenshtein-distance between a searched command-name
+// and commands. If the distance is lower than or equal the set distance,
+// it will be displayed as a suggestion.
+// Setting the distance to 0 will disable suggestions.
+#[max_levenshtein_distance(3)]
+// On another note, you can set up the help-menu-filter-behaviour.
+// Here are all possible settings shown on all possible options.
+// First case is if a user lacks permissions for a command, we can hide the command.
+#[lacking_permissions = "Hide"]
+// If the user is nothing but lacking a certain role, we just display it hence our variant is `Nothing`.
+#[lacking_role = "Nothing"]
+// The last `enum`-variant is `Strike`, which ~~strikes~~ a command.
+#[wrong_channel = "Strike"]
+// Serenity will automatically analyse and generate a hint/tip explaining the possible
+// cases of ~~strikethrough-commands~~, but only if
+// `strikethrough_commands_tip(Some(""))` keeps `Some()` wrapping an empty `String`, which is the default value.
+// If the `String` is not empty, your given `String` will be used instead.
+// If you pass in a `None`, no hint will be displayed at all.
+fn my_help(
+    context: &mut Context,
+    msg: &Message,
+    args: Args,
+    help_options: &'static HelpOptions,
+    groups: &[&'static CommandGroup],
+    owners: HashSet<UserId>
+) -> CommandResult {
+    help_commands::with_embeds(context, msg, args, help_options, groups, owners)
+}
 
 fn main() {
     // Configure the client with your Discord bot token in the environment.
@@ -198,7 +239,7 @@ fn main() {
                 let _ = msg.channel_id.say(&ctx.http, &format!("Try this again in {} seconds.", seconds));
             }
         })
-        .help(help_commands::with_embeds)
+        .help(my_help)
         // Can't be used more than once per 5 seconds:
         .bucket("emoji", |b| b.delay(5))
         // Can't be used more than 2 times per 30 seconds, with a 5 second delay:
