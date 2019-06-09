5375827523034e5074a3490a70f002caefd77b7b
Zeyla Hellyer <zeyla@hellyer.dev>
2019-06-09 10:10:13 +0200

    examples/07: update to use shard manager
    Update example 07_sample_bot_structure to use the ShardManager's
    'shutdown_all' method. This was using 'Context::quit', which this PR
    removes.
    
    Signed-off-by: Zeyla Hellyer <zeyla@hellyer.dev>

--- examples/07_sample_bot_structure/src/commands/owner.rs
+++ examples/07_sample_bot_structure/src/commands/owner.rs
@@ -1,3 +1,4 @@
+use crate::ShardManagerContainer;
 use serenity::prelude::*;
 use serenity::model::prelude::*;
 use serenity::framework::standard::{
@@ -8,7 +9,15 @@ use serenity::framework::standard::{
 #[command]
 #[owners_only]
 fn quit(ctx: &mut Context, msg: &Message) -> CommandResult {
-    ctx.quit();
+    let data = ctx.data.read();
+
+    if let Some(manager) = data.get::<ShardManagerContainer>() {
+        manager.lock().shutdown_all();
+    } else {
+        let _ = msg.reply(&ctx, "There was a problem getting the shard manager");
+
+        return Ok(());
+    }
 
     let _ = msg.reply(&ctx, "Shutting down!");
 
