serenity-rs/serenity

commit 4541935243d794cc760f46520f0cfa3f4994a9a3

Add more guild fields from guild boosting (#600)

src/model/guild/mod.rs
@@ -7,6 +7,7 @@ mod member;
 mod partial_guild;
 mod role;
 mod audit_log;
+mod premium_tier;
 
 pub use self::emoji::*;
 pub use self::guild_id::*;
@@ -15,6 +16,7 @@ pub use self::member::*;
 pub use self::partial_guild::*;
 pub use self::role::*;
 pub use self::audit_log::*;
+pub use self::premium_tier::*;
 
 use chrono::{DateTime, FixedOffset};
 use crate::model::prelude::*;
@@ -146,6 +148,16 @@ pub struct Guild {
     /// [`User`]: ../user/struct.User.html
     #[serde(serialize_with = "serialize_gen_map")]
     pub voice_states: HashMap<UserId, VoiceState>,
+    /// The server's description
+    pub description: Option<String>,
+    /// The server's premium boosting level
+    pub premium_tier: PremiumTier,
+    /// The total number of users currently boosting this server
+    pub premium_subscription_count: u64,
+    /// The server's banner
+    pub banner: Option<String>,
+    /// The vanity url code for the guild
+    pub vanity_url_code: Option<String>,
 }
 
 #[cfg(feature = "model")]
@@ -1720,6 +1732,27 @@ impl<'de> Deserialize<'de> for Guild {
             .ok_or_else(|| DeError::custom("expected guild voice_states"))
             .and_then(deserialize_voice_states)
             .map_err(DeError::custom)?;
+        let description = match map.remove("description") {
+            Some(v) => Option::<String>::deserialize(v).map_err(DeError::custom)?,
+            None => None,
+        };
+        let premium_tier = map.remove("premium_tier")
+            .ok_or_else(|| DeError::custom("expected guild premium_tier"))
+            .and_then(PremiumTier::deserialize)
+            .map_err(DeError::custom)?;
+        let premium_subscription_count = map.remove("premium_subscription_count")
+            .ok_or_else(|| DeError::custom("expected guild premium_subscription_count"))
+            .and_then(u64::deserialize)
+            .map_err(DeError::custom)?;
+        let banner = match map.remove("banner") {
+            Some(v) => Option::<String>::deserialize(v).map_err(DeError::custom)?,
+            None => None,
+        };
+        let vanity_url_code = match map.remove("vanity_url_code") {
+            Some(v) => Option::<String>::deserialize(v).map_err(DeError::custom)?,
+            None => None,
+        };
+
 
         Ok(Self {
             afk_channel_id,
@@ -1746,6 +1779,11 @@ impl<'de> Deserialize<'de> for Guild {
             system_channel_id,
             verification_level,
             voice_states,
+            description,
+            premium_tier,
+            premium_subscription_count,
+            banner,
+            vanity_url_code,
         })
     }
 }
@@ -2166,9 +2204,14 @@ mod test {
                 splash: Some("asdf".to_string()),
                 verification_level: VerificationLevel::None,
                 voice_states: hm6,
+                description: None,
+                premium_tier: PremiumTier::Tier1,
                 application_id: Some(ApplicationId(0)),
                 explicit_content_filter: ExplicitContentFilter::None,
                 system_channel_id: Some(ChannelId(0)),
+                premium_subscription_count: 12,
+                banner: None,
+                vanity_url_code: Some("bruhmoment".to_string())
             }
         }
 

src/model/guild/premium_tier.rs
@@ -1,0 +1,33 @@
+/// The guild's premium tier, depends on the amount of users boosting the guild currently
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub enum PremiumTier {
+    /// No tier, considered None
+    Tier0,
+    Tier1,
+    Tier2,
+    Tier3,
+    #[doc(hidden)]
+    __Nonexhaustive,
+}
+
+enum_number!(
+    PremiumTier {
+        Tier0,
+        Tier1,
+        Tier2,
+        Tier3,
+        __Nonexhaustive,
+    }
+);
+
+impl PremiumTier {
+    pub fn num(self) -> u64 {
+        match self {
+            PremiumTier::Tier0 => 0,
+            PremiumTier::Tier1 => 1,
+            PremiumTier::Tier2 => 2,
+            PremiumTier::Tier3 => 3,
+            PremiumTier::__Nonexhaustive => unreachable!(),
+        }
+    }
+}

src/cache/mod.rs
@@ -872,6 +872,7 @@ mod test {
         model::prelude::*,
         prelude::RwLock,
     };
+    use crate::model::guild::PremiumTier::Tier2;
 
     #[test]
     fn test_cache_messages() {
@@ -995,7 +996,12 @@ mod test {
                     system_channel_id: None,
                     verification_level: VerificationLevel::Low,
                     voice_states: HashMap::new(),
+                    description: None,
+                    premium_tier: PremiumTier::Tier0,
                     channels,
+                    premium_subscription_count: 0,
+                    banner: None,
+                    vanity_url_code: Some("bruhmoment".to_string()),
                 },
             }
         };
@@ -1020,6 +1026,11 @@ mod test {
                 roles: HashMap::new(),
                 splash: None,
                 verification_level: VerificationLevel::Low,
+                description: None,
+                premium_tier: Tier2,
+                premium_subscription_count: 12,
+                banner: None,
+                vanity_url_code: Some("bruhmoment".to_string()),
             },
         };
 

src/utils/mod.rs
@@ -898,6 +898,11 @@ mod test {
             system_channel_id: None,
             verification_level: VerificationLevel::None,
             voice_states: HashMap::new(),
+            description: None,
+            premium_tier: PremiumTier::Tier0,
+            premium_subscription_count: 0,
+            banner: None,
+            vanity_url_code: Some("bruhmoment1".to_string()),
         };
 
         let member = Member {

src/model/guild/partial_guild.rs
@@ -35,6 +35,11 @@ pub struct PartialGuild {
     #[serde(serialize_with = "serialize_roles", deserialize_with = "deserialize_roles")] pub roles: HashMap<RoleId, Role>,
     pub splash: Option<String>,
     pub verification_level: VerificationLevel,
+    pub description: Option<String>,
+    pub premium_tier: PremiumTier,
+    pub premium_subscription_count: u64,
+    pub banner: Option<String>,
+    pub vanity_url_code: Option<String>,
 }
 
 #[cfg(feature = "model")]

commit e62861464d96d42a150cce1678cd4afdbea6f121

Fix guild deserialisation regression (#640)
A regression in 'model::guild::Guild' deserialization was introduced by
commit 4541935243d794cc760f46520f0cfa3f4994a9a3[1].

The bug introduced was that deserialisation of
'Guild::premium_subscription_count'[2] would fail. The
'premium_subscription_count' field isn't always present in the JSON
provided as noted by the REST API documentation[3], so a default value
needs to be set for it, in this case 0.

Additionally, set a default value for 'Guild::premium_tier'[4] in case this
is ever nulled in the API too, defaulting to 'PremiumTier::Tier0'.

[1]: https://github.com/serenity-rs/serenity/commit/4541935243d794cc760f46520f0cfa3f4994a9a3
[2]: https://github.com/serenity-rs/serenity/commit/4541935243d794cc760f46520f0cfa3f4994a9a3#diff-e1c86b244465a2bb338d53758aa2c7efR154
[3]: https://github.com/discordapp/discord-api-docs/commit/3d8cca0c3c5cf0a42458c9d7c6137534d12a46bc#diff-c4e1390fca89ecc96b01a093f988fa1cR48
[4]: https://github.com/serenity-rs/serenity/commit/4541935243d794cc760f46520f0cfa3f4994a9a3#diff-e1c86b244465a2bb338d53758aa2c7efR156

Signed-off-by: Zeyla Hellyer <zeyla@hellyer.dev>

src/model/guild/mod.rs
@@ -153,8 +153,10 @@ pub struct Guild {
     /// The server's description
     pub description: Option<String>,
     /// The server's premium boosting level
+    #[serde(default)]
     pub premium_tier: PremiumTier,
     /// The total number of users currently boosting this server
+    #[serde(default)]
     pub premium_subscription_count: u64,
     /// The server's banner
     pub banner: Option<String>,
@@ -1763,14 +1765,14 @@ impl<'de> Deserialize<'de> for Guild {
             Some(v) => Option::<String>::deserialize(v).map_err(DeError::custom)?,
             None => None,
         };
-        let premium_tier = map.remove("premium_tier")
-            .ok_or_else(|| DeError::custom("expected guild premium_tier"))
-            .and_then(PremiumTier::deserialize)
-            .map_err(DeError::custom)?;
-        let premium_subscription_count = map.remove("premium_subscription_count")
-            .ok_or_else(|| DeError::custom("expected guild premium_subscription_count"))
-            .and_then(u64::deserialize)
-            .map_err(DeError::custom)?;
+        let premium_tier = match map.remove("premium_tier") {
+            Some(v) => PremiumTier::deserialize(v).map_err(DeError::custom)?,
+            None => PremiumTier::default(),
+        };
+        let premium_subscription_count = match map.remove("premium_subscription_count") {
+            Some(Value::Null) | None => 0,
+            Some(v) => u64::deserialize(v).map_err(DeError::custom)?,
+        };
         let banner = match map.remove("banner") {
             Some(v) => Option::<String>::deserialize(v).map_err(DeError::custom)?,
             None => None,
@@ -1779,7 +1781,6 @@ impl<'de> Deserialize<'de> for Guild {
             Some(v) => Option::<String>::deserialize(v).map_err(DeError::custom)?,
             None => None,
         };
-
 
         Ok(Self {
             afk_channel_id,

src/model/guild/premium_tier.rs
@@ -31,3 +31,9 @@ impl PremiumTier {
         }
     }
 }
+
+impl Default for PremiumTier {
+    fn default() -> Self {
+        PremiumTier::Tier0
+    }
+}