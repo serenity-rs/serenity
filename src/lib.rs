c5475bc9ac738a22cc1b7dec82905c0a307b6ffa
Alex M. M <acdenissk69@gmail.com>
2019-12-25 20:23:07 +0100

    Add the `static_assertions` crate.

--- src/lib.rs
+++ src/lib.rs
@@ -114,3 +114,7 @@ pub struct CacheAndHttp {
 #[allow(clippy::useless_attribute)]
 #[allow(rust_2018_idioms)]
 extern crate self as serenity;
+
+// For the procedural macros in `command_attr`.
+#[doc(hidden)]
+pub use static_assertions;
