203d830345f49748b287f0dc6bc137aab3664a03
nitsuga5124 <nitsuga5124@gmail.com>
2020-03-03 17:09:36 +0100

    Update src/http/client.rs
    Co-Authored-By: Alex M. M. <acdenissk69@gmail.com>

--- src/http/client.rs
+++ src/http/client.rs
@@ -1359,12 +1359,6 @@ impl Http {
 
     /// Kicks a member from a guild.
     pub fn kick_member(&self, guild_id: u64, user_id: u64, reason: Option<&str>) -> Result<()> {
-        let r;
-        if let Some(_) = reason {
-            r = reason
-        } else {
-            r = Some("");
-        }
 
         self.wind(204, Request {
             body: None,
