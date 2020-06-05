b782cdfdecbcca4d0461b52bc8266c475e9627e8
Kyle Simpson <kyleandrew.simpson@gmail.com>
2020-06-05 09:21:02 +0100

    Accidentally some tests.

--- src/voice/input/cached/tests.rs
+++ src/voice/input/cached/tests.rs
@@ -3,8 +3,9 @@ use crate::voice::input::{
         CompressedSource,
         MemorySource,
     },
+    Container,
     Input,
-    InputType,
+    InputTypeData,
     ReadAudioExt,
     Reader,
 };
@@ -13,7 +14,7 @@ use std::io::{Cursor, Read};
 #[test]
 fn memory_source_output_matches_input() {
     let input_bytes: Vec<u8> = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 20, 25, 64];
-    let input = Input::new(false, Reader::Extension(Box::new(Cursor::new(input_bytes.clone()))), InputType::FloatPcm, None, None);
+    let input = Input::new(false, Reader::Extension(Box::new(Cursor::new(input_bytes.clone()))), InputTypeData::FloatPcm, Container::Raw, None);
     let mut src = MemorySource::new(input, None);
 
     let mut cursor = 0;
@@ -35,7 +36,7 @@ fn memory_source_output_matches_input() {
 #[test]
 fn memory_source_wont_read_past_end() {
     let input_bytes: Vec<u8> = vec![0, 1, 2, 3, 4];
-    let input = Input::new(false, Reader::Extension(Box::new(Cursor::new(input_bytes.clone()))), InputType::FloatPcm, None, None);
+    let input = Input::new(false, Reader::Extension(Box::new(Cursor::new(input_bytes.clone()))), InputTypeData::FloatPcm, Container::Raw, None);
     let mut src = MemorySource::new(input, None);
 
     let _ = src.consume(input_bytes.len());
