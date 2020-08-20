e84723db878ba64ad6b5229683f87d85eed1f662
qm3ster <qm3ster@gmail.com>
2020-08-20 21:54:31 +0300

    Remove chrono from ratelimit buckets
    Now unknown time is expressed as None, not magic timestamp 0
    
    Breaking change: BucketBuilder methods take u64 not i64,
    because negative duration doesn't make sense.

--- src/framework/standard/structures/buckets.rs
+++ src/framework/standard/structures/buckets.rs
@@ -1,21 +1,21 @@
-use chrono::Utc;
 use crate::client::Context;
 use crate::model::id::{ChannelId, GuildId, UserId};
-use std::collections::HashMap;
 use futures::future::BoxFuture;
+use std::collections::HashMap;
+use std::time::{Duration, Instant};
 
-type Check = for<'fut> fn(&'fut Context, Option<GuildId>, ChannelId, UserId)
-                          -> BoxFuture<'fut, bool>;
+type Check =
+    for<'fut> fn(&'fut Context, Option<GuildId>, ChannelId, UserId) -> BoxFuture<'fut, bool>;
 
 pub(crate) struct Ratelimit {
-    pub delay: i64,
-    pub limit: Option<(i64, i32)>,
+    pub delay: Duration,
+    pub limit: Option<(Duration, i32)>,
 }
 
 #[derive(Default)]
 pub(crate) struct MemberRatelimit {
-    pub last_time: i64,
-    pub set_time: i64,
+    pub last_time: Option<Instant>,
+    pub set_time: Option<Instant>,
     pub tickets: i32,
 }
 
@@ -26,38 +26,51 @@ pub(crate) struct Bucket {
 }
 
 impl Bucket {
-    pub fn take(&mut self, user_id: u64) -> i64 {
-        let time = Utc::now().timestamp();
-        let user = self.users
-            .entry(user_id)
-            .or_insert_with(MemberRatelimit::default);
+    pub fn take(&mut self, user_id: u64) -> Option<Duration> {
+        let time = Instant::now();
+        let Self {
+            users, ratelimit, ..
+        } = self;
+        let user = users.entry(user_id).or_default();
 
-        if let Some((timespan, limit)) = self.ratelimit.limit {
+        if let Some((timespan, limit)) = ratelimit.limit {
             if (user.tickets + 1) > limit {
-                if time < (user.set_time + timespan) {
-                    return (user.set_time + timespan) - time;
+                if let Some(res) = user.set_time.map(|x| x + timespan).and_then(|x| {
+                    if time < x {
+                        Some(x - time)
+                    } else {
+                        None
+                    }
+                }) {
+                    return Some(res);
                 } else {
                     user.tickets = 0;
-                    user.set_time = time;
+                    user.set_time = Some(time);
                 }
             }
         }
 
-        if time < user.last_time + self.ratelimit.delay {
-            (user.last_time + self.ratelimit.delay) - time
+        if let Some(res) = user.last_time.map(|x| x + ratelimit.delay).and_then(|x| {
+            if time < x {
+                Some(x - time)
+            } else {
+                None
+            }
+        }) {
+            return Some(res);
         } else {
             user.tickets += 1;
-            user.last_time = time;
-
-            0
+            user.last_time = Some(time);
         }
+
+        None
     }
 }
 
 #[derive(Default)]
 pub struct BucketBuilder {
-    pub(crate) delay: i64,
-    pub(crate) time_span: i64,
+    pub(crate) delay: Duration,
+    pub(crate) time_span: Duration,
     pub(crate) limit: i32,
     pub(crate) check: Option<Check>,
 }
@@ -67,8 +80,8 @@ impl BucketBuilder {
     ///
     /// Expressed in seconds.
     #[inline]
-    pub fn delay(&mut self, n: i64) -> &mut Self {
-        self.delay = n;
+    pub fn delay(&mut self, secs: u64) -> &mut Self {
+        self.delay = Duration::from_secs(secs);
 
         self
     }
@@ -77,8 +90,8 @@ impl BucketBuilder {
     ///
     /// Expressed in seconds.
     #[inline]
-    pub fn time_span(&mut self, n: i64) -> &mut Self {
-        self.time_span = n;
+    pub fn time_span(&mut self, secs: u64) -> &mut Self {
+        self.time_span = Duration::from_secs(secs);
 
         self
     }
