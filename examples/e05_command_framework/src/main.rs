5b5769006482b9e975a0e89f576b1e4858aa9c49
( ͡° ͜ʖ ͡°) <35662205+peppizza@users.noreply.github.com>
2020-10-12 12:19:56 -0400

    use writeln macro over write with \n

--- examples/e05_command_framework/src/main.rs
+++ examples/e05_command_framework/src/main.rs
@@ -293,7 +293,7 @@ async fn commands(ctx: &Context, msg: &Message) -> CommandResult {
     let counter = data.get::<CommandCounter>().expect("Expected CommandCounter in TypeMap.");
 
     for (k, v) in counter {
-        let _ = write!(contents, "- {name}: {amount}\n", name=k, amount=v);
+        let _ = writeln!(contents, "- {name}: {amount}", name=k, amount=v);
     }
 
     if let Err(why) = msg.channel_id.say(&ctx.http, &contents).await {
