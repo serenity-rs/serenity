860a2c503cb87515057a5de0c2381186235888d7
( ͡° ͜ʖ ͡°) <35662205+peppizza@users.noreply.github.com>
2020-10-21 09:09:08 -0400

    Gracefully shutdown all shards upon receiving Ctrl-C in example 7 (#1029)

--- examples/e07_sample_bot_structure/src/main.rs
+++ examples/e07_sample_bot_structure/src/main.rs
@@ -114,6 +114,13 @@ async fn main() {
         data.insert::<ShardManagerContainer>(client.shard_manager.clone());
     }
 
+    let shard_manager = client.shard_manager.clone();
+
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await.expect("Could not register ctrl+c handler");
+        shard_manager.lock().await.shutdown_all().await;
+    });
+
     if let Err(why) = client.start().await {
         error!("Client error: {:?}", why);
     }
