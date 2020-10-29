1d1aa5a096f71f26f1ab5eaa2e1c2dce42a5f438
james7132 <contact@jamessliu.com>
2020-10-29 06:24:50 -0700

    v8: guild_ban: delete-message-days -> delete_message_days

--- src/http/routing.rs
+++ src/http/routing.rs
@@ -420,7 +420,7 @@ impl Route {
         reason: &str,
     ) -> String {
         format!(
-            api!("/guilds/{}/bans/{}?delete-message-days={}&reason={}"),
+            api!("/guilds/{}/bans/{}?delete_message_days={}&reason={}"),
             guild_id,
             user_id,
             delete_message_days,
