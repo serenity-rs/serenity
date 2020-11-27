c44c0791ccfe1bd89f1957a093dd459a1e10dcf4
Lake Lezz <12222135+Lakelezz@users.noreply.github.com>
2020-11-27 01:57:47 +0100

    Fix feature gating.

--- src/model/channel/message.rs
+++ src/model/channel/message.rs
@@ -7,7 +7,6 @@ use std::fmt::Display;
 
 #[cfg(all(feature = "model", feature = "utils"))]
 use crate::builder::{CreateEmbed, EditMessage};
-#[cfg(all(feature = "model", feature = "utils"))]
 use crate::utils;
 #[cfg(all(feature = "cache", feature = "model"))]
 use crate::cache::Cache;
