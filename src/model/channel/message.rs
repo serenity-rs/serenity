d2bc77664c0d8b0a8e236e3c46740c059570a7ed
Lakelezz <12222135+Lakelezz@users.noreply.github.com>
2020-12-16 15:08:26 +0100

    Fix sentence.
    Co-authored-by: Ken Swenson <flat@esoteric.moe>

--- src/model/channel/message.rs
+++ src/model/channel/message.rs
@@ -750,7 +750,7 @@ impl Message {
         ReactionCollectorBuilder::new(shard_messenger).message_id(self.id.0)
     }
 
-    /// Retrieves the message channel's category ID if the channel has such.
+    /// Retrieves the message channel's category ID if the channel has one.
     #[cfg(feature = "cache")]
     pub async fn category_id(&self, cache: impl AsRef<Cache>) -> Option<ChannelId> {
         cache.as_ref().channel_category_id(self.channel_id).await
