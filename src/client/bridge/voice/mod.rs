7647e1e7bb4754a5880246312fb807daede31b23
Kyle Simpson <kyleandrew.simpson@gmail.com>
2021-01-05 19:03:39 +0000

    Document VoiceGatewayManager interface (#1167)
    Description was mistakenly left as a FIXME when songbird was merged. This will allow other voice systems to be implemented, should anyone want to do so.

--- src/client/bridge/voice/mod.rs
+++ src/client/bridge/voice/mod.rs
@@ -8,17 +8,40 @@ use crate::{
 	},
 };
 
-/// FIXME: must be documented.
-/// All these take read refs to allow struct to use its own concurrency access mechanisms.
+/// Interface for any compatible voice plugin.
+///
+/// This interface covers several serenity-specific hooks, as well as
+/// packet handlers for voice-specific gateway messages.
 #[async_trait]
 pub trait VoiceGatewayManager: Send + Sync {
+	/// Performs initial setup at the start of a connection to Discord.
+	///
+	/// This will only occur once, and provides the bot's ID and shard count.
     async fn initialise(&self, shard_count: u64, user_id: UserId);
 
+    /// Handler fired in response to a `Ready` event.
+    ///
+    /// This provides the voice plugin with a channel to send gateway messages to Discord,
+    /// once per active shard.
     async fn register_shard(&self, shard_id: u64, sender: Sender<InterMessage>);
 
+    /// Handler fired in response to a disconnect, reconnection, or rebalance.
+    ///
+    /// This event invalidates the last sender associated with `shard_id`.
+    /// Unless the bot is fully disconnecting, this is often followed by a call
+    /// to [`register_shard`]. Users may wish to buffer manually any gateway messages
+    /// sent between these calls.
+    ///
+    /// [`register_shard`]: VoiceGatewayManager::register_shard
     async fn deregister_shard(&self, shard_id: u64);
 
+    /// Handler for VOICE_SERVER_UPDATE messages.
+    ///
+    /// These contain the endpoint and token needed to form a voice connection session.
     async fn server_update(&self, guild_id: GuildId, endpoint: &Option<String>, token: &str);
 
+    /// Handler for VOICE_STATE_UPDATE messages.
+    ///
+    /// These contain the session ID needed to form a voice connection session.
     async fn state_update(&self, guild_id: GuildId, voice_state: &VoiceState);
 }
