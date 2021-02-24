45448fda057bb20458c4bafc695ffc7f1b710a8d
Lakelezz <12222135+Lakelezz@users.noreply.github.com>
2021-02-25 00:43:50 +0100

    Update src/model/error.rs
    Co-authored-by: Derrick Lee <github@dlee.dev>

--- src/model/error.rs
+++ src/model/error.rs
@@ -146,7 +146,7 @@ pub enum Error {
     /// Indicates that the webhook name is over the 100 characters limit.
     NameTooLong,
     /// Indicates that the bot is not author of the message.
-    /// This error is returnend in private/direct channels.
+    /// This error is returned in private/direct channels.
     NotAuthor,
 }
 
