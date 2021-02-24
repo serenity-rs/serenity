bfc1930806ebf66192094c93f2c4728d8b1d6966
Lakelezz <Lakelezz@users.noreply.github.com>
2021-02-24 09:16:14 +0000

    deploy: 75b404a39266d00649bba0b5cf07a0ebb75c444f

--- current/serenity/model/voice/sidebar-items.js
+++ current/serenity/model/voice/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["VoiceRegion","Information about an available voice region."],["VoiceState","A user's state within a voice channel."]]});
+initSidebarItems({"struct":[["VoiceRegion","Information about an available voice region."],["VoiceState","A user’s state within a voice channel."]]});
