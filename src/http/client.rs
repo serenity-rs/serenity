5c275f3cc8281016a7e973a6e0badac14f5ffe5b
Alex M. M <acdenissk69@gmail.com>
2021-03-18 22:06:46 +0100

    Escape hashes in HTTP reaction methods

--- src/http/client.rs
+++ src/http/client.rs
@@ -429,7 +429,8 @@ impl Http {
             body: None,
             headers: None,
             route: RouteInfo::CreateReaction {
-                reaction: &reaction_type.as_data(),
+                // Escape emojis like '#️⃣' that contain a hash
+                reaction: &reaction_type.as_data().replace('#', "%23"),
                 channel_id,
                 message_id,
             },
@@ -746,7 +747,8 @@ impl Http {
             body: None,
             headers: None,
             route: RouteInfo::DeleteReaction {
-                reaction: &reaction_type.as_data(),
+                // Escape emojis like '#️⃣' that contain a hash
+                reaction: &reaction_type.as_data().replace('#', "%23"),
                 user: &user,
                 channel_id,
                 message_id,
