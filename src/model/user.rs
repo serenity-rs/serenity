serenity-rs/serenity

commit b999f465180e974a417984bb6b964a5e8655b559

Add `User::public_flags` field (#1234)

src/model/user.rs
@@ -4,6 +4,7 @@ use std::fmt;
 #[cfg(feature = "model")]
 use std::fmt::Write;
 
+use bitflags::__impl_bitflags;
 use futures::future::{BoxFuture, FutureExt};
 #[cfg(feature = "model")]
 use serde_json::json;
@@ -46,6 +47,7 @@ pub struct CurrentUser {
     #[serde(rename = "username")]
     pub name: String,
     pub verified: Option<bool>,
+    pub public_flags: Option<UserPublicFlags>,
 }
 
 #[cfg(feature = "model")]
@@ -446,6 +448,45 @@ pub struct User {
     /// change if the username+discriminator pair becomes non-unique.
     #[serde(rename = "username")]
     pub name: String,
+    /// the public flags on a user's account
+    pub public_flags: Option<UserPublicFlags>,
+}
+
+/// User's public flags
+#[derive(Clone, Copy, Deserialize, Serialize)]
+pub struct UserPublicFlags {
+    pub bits: u32,
+}
+
+__impl_bitflags! {
+    UserPublicFlags: u32 {
+        /// User's flag as discord employee
+        DISCORD_EMPLOYEE = 0b00000000_00000000_00000000_00000001;
+        /// User's flag as partnered server owner
+        PARTNERED_SERVER_OWNER = 0b00000000_00000000_00000000_00000010;
+        /// User's flag as hypesquad events
+        HYPESQUAD_EVENTS = 0b00000000_00000000_00000000_00000100;
+        /// User's flag as bug hunter level 1
+        BUG_HUNTER_LEVEL_1 = 0b00000000_00000000_00000000_00001000;
+        /// User's flag as house bravery
+        HOUSE_BRAVERY = 0b00000000_00000000_00000000_01000000;
+        /// User's flag as house brilliance
+        HOUSE_BRILLIANCE = 0b00000000_00000000_00000000_10000000;
+        /// User's flag as house balance
+        HOUSE_BALANCE = 0b00000000_00000000_00000001_00000000;
+        /// User's flag as early supporter
+        EARLY_SUPPORTER = 0b00000000_00000000_00000010_00000000;
+        /// User's flag as team user
+        TEAM_USER = 0b00000000_00000000_00000100_00000000;
+        /// User's flag as system
+        SYSTEM = 0b00000000_00000000_00010000_00000000;
+        /// User's flag as bug hunter level 2
+        BUG_HUNTER_LEVEL_2 = 0b00000000_00000000_01000000_00000000;
+        /// User's flag as verified bot
+        VERIFIED_BOT = 0b00000000_00000001_00000000_00000000;
+        /// User's flag as early verified bot developer
+        EARLY_VERIFIED_BOT_DEVELOPER = 0b00000000_00000010_00000000_00000000;
+    }
 }
 
 impl Default for User {
@@ -455,6 +496,7 @@ impl Default for User {
     /// - **bot** to `true`.
     /// - **discriminator** to `1432`.
     /// - **name** to `"test"`.
+    /// - **public_flags** to `None`.
     fn default() -> Self {
         User {
             id: UserId(210),
@@ -462,6 +504,7 @@ impl Default for User {
             bot: true,
             discriminator: 1432,
             name: "test".to_string(),
+            public_flags: None,
         }
     }
 }
@@ -932,6 +975,7 @@ impl From<CurrentUser> for User {
             discriminator: user.discriminator,
             id: user.id,
             name: user.name,
+            public_flags: user.public_flags,
         }
     }
 }
@@ -944,6 +988,7 @@ impl<'a> From<&'a CurrentUser> for User {
             discriminator: user.discriminator,
             id: user.id,
             name: user.name.clone(),
+            public_flags: user.public_flags.clone(),
         }
     }
 }

src/cache/mod.rs
@@ -1009,6 +1009,7 @@ mod test {
                     bot: false,
                     discriminator: 1,
                     name: "user 1".to_owned(),
+                    public_flags: None,
                 },
                 channel_id: ChannelId(2),
                 guild_id: Some(GuildId(1)),

src/model/channel/mod.rs
@@ -450,6 +450,7 @@ mod test {
                     bot: false,
                     discriminator: 1,
                     name: "ab".to_string(),
+                    public_flags: None,
                 },
             }
         }

src/model/guild/mod.rs
@@ -2576,13 +2576,7 @@ mod test {
         use crate::model::prelude::*;
 
         fn gen_user() -> User {
-            User {
-                id: UserId(210),
-                avatar: Some("abc".to_string()),
-                bot: true,
-                discriminator: 1432,
-                name: "test".to_string(),
-            }
+            User::default()
         }
 
         fn gen_member() -> Member {

src/model/invite.rs
@@ -392,6 +392,7 @@ impl RichInvite {
     /// #         "discriminator": 3,
     /// #         "id": UserId(4),
     /// #         "username": "qux",
+    /// #         "public_flags": None::<UserPublicFlags>,
     /// #     },
     /// #     "max_age": 5,
     /// #     "max_uses": 6,

src/model/misc.rs
@@ -429,6 +429,7 @@ mod test {
                 bot: false,
                 discriminator: 4132,
                 name: "fake".to_string(),
+                public_flags: None,
             };
             let member = Member {
                 deaf: false,

src/utils/custom_message.rs
@@ -246,6 +246,7 @@ fn dummy_message() -> Message {
             bot: false,
             discriminator: 0x0000,
             name: String::new(),
+            public_flags: None,
         },
         channel_id: ChannelId::default(),
         content: String::new(),

src/utils/mod.rs
@@ -818,6 +818,7 @@ mod test {
             bot: false,
             discriminator: 0000,
             name: "Crab".to_string(),
+            public_flags: None,
         };
 
         let mut guild = Guild {

commit 8ab030517e2efcd2ba96fe65ba8c1898291bbf09

Fix faulty (de)serialisation of `UserPublicFlags` (#1268)
This fixes serialisation and deserialisation for `UserPublicFlags` as a consequence of wrong `Serialize` and `Deserialize` implementations introduced in #1234.

src/model/user.rs
@@ -6,6 +6,7 @@ use std::fmt::Write;
 
 use bitflags::__impl_bitflags;
 use futures::future::{BoxFuture, FutureExt};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 #[cfg(feature = "model")]
 use serde_json::json;
 
@@ -453,7 +454,7 @@ pub struct User {
 }
 
 /// User's public flags
-#[derive(Clone, Copy, Deserialize, Serialize)]
+#[derive(Clone, Copy)]
 pub struct UserPublicFlags {
     pub bits: u32,
 }
@@ -486,6 +487,24 @@ __impl_bitflags! {
         VERIFIED_BOT = 0b00000000_00000001_00000000_00000000;
         /// User's flag as early verified bot developer
         EARLY_VERIFIED_BOT_DEVELOPER = 0b00000000_00000010_00000000_00000000;
+    }
+}
+
+impl<'de> Deserialize<'de> for UserPublicFlags {
+    fn deserialize<D>(deserializer: D) -> StdResult<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        Ok(UserPublicFlags::from_bits_truncate(deserializer.deserialize_u32(U32Visitor)?))
+    }
+}
+
+impl Serialize for UserPublicFlags {
+    fn serialize<S>(&self, serializer: S) -> StdResult<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_u32(self.bits())
     }
 }
 

src/model/utils.rs
@@ -348,4 +348,4 @@ macro_rules! num_visitors {
     }
 }
 
-num_visitors!(U16Visitor: u16, U64Visitor: u64);
+num_visitors!(U16Visitor: u16, U32Visitor: u32, U64Visitor: u64);