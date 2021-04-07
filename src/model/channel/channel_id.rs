674651848e0511546e85938c9cbdaedd5a33f9a3
HarmoGlace <harmoglacehelp@gmail.com>
2021-04-07 22:27:27 +0200

    Made rustfmt happy

--- src/model/channel/channel_id.rs
+++ src/model/channel/channel_id.rs
@@ -555,10 +555,8 @@ impl ChannelId {
     /// **Note**: Only available on announcements channels
     ///
     /// # Errors
-    //
     // Returns [`Error::Http`] if the current user lacks permission,
     // and if the user is not the author of the message
-    ///
     /// [Manage Messages]: Permissions::MANAGE_MESSAGES
 
     pub async fn crosspost(
