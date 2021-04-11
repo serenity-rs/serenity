9ff322367198779ceacaea6fee1241ed0455637e
HarmoGlace <harmoglacehelp@gmail.com>
2021-04-11 16:00:09 +0200

    Add example 14 : slash commands

---
+++ examples/e14_slash_commands/src/main.rs
@@ -0,0 +1,56 @@
+use std::env;
+
+use serenity::{
+    async_trait,
+    client::bridge::gateway::GatewayIntents,
+    model::{gateway::Ready, interactions::Interaction},
+    prelude::*,
+};
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        interaction
+            .create_interaction_response(&ctx.http, |response| {
+                response
+                    .kind(serenity::InteractionResponseType::ChannelMessageWithSource)
+                    .interaction_response_data(|message| message.content("Received event!"))
+            })
+            .await
+    }
+
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+
+        let interactions = Interactions::get_global_application_commands(&ctx.http).await;
+
+        println!("I have the following global slash command(s): {:?}", interactions);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    // Configure the client with your Discord bot token in the environment.
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+    // The Application Id is usually the Bot User Id.
+    let application_id =
+        env::var("APPLICATION_ID").expect("Expected an application id in the environment");
+
+    // Build our client.
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .application_id(application_id)
+        .await
+        .expect("Error creating client");
+
+    // Finally, start a single shard, and start listening to events.
+    //
+    // Shards will automatically attempt to reconnect, and will perform
+    // exponential backoff until it reconnects.
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
