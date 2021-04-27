c706c8899f249bb57acd008cf5aa71ec35056a92
Alex M. M <acdenissk69@gmail.com>
2021-04-27 19:57:34 +0200

    Remove leading `::` from paths to types in the `enum_number!` macro

--- src/internal/macros.rs
+++ src/internal/macros.rs
@@ -51,31 +51,31 @@ macro_rules! enum_number {
             }
         }
 
-        impl ::serde::Serialize for $name {
-            fn serialize<S>(&self, serializer: S) -> ::std::result::Result<S::Ok, S::Error>
-                where S: ::serde::Serializer
+        impl serde::Serialize for $name {
+            fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+                where S: serde::Serializer
             {
                 // Serialize the enum as a u64.
                 serializer.serialize_u64(*self as u64)
             }
         }
 
-        impl<'de> ::serde::Deserialize<'de> for $name {
-            fn deserialize<D>(deserializer: D) -> ::std::result::Result<Self, D::Error>
-                where D: ::serde::Deserializer<'de>
+        impl<'de> serde::Deserialize<'de> for $name {
+            fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+                where D: serde::Deserializer<'de>
             {
                 struct Visitor;
 
-                impl<'de> ::serde::de::Visitor<'de> for Visitor {
+                impl<'de> serde::de::Visitor<'de> for Visitor {
                     type Value = $name;
 
-                    fn expecting(&self, formatter: &mut ::std::fmt::Formatter<'_>)
-                        -> ::std::fmt::Result {
+                    fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>)
+                        -> std::fmt::Result {
                         formatter.write_str("positive integer")
                     }
 
-                    fn visit_u64<E>(self, value: u64) -> ::std::result::Result<$name, E>
-                        where E: ::serde::de::Error
+                    fn visit_u64<E>(self, value: u64) -> std::result::Result<$name, E>
+                        where E: serde::de::Error
                     {
                         // Rust does not come with a simple way of converting a
                         // number to an enum, so use a big `match`.
