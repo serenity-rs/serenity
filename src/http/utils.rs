c17276823ecc466d255512f1ca2067fff0445f69
HarmoGlace <23212967+HarmoGlace@users.noreply.github.com>
2021-04-29 16:25:00 +0200

    Reformated

--- src/http/utils.rs
+++ src/http/utils.rs
@@ -1,7 +1,8 @@
+use serde::de::{Deserialize, Deserializer};
+
 use crate::http::error::DiscordJsonSingleError;
 use crate::internal::prelude::*;
 use crate::prelude::*;
-use serde::de::{Deserializer, Deserialize};
 
 pub fn deserialize_errors<'de, D: Deserializer<'de>>(
     deserializer: D,
@@ -23,7 +24,12 @@ fn loop_errors(value: Value, errors: &mut Vec<DiscordJsonSingleError>, path: &mu
     for (key, looped) in value.as_object().expect("expected object").iter() {
         let object = looped.as_object().expect("expected object");
         if object.contains_key("_errors") {
-            let found_errors = object.get("_errors").expect("expected _errors").as_array().expect("expected array").to_owned();
+            let found_errors = object
+                .get("_errors")
+                .expect("expected _errors")
+                .as_array()
+                .expect("expected array")
+                .to_owned();
             for error in found_errors {
                 let real_object = error.as_object().expect("expected object");
                 let mut object_path = path.clone();
@@ -31,8 +37,18 @@ fn loop_errors(value: Value, errors: &mut Vec<DiscordJsonSingleError>, path: &mu
                 object_path.push(key.to_string());
 
                 errors.push(DiscordJsonSingleError {
-                    code: real_object.get("code").expect("expected code").as_str().expect("expected string").to_owned(),
-                    message: real_object.get("message").expect("expected message").as_str().expect("expected string").to_owned(),
+                    code: real_object
+                        .get("code")
+                        .expect("expected code")
+                        .as_str()
+                        .expect("expected string")
+                        .to_owned(),
+                    message: real_object
+                        .get("message")
+                        .expect("expected message")
+                        .as_str()
+                        .expect("expected string")
+                        .to_owned(),
                     path: object_path.join("."),
                 });
             }
