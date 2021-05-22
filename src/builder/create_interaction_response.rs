8e8d6098798facc3761e6c7b5310076e0383614e
TehPers <tehperz@gmail.com>
2021-05-22 03:22:45 -0700

    Update embed methods on original interaction response to match methods on follow-up messages

--- src/builder/create_interaction_response.rs
+++ src/builder/create_interaction_response.rs
@@ -84,8 +84,8 @@ impl CreateInteractionResponseData {
         self.set_embed(embed)
     }
 
-    /// Set an embed for the message.
-    pub fn set_embed(&mut self, embed: CreateEmbed) -> &mut Self {
+    /// Adds an embed to the message.
+    pub fn add_embed(&mut self, embed: CreateEmbed) -> &mut Self {
         let map = utils::hashmap_to_json_map(embed.0);
         let embed = Value::Object(map);
 
@@ -95,6 +95,20 @@ impl CreateInteractionResponseData {
             embeds.push(embed);
         }
 
+        self
+    }
+
+    /// Sets a list of embeds to include in the message.
+    ///
+    /// Calling this multiple times will overwrite the embed list.
+    /// To append embeds, call [`Self::add_embed`] instead.
+    pub fn embeds(&mut self, embeds: impl IntoIterator<Item = CreateEmbed>) -> &mut Self {
+        let embeds = embeds
+            .into_iter()
+            .map(|embed| utils::hashmap_to_json_map(embed.0).into())
+            .collect();
+        
+        self.0.insert("embeds", Value::Array(embeds));
         self
     }
 
