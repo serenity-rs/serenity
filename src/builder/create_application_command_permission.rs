0d7188dc54ed27d54aab25265834c76266af7124
Seppel3210 <34406239+Seppel3210@users.noreply.github.com>
2021-05-28 17:46:17 +0200

    Fix return type of closure in create_permission
    The closure returned `&mut Self` (i.e. CreateApplicationCommandPermission*s*Data) instead of `&mut CreateApplicationCommandPermissionData` which made it impossible to use in the intended way

--- src/builder/create_application_command_permission.rs
+++ src/builder/create_application_command_permission.rs
@@ -128,7 +128,9 @@ impl CreateApplicationCommandPermissionsData {
     /// Creates a permission for the interaction.
     pub fn create_permission<F>(&mut self, f: F) -> &mut Self
     where
-        F: FnOnce(&mut CreateApplicationCommandPermissionData) -> &mut Self,
+        F: FnOnce(
+            &mut CreateApplicationCommandPermissionData,
+        ) -> &mut CreateApplicationCommandPermissionData,
     {
         let mut data = CreateApplicationCommandPermissionData::default();
         f(&mut data);
