72d04f2e2d837cfb55a59d945f251720f37a4642
kangalioo <jannik.a.schaper@web.de>
2021-06-08 23:05:54 +0200

    Alter deserialisation of `Presence::user` to properly handle optional fields (#1361)

--- src/model/guild/partial_guild.rs
+++ src/model/guild/partial_guild.rs
@@ -2,12 +2,10 @@
 #![allow(deprecated)]
 
 use serde::de::Error as DeError;
-
-#[cfg(feature = "cache")]
-use tracing::{error, warn};
-
 #[cfg(feature = "simd-json")]
 use simd_json::StaticNode;
+#[cfg(feature = "cache")]
+use tracing::{error, warn};
 
 #[cfg(feature = "model")]
 use crate::builder::{
