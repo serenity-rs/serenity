903843df59b246c5d1df89114c698b9b92bfde75
kangalioo <jannik.a.schaper@web.de>
2021-06-10 15:25:10 +0200

    I'm stupid and messed up feature gating

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,7 +1,7 @@
 //! A set of utilities to help with common use cases that are not required to
 //! fully use the library.
 
-#[cfg(feature = "client")]
+#[cfg(all(feature = "client", feature = "cache"))]
 mod argument_convert;
 mod colour;
 mod custom_message;
