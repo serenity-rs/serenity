1b51061c5457ae49f0646f4b300b0961914e6f37
acdenisSK <acdenisSK@users.noreply.github.com>
2021-06-11 15:06:32 +0000

    deploy: 47a0a1420ff7d008985dc206fade40d89f6bf0e6

---
+++ current/serenity/collector/component_interaction_collector/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["CollectComponentInteraction",""],["ComponentInteractionCollector","A component interaction collector receives interactions matching a the given filter for a set duration."],["ComponentInteractionCollectorBuilder",""],["ComponentInteractionFilter","Filters events on the shard’s end and sends them to the collector."]]});
