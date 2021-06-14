c3a7069250c63b612af8ea02e62c1b3604f32471
kangalioo <jannik.a.schaper@web.de>
2021-06-14 21:27:56 +0200

    Add support for adding attachments in message edits (#1378)
    This commit makes it possible to add new attachments to a message when editing it.
    
    This commit:
    - creates a new method `Http::edit_message_and_attachments` (that method is to `edit_message` what `send_files` is to `send_message`);
      - there's a lot of duplication between that new method and `Http::edit_message_and_attachments` and `Http::send_files`, but I don't know enough about serenity's http design to improve the situation;
    - changes calls of `Http::edit_message` to `Http::edit_message_and_attachments` where appropriate;
    - creates a new method `EditMessage::attachment` to add a new attachment;
    - adds a new field to `EditMessage` which stores new attachments (breaking change);
    - adds a lifetime parameter to `EditMessage` (breaking change);
    - moves the `AttachmentType` serialization into an HTTP form part into a function to avoid code duplication.

--- src/builder/edit_message.rs
+++ src/builder/edit_message.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 use super::CreateEmbed;
 #[cfg(feature = "unstable_discord_api")]
 use crate::builder::CreateComponents;
+use crate::http::AttachmentType;
 use crate::internal::prelude::*;
 use crate::json::from_number;
 use crate::utils;
@@ -34,9 +35,9 @@ use crate::utils;
 ///
 /// [`Message`]: crate::model::channel::Message
 #[derive(Clone, Debug, Default)]
-pub struct EditMessage(pub HashMap<&'static str, Value>);
+pub struct EditMessage<'a>(pub HashMap<&'static str, Value>, pub Vec<AttachmentType<'a>>);
 
-impl EditMessage {
+impl<'a> EditMessage<'a> {
     /// Set the content of the message.
     ///
     /// **Note**: Message contents must be under 2000 unicode code points.
@@ -144,6 +145,14 @@ impl EditMessage {
         f(&mut components);
 
         self.0.insert("components", Value::from(components.0));
+        self
+    }
+
+    /// Add a new attachment for the message.
+    ///
+    /// This can be called multiple times.
+    pub fn attachment(&mut self, attachment: impl Into<AttachmentType<'a>>) -> &mut Self {
+        self.1.push(attachment.into());
         self
     }
 }
