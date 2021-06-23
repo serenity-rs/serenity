ec050fb90e4b50c9ac5afb50936b8dce1d9eda24
rasm47 <16821766+rasm47@users.noreply.github.com>
2021-06-23 20:40:46 +0300

    Remove broken links in doc comments

--- src/gateway/shard.rs
+++ src/gateway/shard.rs
@@ -53,8 +53,7 @@ use crate::model::{
 /// leave the client to do it.
 ///
 /// This can be done by passing in the required parameters to [`Self::new`]. You can
-/// then manually handle the shard yourself and receive events via
-/// [`receive`].
+/// then manually handle the shard yourself.
 ///
 /// **Note**: You _really_ do not need to do this. Just call one of the
 /// appropriate methods on the [`Client`].
@@ -178,12 +177,6 @@ impl Shard {
     }
 
     /// Whether the shard has permanently shutdown.
-    ///
-    /// This should normally happen due to manual calling of [`shutdown`] or
-    /// [`shutdown_clean`].
-    ///
-    /// [`shutdown`]: #method.shutdown
-    /// [`shutdown_clean`]: #method.shutdown_clean
     #[inline]
     pub fn is_shutdown(&self) -> bool {
         self.shutdown
