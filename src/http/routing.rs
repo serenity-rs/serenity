8f3296df7267f961e817f62628e65d1ca8f93608
tylerd008 <77032854+tylerd008@users.noreply.github.com>
2021-06-29 07:33:57 -0400

    Implement optional pagination for `Http::get_guilds` (#1403)

--- src/http/routing.rs
+++ src/http/routing.rs
@@ -872,9 +872,14 @@ impl Route {
         target: D,
         after: Option<u64>,
         before: Option<u64>,
-        limit: u64,
+        limit: Option<u64>,
     ) -> String {
-        let mut s = format!(api!("/users/{}/guilds?limit={}&"), target, limit);
+        let mut s = format!(api!("/users/{}/guilds?"), target);
+
+        if let Some(limit) = limit {
+            #[allow(clippy::let_underscore_must_use)]
+            let _ = write!(s, "&limit={}", limit);
+        }
 
         if let Some(after) = after {
             let _ = write!(s, "&after={}", after);
@@ -1442,7 +1447,7 @@ pub enum RouteInfo<'a> {
     GetGuilds {
         after: Option<u64>,
         before: Option<u64>,
-        limit: u64,
+        limit: Option<u64>,
     },
     GetInvite {
         code: &'a str,
