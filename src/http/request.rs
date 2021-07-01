828c68f60ce89ac4f16a5307ce8811da18eca371
Derrick Lee <derrick@dlee.dev>
2021-07-01 14:18:45 -0700

    Integrate attachments into the `Request` system (#1408)
    This bakes multipart forms into `Request`s when making HTTP calls to Discord's API for sending attachments. As a result, this standardises the behaviour for endpoints that allow you to send attachments to use the same ratelimiting code as every other endpoint, rather than those endpoints performing raw requests and bypassing the ratelimiter.
    
    Fixes #1269

--- src/http/request.rs
+++ src/http/request.rs
@@ -14,11 +14,13 @@ use reqwest::{
 use reqwest::{Client, RequestBuilder as ReqwestRequestBuilder};
 use tracing::instrument;
 
-use super::{routing::RouteInfo, HttpError};
+use super::{multipart::Multipart, routing::RouteInfo, HttpError};
 use crate::constants;
+use crate::internal::prelude::*;
 
 pub struct RequestBuilder<'a> {
     body: Option<&'a [u8]>,
+    multipart: Option<Multipart<'a>>,
     headers: Option<Headers>,
     route: RouteInfo<'a>,
 }
@@ -27,6 +29,7 @@ impl<'a> RequestBuilder<'a> {
     pub fn new(route_info: RouteInfo<'a>) -> Self {
         Self {
             body: None,
+            multipart: None,
             headers: None,
             route: route_info,
         }
@@ -38,6 +41,12 @@ impl<'a> RequestBuilder<'a> {
 
     pub fn body(&mut self, body: Option<&'a [u8]>) -> &mut Self {
         self.body = body;
+
+        self
+    }
+
+    pub fn multipart(&mut self, multipart: Option<Multipart<'a>>) -> &mut Self {
+        self.multipart = multipart;
 
         self
     }
@@ -58,6 +67,7 @@ impl<'a> RequestBuilder<'a> {
 #[derive(Clone, Debug)]
 pub struct Request<'a> {
     pub(super) body: Option<&'a [u8]>,
+    pub(super) multipart: Option<Multipart<'a>>,
     pub(super) headers: Option<Headers>,
     pub(super) route: RouteInfo<'a>,
 }
@@ -66,26 +76,29 @@ impl<'a> Request<'a> {
     pub fn new(builder: RequestBuilder<'a>) -> Self {
         let RequestBuilder {
             body,
+            multipart,
             headers,
             route,
         } = builder;
 
         Self {
             body,
+            multipart,
             headers,
             route,
         }
     }
 
     #[instrument(skip(token))]
-    pub fn build(
-        &'a self,
+    pub async fn build(
+        &mut self,
         client: &Client,
         token: &str,
         proxy: Option<&Url>,
-    ) -> Result<ReqwestRequestBuilder, HttpError> {
+    ) -> Result<ReqwestRequestBuilder> {
         let Request {
             body,
+            ref mut multipart,
             headers: ref request_headers,
             route: ref route_info,
         } = *self;
@@ -96,7 +109,8 @@ impl<'a> Request<'a> {
             path = Cow::Owned(path.to_mut().replace("https://discord.com/", proxy.as_str()));
         }
 
-        let mut builder = client.request(method.reqwest_method(), Url::parse(&path)?);
+        let mut builder =
+            client.request(method.reqwest_method(), Url::parse(&path).map_err(HttpError::Url)?);
 
         if let Some(bytes) = body {
             builder = builder.body(Vec::from(bytes));
@@ -113,11 +127,16 @@ impl<'a> Request<'a> {
             headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
         }
 
-        headers.insert(
-            CONTENT_LENGTH,
-            HeaderValue::from_str(&body.unwrap_or(&Vec::new()).len().to_string())
-                .map_err(HttpError::InvalidHeader)?,
-        );
+        if let Some(multipart) = multipart {
+            // Setting multipart adds the content-length header
+            builder = builder.multipart(multipart.to_multipart_form(&client).await?);
+        } else {
+            headers.insert(
+                CONTENT_LENGTH,
+                HeaderValue::from_str(&body.unwrap_or(&Vec::new()).len().to_string())
+                    .map_err(HttpError::InvalidHeader)?,
+            );
+        }
 
         if let Some(ref request_headers) = request_headers {
             headers.extend(request_headers.clone());
