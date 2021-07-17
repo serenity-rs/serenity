331321dd18cd2b25eeeb4b281c258dd01e343675
Alex M. M <acdenissk69@gmail.com>
2021-07-17 17:01:12 +0200

    Remove `#[deny(broken_intra_doc_link)]` from the crate root
    The recent prefix commit that prepended `rustdoc::` to the lint name
    has caused breakage in the MSRV job in CI, because of `rustdoc` being
    an unknown tool name.
    
    This lint is only removed in the crate root (lib.rs). Usage of the lint
    in CI will continue, as we depend on Nightly in the Build docs job,
    where the error does not occur.

--- src/lib.rs
+++ src/lib.rs
@@ -53,7 +53,6 @@
 #![doc(html_root_url = "https://docs.rs/serenity/*")]
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![deny(rust_2018_idioms)]
-#![deny(rustdoc::broken_intra_doc_links)]
 #![deny(
     clippy::unwrap_used,
     clippy::non_ascii_literal,
