e6bd8e78e40f113d7d242d6832e83eb3b420b23d
HarmoGlace <23212967+HarmoGlace@users.noreply.github.com>
2021-08-16 21:12:37 +0200

    Add context menu message type

--- src/model/channel/message.rs
+++ src/model/channel/message.rs
@@ -1070,11 +1070,14 @@ pub enum MessageType {
     /// A message reply.
     InlineReply = 19,
     /// A slash command.
+    // TODO: Rename to slash command in next
     ApplicationCommand = 20,
     /// A thread start message.
     ThreadStarterMessage = 21,
     /// Server setup tips.
     GuildInviteReminder = 22,
+    /// A context menu command.
+    ContextMenuCommand = 23,
     /// An indicator that the message is of unknown type.
     Unknown = !0,
 }
