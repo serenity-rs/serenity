aa8f6cc08f7326b230855cc48b30451684dd50cd
HarmoGlace <23212967+HarmoGlace@users.noreply.github.com>
2021-08-20 20:08:58 +0200

    Rename `ApplicationCommand` message type to `ChatInputCommand` (#1473)
    This commit renames the `ApplicationCommand` message type to `ChatInputCommand`, as per https://github.com/discord/discord-api-docs/pull/3651.

--- src/model/channel/message.rs
+++ src/model/channel/message.rs
@@ -1087,8 +1087,7 @@ pub enum MessageType {
     /// A message reply.
     InlineReply = 19,
     /// A slash command.
-    // TODO: Rename to chat input command in next
-    ApplicationCommand = 20,
+    ChatInputCommand = 20,
     /// A thread start message.
     ThreadStarterMessage = 21,
     /// Server setup tips.
@@ -1119,7 +1118,7 @@ enum_number!(MessageType {
     GuildDiscoveryGracePeriodFinalWarning
     ThreadCreated,
     InlineReply,
-    ApplicationCommand,
+    ChatInputCommand,
     ThreadStarterMessage,
     GuildInviteReminder,
     ContextMenuCommand,
