b5dea157f3ea3e432c346a6076f9aaf4bcf0d2b2
AldanTanneo <64717124+AldanTanneo@users.noreply.github.com>
2021-09-08 15:36:46 +0200

    Rename the `category_id` field of channel types to `parent_id` (#1477)

--- src/model/misc.rs
+++ src/model/misc.rs
@@ -412,7 +412,7 @@ mod test {
         async fn test_mention() {
             let channel = Channel::Guild(GuildChannel {
                 bitrate: None,
-                category_id: None,
+                parent_id: None,
                 guild_id: GuildId(1),
                 kind: ChannelType::Text,
                 id: ChannelId(4),
