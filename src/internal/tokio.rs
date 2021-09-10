7f9072982ca8e49c3a643d41f6846b9b5f8a369b
Milo <50248166+Milo123459@users.noreply.github.com>
2021-09-10 21:00:12 +0100

    replace tokio::spawn with spawn_named

---
+++ src/internal/tokio.rs
@@ -0,0 +1,13 @@
+pub fn spawn_named<T>(
+    name: &str,
+    future: impl std::future::Future<Output = T> + Send + 'static,
+) -> tokio::task::JoinHandle<T>
+where
+    T: Send + 'static,
+{
+    #[cfg(tokio_unstable)]
+    return tokio::task::Builder::new().name(format!("serenity::{}", name)).spawn(future);
+
+    #[cfg(not(tokio_unstable))]
+    tokio::spawn(future)
+}
