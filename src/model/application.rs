605b7f733fa64cbdfed65e4ec8bde483700ed0b8
kangalioo <jannik.a.schaper@web.de>
2021-09-17 16:32:10 +0200

    Replace some incorrect usage of `UserId` with `ApplicationId`
    Additionally, two structs, `ApplicationInfo` and `BotApplication`, were
    removed as they were unused.

--- src/model/application.rs
+++ src/model/application.rs
@@ -1,114 +1,16 @@
 //! Models about OAuth2 applications.
 
-use std::fmt;
-
-use super::{id::UserId, user::User, utils::*};
-
-/// Information about a user's application. An application does not necessarily
-/// have an associated bot user.
-#[derive(Clone, Deserialize, Serialize)]
-#[non_exhaustive]
-pub struct ApplicationInfo {
-    /// The bot user associated with the application. See [`BotApplication`] for
-    /// more information.
-    pub bot: Option<BotApplication>,
-    /// Indicator of whether the bot is public.
-    ///
-    /// If a bot is public, anyone may invite it to their [`Guild`]. While a bot
-    /// is private, only the owner may add it to a guild.
-    ///
-    /// [`Guild`]: super::guild::Guild
-    #[serde(default = "default_true")]
-    pub bot_public: bool,
-    /// Indicator of whether the bot requires an OAuth2 code grant.
-    pub bot_require_code_grant: bool,
-    /// A description of the application, assigned by the application owner.
-    pub description: String,
-    /// A set of bitflags assigned to the application, which represent gated
-    /// feature flags that have been enabled for the application.
-    pub flags: Option<u64>,
-    /// A hash pointing to the application's icon.
-    ///
-    /// This is not necessarily equivalent to the bot user's avatar.
-    pub icon: Option<String>,
-    /// The unique numeric Id of the application.
-    pub id: UserId,
-    /// The name assigned to the application by the application owner.
-    pub name: String,
-    /// A list of redirect URIs assigned to the application.
-    pub redirect_uris: Vec<String>,
-    /// A list of RPC Origins assigned to the application.
-    pub rpc_origins: Vec<String>,
-    /// The application team group.
-    pub team: Option<Vec<Team>>,
-    /// The given secret to the application.
-    ///
-    /// This is not equivalent to the application's bot user's token.
-    pub secret: String,
-}
-
-impl fmt::Debug for ApplicationInfo {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("ApplicationInfo")
-            .field("bot", &self.bot)
-            .field("bot_public", &self.bot_public)
-            .field("bot_require_code_grant", &self.bot_require_code_grant)
-            .field("description", &self.description)
-            .field("flags", &self.flags)
-            .field("icon", &self.icon)
-            .field("id", &self.id)
-            .field("name", &self.name)
-            .field("redirect_uris", &self.redirect_uris)
-            .field("rpc_origins", &self.rpc_origins)
-            .field("team", &self.team)
-            .finish()
-    }
-}
-
-/// Information about an application with an application's bot user.
-#[derive(Clone, Deserialize, Serialize)]
-#[non_exhaustive]
-pub struct BotApplication {
-    /// The unique Id of the bot user.
-    pub id: UserId,
-    /// A hash of the avatar, if one is assigned.
-    ///
-    /// Can be used to generate a full URL to the avatar.
-    pub avatar: Option<String>,
-    /// Indicator of whether it is a bot.
-    #[serde(default)]
-    pub bot: bool,
-    /// The discriminator assigned to the bot user.
-    ///
-    /// While discriminators are not unique, the `username#discriminator` pair
-    /// is.
-    pub discriminator: u16,
-    /// The bot user's username.
-    pub name: String,
-    /// The token used to authenticate as the bot user.
-    ///
-    /// **Note**: Keep this information private, as untrusted sources can use it
-    /// to perform any action with a bot user.
-    pub token: String,
-}
-
-impl fmt::Debug for BotApplication {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("BotApplication")
-            .field("id", &self.id)
-            .field("avatar", &self.avatar)
-            .field("bot", &self.bot)
-            .field("discriminator", &self.discriminator)
-            .field("name", &self.name)
-            .finish()
-    }
-}
+use super::{
+    id::{ApplicationId, UserId},
+    user::User,
+    utils::*,
+};
 
 /// Partial information about the given application.
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct PartialCurrentApplicationInfo {
     /// The unique Id of the user.
-    pub id: UserId,
+    pub id: ApplicationId,
     /// The flags associated with the application.
     ///
     /// These flags are unknown and are not yet documented in the Discord API
@@ -122,7 +24,7 @@ pub struct PartialCurrentApplicationInfo {
 pub struct CurrentApplicationInfo {
     pub description: String,
     pub icon: Option<String>,
-    pub id: UserId,
+    pub id: ApplicationId,
     pub name: String,
     pub owner: User,
     #[serde(default)]
