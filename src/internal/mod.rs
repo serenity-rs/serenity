1f81ed6e4a32c8a9902600eaaf8cf8e4603eb9b3
Erk <Erk-@users.noreply.github.com>
2021-09-20 19:53:40 +0200

    Add support for transport compression in the gateway (#1508)

--- src/internal/mod.rs
+++ src/internal/mod.rs
@@ -8,3 +8,9 @@ pub mod ws_impl;
 
 #[cfg(any(feature = "tokio", feature = "tokio_compat"))]
 pub mod tokio;
+
+#[cfg(feature = "transport_compression")]
+mod inflater;
+
+#[cfg(feature = "transport_compression")]
+pub(crate) use inflater::Inflater;
