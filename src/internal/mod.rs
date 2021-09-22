08b44d7fd2a242254d990750579ab63c33b471ee
Alex M. M <acdenissk69@gmail.com>
2021-09-22 19:30:27 +0200

    Revert "Add support for transport compression in the gateway (#1508)"
    This reverts commit a2e899f0d4cfe89ed484d580c5ae8463d70a8c6e
    
    The commit broke the gateway, preventing it from handling events
    properly, such as the `READY` event not emitting. Hence, it is being
    reverted.

--- src/internal/mod.rs
+++ src/internal/mod.rs
@@ -8,9 +8,3 @@ pub mod ws_impl;
 
 #[cfg(any(feature = "tokio", feature = "tokio_compat"))]
 pub mod tokio;
-
-#[cfg(feature = "transport_compression")]
-mod inflater;
-
-#[cfg(feature = "transport_compression")]
-pub(crate) use inflater::Inflater;
