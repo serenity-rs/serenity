5dafadd5c548f3168b62b1ba64afb5408dc20d27
Alex M. M <acdenissk69@gmail.com>
2021-09-22 19:30:27 +0200

    Revert "Add support for transport compression in the gateway (#1508)"
    This reverts commit a2e899f0d4cfe89ed484d580c5ae8463d70a8c6e
    
    The commit broke the gateway, preventing it from handling events
    properly, such as the `READY` event not emitting. Hence, it is being
    reverted.

--- src/internal/mod.rs
+++ src/internal/mod.rs
@@ -5,9 +5,3 @@ pub mod prelude;
 
 #[cfg(feature = "gateway")]
 pub mod ws_impl;
-
-#[cfg(feature = "transport_compression")]
-mod inflater;
-
-#[cfg(feature = "transport_compression")]
-pub(crate) use inflater::Inflater;
