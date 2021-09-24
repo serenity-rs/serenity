5eff72333ac8e5c6e8ff118d2b5b5568246c8373
Jared L <48422312+lhjt@users.noreply.github.com>
2021-09-25 06:02:55 +1000

    Create `edit_thread` method
    This commit adds the `edit_thread` method to the `client`, `ChannelId`, and `GuildChannel`.

--- src/http/client.rs
+++ src/http/client.rs
@@ -406,6 +406,20 @@ impl Http {
         .await
     }
 
+    /// Edits a thread channel in the [`GuildChannel`] given its Id.
+    pub async fn edit_thread(&self, channel_id: u64, map: &JsonMap) -> Result<GuildChannel> {
+        let body = serde_json::to_vec(map)?;
+
+        self.fire(Request {
+            body: Some(&body),
+            headers: None,
+            route: RouteInfo::EditThread {
+                channel_id,
+            },
+        })
+        .await
+    }
+
     /// Creates a private thread channel in the [`GuildChannel`] given its Id.
     pub async fn create_private_thread(
         &self,
