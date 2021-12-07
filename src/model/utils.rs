f0c03d8b6d0c7b37c93080f32758655c3b7b7c3e
vicky5124 <vickyf5124@gmail.com>
2021-12-07 18:24:57 +0100

    Remove unused import 	modified:   src/model/utils.rs

--- src/model/utils.rs
+++ src/model/utils.rs
@@ -2,8 +2,6 @@ use std::marker::PhantomData;
 use std::{collections::HashMap, hash::Hash};
 
 use serde::ser::{Serialize, SerializeSeq, Serializer};
-#[cfg(feature = "simd-json")]
-use simd_json::ValueAccess;
 
 #[cfg(all(feature = "cache", feature = "model"))]
 use super::permissions::Permissions;
