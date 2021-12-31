5583484518e1b3ed64d9a59162ca4524903a3719
nickelc <constantin.nickel@gmail.com>
2021-12-31 05:31:06 +0100

    Remove superfluous link reference definitions (#1645)
    These are either unused or covered by Rust's intra-doc links.

--- src/model/guild/mod.rs
+++ src/model/guild/mod.rs
@@ -1293,7 +1293,6 @@ impl Guild {
     /// Returns an [`Error::Http`] if some mandatory fields are not provided.
     ///
     /// [`Error::Http`]: crate::error::Error::Http
-    /// [`GuildWelcomeScreen`]: super::guild::GuildWelcomeScreen
     pub async fn edit_welcome_screen<F>(
         &self,
         http: impl AsRef<Http>,
@@ -1313,7 +1312,6 @@ impl Guild {
     /// permission.
     ///
     /// [`Error::Http`]: crate::error::Error::Http
-    /// [`GuildWelcomeScreen`]: super::guild::GuildWelcomeScreen
     pub async fn edit_widget<F>(&self, http: impl AsRef<Http>, f: F) -> Result<GuildWidget>
     where
         F: FnOnce(&mut EditGuildWidget) -> &mut EditGuildWidget,
