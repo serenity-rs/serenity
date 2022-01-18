701d7593a1d88ef9e1987efdd6024ae5336115f1
nickelc <constantin.nickel@gmail.com>
2022-01-18 17:51:03 +0100

    Use the `bitflags!` macro instead of the undocumented macro (#1662)
    The use of the `__impl_bitflags` macro was introduced at the time to help
    language servers to resolve types for autocompletion. Much has changed
    since then, `RLS` has been superseded by `rust-analyzer` and
    `rust-analyzer` supports `local_inner_macros`.
    
    BREAKING CHANGE: The `bits` field of the bitflags is now private. Use
    `T::bits()` or `T::from_bits_truncate()` instead.

--- src/model/guild/system_channel.rs
+++ src/model/guild/system_channel.rs
@@ -1,21 +1,17 @@
-use bitflags::__impl_bitflags;
+use bitflags::bitflags;
 
-/// Describes a system channel flags.
-#[derive(Copy, PartialEq, Eq, Clone, PartialOrd, Ord, Hash, Default)]
-pub struct SystemChannelFlags {
-    pub bits: u64,
-}
-
-__impl_bitflags! {
-    SystemChannelFlags: u64 {
+bitflags! {
+    /// Describes a system channel flags.
+    #[derive(Default)]
+    pub struct SystemChannelFlags: u64 {
         /// Suppress member join notifications.
-        SUPPRESS_JOIN_NOTIFICATIONS = 1 << 0;
+        const SUPPRESS_JOIN_NOTIFICATIONS = 1 << 0;
         /// Suppress server boost notifications.
-        SUPPRESS_PREMIUM_SUBSCRIPTIONS = 1 << 1;
+        const SUPPRESS_PREMIUM_SUBSCRIPTIONS = 1 << 1;
         /// Suppress server setup tips.
-        SUPPRESS_GUILD_REMINDER_NOTIFICATIONS = 1 << 2;
+        const SUPPRESS_GUILD_REMINDER_NOTIFICATIONS = 1 << 2;
         /// Hide member join sticker reply buttons.
-        SUPPRESS_JOIN_NOTIFICATION_REPLIES = 1 << 3;
+        const SUPPRESS_JOIN_NOTIFICATION_REPLIES = 1 << 3;
     }
 }
 
