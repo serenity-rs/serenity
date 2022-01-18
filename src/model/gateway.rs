6df523ec9df746ce9a75d9ce6f91a96bb74627a2
nickelc <constantin.nickel@gmail.com>
2022-01-18 17:51:03 +0100

    Use the `bitflags!` macro instead of the undocumented macro (#1662)
    The use of the `__impl_bitflags` macro was introduced at the time to help
    language servers to resolve types for autocompletion. Much has changed
    since then, `RLS` has been superseded by `rust-analyzer` and
    `rust-analyzer` supports `local_inner_macros`.
    
    BREAKING CHANGE: The `bits` field of the bitflags is now private. Use
    `T::bits()` or `T::from_bits_truncate()` instead.

--- src/model/gateway.rs
+++ src/model/gateway.rs
@@ -1,9 +1,6 @@
 //! Models pertaining to the gateway.
 
-// TODO: Remove after `GuildStatus` was replaced
-#![allow(deprecated)]
-
-use bitflags::{__impl_bitflags, bitflags};
+use bitflags::bitflags;
 use url::Url;
 
 use super::prelude::*;
@@ -529,43 +526,33 @@ pub struct ActivityTimestamps {
     pub start: Option<u64>,
 }
 
-/// [Gateway Intents] will limit the events your bot will receive via the gateway.
-/// By default, all intents except [Privileged Intents] are selected.
-///
-/// # What are Intents
-///
-/// A [gateway intent] sets the types of gateway events
-/// (e.g. member joins, guild integrations, guild emoji updates, ...) the
-/// bot shall receive. Carefully picking the needed intents greatly helps
-/// the bot to scale, as less intents will result in less events to be
-/// received via the network from Discord and less processing needed for
-/// handling the data.
-///
-/// # Privileged Intents
-///
-/// The intents [`GatewayIntents::GUILD_PRESENCES`] and [`GatewayIntents::GUILD_MEMBERS`]
-/// are [Privileged Intents]. They need to be enabled in the
-/// *developer portal*.
-///
-/// **Note**:
-/// Once the bot is in 100 guilds or more, [the bot must be verified] in
-/// order to use privileged intents.
-///
-/// [gateway intent]: https://discord.com/developers/docs/topics/gateway#privileged-intents
-/// [Privileged Intents]: https://discord.com/developers/docs/topics/gateway#privileged-intents
-/// [the bot must be verified]: https://support.discord.com/hc/en-us/articles/360040720412-Bot-Verification-and-Data-Whitelisting
-#[derive(Copy, PartialEq, Eq, Clone, PartialOrd, Ord, Hash)]
-pub struct GatewayIntents {
-    /// The flags composing gateway intents.
+bitflags! {
+    /// [Gateway Intents] will limit the events your bot will receive via the gateway.
+    /// By default, all intents except [Privileged Intents] are selected.
     ///
-    /// # Note
-    /// Do not modify this yourself; use the provided methods.
-    /// Do the same when creating, unless you're absolutely certain that you're giving valid intents flags.
-    pub bits: u64,
-}
-
-__impl_bitflags! {
-    GatewayIntents: u64 {
+    /// # What are Intents
+    ///
+    /// A [gateway intent] sets the types of gateway events
+    /// (e.g. member joins, guild integrations, guild emoji updates, ...) the
+    /// bot shall receive. Carefully picking the needed intents greatly helps
+    /// the bot to scale, as less intents will result in less events to be
+    /// received via the network from Discord and less processing needed for
+    /// handling the data.
+    ///
+    /// # Privileged Intents
+    ///
+    /// The intents [`GatewayIntents::GUILD_PRESENCES`] and [`GatewayIntents::GUILD_MEMBERS`]
+    /// are [Privileged Intents]. They need to be enabled in the
+    /// *developer portal*.
+    ///
+    /// **Note**:
+    /// Once the bot is in 100 guilds or more, [the bot must be verified] in
+    /// order to use privileged intents.
+    ///
+    /// [gateway intent]: https://discord.com/developers/docs/topics/gateway#privileged-intents
+    /// [Privileged Intents]: https://discord.com/developers/docs/topics/gateway#privileged-intents
+    /// [the bot must be verified]: https://support.discord.com/hc/en-us/articles/360040720412-Bot-Verification-and-Data-Whitelisting
+    pub struct GatewayIntents: u64 {
         /// Enables following gateway events:
         ///
         /// - GUILD_CREATE
@@ -586,7 +573,7 @@ __impl_bitflags! {
         /// - STAGE_INSTANCE_CREATE
         /// - STAGE_INSTANCE_UPDATE
         /// - STAGE_INSTANCE_DELETE
-        GUILDS = 1;
+        const GUILDS = 1;
         /// Enables following gateway events:
         ///
         /// - GUILD_MEMBER_ADD
@@ -600,37 +587,37 @@ __impl_bitflags! {
         /// Developer Portal and enable the toggle for *Privileged Intents*.
         ///
         /// This intent is also necessary to even receive the events in contains.
-        GUILD_MEMBERS = 1 << 1;
+        const GUILD_MEMBERS = 1 << 1;
         /// Enables following gateway events:
         ///
         /// - GUILD_BAN_ADD
         /// - GUILD_BAN_REMOVE
-        GUILD_BANS = 1 << 2;
+        const GUILD_BANS = 1 << 2;
         /// Enables following gateway event:
         ///
         /// - GUILD_EMOJIS_UPDATE
         /// - GUILD_STICKERS_UPDATE
-        GUILD_EMOJIS_AND_STICKERS = 1 << 3;
+        const GUILD_EMOJIS_AND_STICKERS = 1 << 3;
         /// Enables following gateway event:
         ///
         /// - GUILD_INTEGRATIONS_UPDATE
         /// - INTEGRATION_CREATE
         /// - INTEGRATION_UPDATE
         /// - INTEGRATION_DELETE
-        GUILD_INTEGRATIONS = 1 << 4;
+        const GUILD_INTEGRATIONS = 1 << 4;
         /// Enables following gateway event:
         ///
         /// - WEBHOOKS_UPDATE
-        GUILD_WEBHOOKS = 1 << 5;
+        const GUILD_WEBHOOKS = 1 << 5;
         /// Enables following gateway events:
         ///
         /// - INVITE_CREATE
         /// - INVITE_DELETE
-        GUILD_INVITES = 1 << 6;
+        const GUILD_INVITES = 1 << 6;
         /// Enables following gateway event:
         ///
         /// - VOICE_STATE_UPDATE
-        GUILD_VOICE_STATES = 1 << 7;
+        const GUILD_VOICE_STATES = 1 << 7;
         /// Enables following gateway event:
         ///
         /// - PRESENCE_UPDATE
@@ -641,43 +628,43 @@ __impl_bitflags! {
         /// Developer Portal and enable the toggle for *Privileged Intents*.
         ///
         /// This intent is also necessary to even receive the events in contains.
-        GUILD_PRESENCES = 1 << 8;
+        const GUILD_PRESENCES = 1 << 8;
         /// Enables following gateway events:
         ///
         /// - MESSAGE_CREATE
         /// - MESSAGE_UPDATE
         /// - MESSAGE_DELETE
         /// - MESSAGE_DELETE_BULK
-        GUILD_MESSAGES = 1 << 9;
+        const GUILD_MESSAGES = 1 << 9;
         /// Enables following gateway events:
         ///
         /// - MESSAGE_REACTION_ADD
         /// - MESSAGE_REACTION_REMOVE
         /// - MESSAGE_REACTION_REMOVE_ALL
         /// - MESSAGE_REACTION_REMOVE_EMOJI
-        GUILD_MESSAGE_REACTIONS = 1 << 10;
+        const GUILD_MESSAGE_REACTIONS = 1 << 10;
         /// Enable following gateway event:
         ///
         /// - TYPING_START
-        GUILD_MESSAGE_TYPING = 1 << 11;
+        const GUILD_MESSAGE_TYPING = 1 << 11;
         /// Enable following gateway events:
         ///
         /// - MESSAGE_CREATE
         /// - MESSAGE_UPDATE
         /// - MESSAGE_DELETE
         /// - CHANNEL_PINS_UPDATE
-        DIRECT_MESSAGES = 1 << 12;
+        const DIRECT_MESSAGES = 1 << 12;
         /// Enable following gateway events:
         ///
         /// - MESSAGE_REACTION_ADD
         /// - MESSAGE_REACTION_REMOVE
         /// - MESSAGE_REACTION_REMOVE_ALL
         /// - MESSAGE_REACTION_REMOVE_EMOJI
-        DIRECT_MESSAGE_REACTIONS = 1 << 13;
+        const DIRECT_MESSAGE_REACTIONS = 1 << 13;
         /// Enable following gateway event:
         ///
         /// - TYPING_START
-        DIRECT_MESSAGE_TYPING = 1 << 14;
+        const DIRECT_MESSAGE_TYPING = 1 << 14;
     }
 }
 
