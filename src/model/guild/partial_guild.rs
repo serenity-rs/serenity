7f76e3bd77ed32e5e8d4ad235af27ee07f32c7ec
Constantin Nickel <constantin.nickel@gmail.com>
2022-04-01 18:30:36 +0200

    Make the non cache version of the `shard_id` methods synchronous
    This was overlooked when the cache was made synchronous.

--- src/model/guild/partial_guild.rs
+++ src/model/guild/partial_guild.rs
@@ -1424,8 +1424,8 @@ impl PartialGuild {
     /// ```
     #[cfg(all(feature = "utils", not(feature = "cache")))]
     #[inline]
-    pub async fn shard_id(&self, shard_count: u64) -> u64 {
-        self.id.shard_id(shard_count).await
+    pub fn shard_id(&self, shard_count: u64) -> u64 {
+        self.id.shard_id(shard_count)
     }
 
     /// Returns the formatted URL of the guild's splash image, if one exists.
--- src/model/invite.rs
+++ src/model/invite.rs
@@ -280,8 +280,8 @@ impl InviteGuild {
     /// ```
     #[cfg(all(feature = "utils", not(feature = "cache")))]
     #[inline]
-    pub async fn shard_id(&self, shard_count: u64) -> u64 {
-        self.id.shard_id(shard_count).await
+    pub fn shard_id(&self, shard_count: u64) -> u64 {
+        self.id.shard_id(shard_count)
     }
 }
 
