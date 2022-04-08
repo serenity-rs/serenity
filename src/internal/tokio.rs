00677552480dd4e60e85eba19a79386053ff0f41
Constantin Nickel <constantin.nickel@gmail.com>
2022-04-08 18:24:42 +0200

    fixup! update spawn_named

--- src/internal/tokio.rs
+++ src/internal/tokio.rs
@@ -1,6 +1,6 @@
 use std::future::Future;
 
-#[cfg(all(tokio_unstable, not(feature = "tokio_compat"), feature = "tokio_task_builder"))]
+#[cfg(all(tokio_unstable, feature = "tokio_task_builder", not(feature = "tokio_compat")))]
 pub fn spawn_named<F, T>(name: &str, future: F) -> tokio::task::JoinHandle<T>
 where
     F: Future<Output = T> + Send + 'static,
@@ -9,7 +9,7 @@ where
     tokio::task::Builder::new().name(&*format!("serenity::{}", name)).spawn(future)
 }
 
-#[cfg(any(not(tokio_unstable), feature = "tokio_compat"))]
+#[cfg(any(not(all(tokio_unstable, feature = "tokio_task_builder")), feature = "tokio_compat"))]
 pub fn spawn_named<F, T>(_name: &str, future: F) -> tokio::task::JoinHandle<T>
 where
     F: Future<Output = T> + Send + 'static,
