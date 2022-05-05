225d5a2df857c4de152155792d2184f797cccfde
nickelc <constantin.nickel@gmail.com>
2022-05-05 22:27:20 +0200

    Replace `gateway::WsStream` & extension traits with a newtype struct (#1893)
    The newtype struct `gateway::WsClient` hides the WebSocket
    implementation details and is the first step to remove the
    `async_tungstenite` & `tungstenite` types from the public API.
    
    The `WebSocketGatewayClientExt`, `ReceiverExt` and `SenderExt` trait
    methods are merged into the implementation of the newtype struct.
    
    BREAKING CHANGES:
    - `gateway::WsStream` is replaced by `gateway::WsClient`.
    - The `gateway::WebSocketGatewayClientExt` trait is removed.

--- src/gateway/ws.rs
+++ src/gateway/ws.rs
@@ -1,57 +1,107 @@
 use std::env::consts;
+use std::io::Read;
 use std::time::SystemTime;
 
-use async_trait::async_trait;
-use tracing::{debug, instrument, trace};
+use async_tungstenite::tokio::{connect_async_with_config, ConnectStream};
+use async_tungstenite::tungstenite::protocol::{CloseFrame, WebSocketConfig};
+use async_tungstenite::tungstenite::{Error as WsError, Message};
+use async_tungstenite::WebSocketStream;
+use flate2::read::ZlibDecoder;
+use futures::{SinkExt, StreamExt};
+use tokio::time::{timeout, Duration};
+use tracing::{debug, instrument, trace, warn};
+use url::Url;
 
 use crate::client::bridge::gateway::ChunkGuildFilter;
 use crate::constants::{self, OpCode};
-use crate::gateway::{CurrentPresence, WsStream};
-use crate::internal::prelude::*;
-use crate::internal::ws_impl::SenderExt;
-use crate::json::json;
+use crate::gateway::{CurrentPresence, GatewayError};
+use crate::json::{from_str, json, to_string, Value};
 use crate::model::gateway::GatewayIntents;
 use crate::model::id::GuildId;
+use crate::{Error, Result};
 
-#[async_trait]
-pub trait WebSocketGatewayClientExt {
-    async fn send_chunk_guild(
-        &mut self,
-        guild_id: GuildId,
-        shard_info: &[u64; 2],
-        limit: Option<u16>,
-        filter: ChunkGuildFilter,
-        nonce: Option<&str>,
-    ) -> Result<()>;
+pub struct WsClient(WebSocketStream<ConnectStream>);
 
-    async fn send_heartbeat(&mut self, shard_info: &[u64; 2], seq: Option<u64>) -> Result<()>;
+const TIMEOUT: Duration = Duration::from_millis(500);
+const DECOMPRESSION_MULTIPLIER: usize = 3;
 
-    async fn send_identify(
-        &mut self,
-        shard_info: &[u64; 2],
-        token: &str,
-        intents: GatewayIntents,
-    ) -> Result<()>;
+impl WsClient {
+    pub(crate) async fn connect(url: Url) -> Result<Self> {
+        let config = WebSocketConfig {
+            max_message_size: None,
+            max_frame_size: None,
+            max_send_queue: None,
+            accept_unmasked_frames: false,
+        };
+        let (stream, _) = connect_async_with_config(url, Some(config)).await?;
 
-    async fn send_presence_update(
-        &mut self,
-        shard_info: &[u64; 2],
-        current_presence: &CurrentPresence,
-    ) -> Result<()>;
+        Ok(Self(stream))
+    }
 
-    async fn send_resume(
-        &mut self,
-        shard_info: &[u64; 2],
-        session_id: &str,
-        seq: u64,
-        token: &str,
-    ) -> Result<()>;
-}
+    pub(crate) async fn recv_json(&mut self) -> Result<Option<Value>> {
+        let message = match timeout(TIMEOUT, self.0.next()).await {
+            Ok(Some(Ok(msg))) => msg,
+            Ok(Some(Err(e))) => return Err(e.into()),
+            Ok(None) | Err(_) => return Ok(None),
+        };
 
-#[async_trait]
-impl WebSocketGatewayClientExt for WsStream {
-    #[instrument(skip(self))]
-    async fn send_chunk_guild(
+        let value = match message {
+            Message::Binary(bytes) => {
+                let mut decompressed =
+                    String::with_capacity(bytes.len() * DECOMPRESSION_MULTIPLIER);
+
+                ZlibDecoder::new(&bytes[..]).read_to_string(&mut decompressed).map_err(|why| {
+                    warn!("Err decompressing bytes: {:?}; bytes: {:?}", why, bytes);
+
+                    why
+                })?;
+
+                from_str(decompressed.as_mut_str()).map_err(|why| {
+                    warn!("Err deserializing bytes: {:?}; bytes: {:?}", why, bytes);
+
+                    why
+                })?
+            },
+            Message::Text(mut payload) => from_str(&mut payload).map_err(|why| {
+                warn!("Err deserializing text: {:?}; text: {}", why, payload);
+
+                why
+            })?,
+            Message::Close(Some(frame)) => {
+                return Err(Error::Gateway(GatewayError::Closed(Some(frame))));
+            },
+            _ => return Ok(None),
+        };
+
+        Ok(Some(value))
+    }
+
+    pub(crate) async fn send_json(&mut self, value: &Value) -> Result<()> {
+        let message = to_string(value).map(Message::Text)?;
+
+        self.0.send(message).await?;
+        Ok(())
+    }
+
+    /// Delegate to `StreamExt::next`
+    pub(crate) async fn next(&mut self) -> Option<std::result::Result<Message, WsError>> {
+        self.0.next().await
+    }
+
+    /// Delegate to `SinkExt::send`
+    pub(crate) async fn send(&mut self, message: Message) -> Result<()> {
+        self.0.send(message).await?;
+        Ok(())
+    }
+
+    /// Delegate to `WebSocketStream::close`
+    pub(crate) async fn close(&mut self, msg: Option<CloseFrame<'_>>) -> Result<()> {
+        self.0.close(msg).await?;
+        Ok(())
+    }
+
+    #[allow(clippy::missing_errors_doc)]
+    pub async fn send_chunk_guild(
         &mut self,
         guild_id: GuildId,
         shard_info: &[u64; 2],
@@ -83,7 +133,7 @@ impl WebSocketGatewayClientExt for WsStream {
     }
 
     #[instrument(skip(self))]
-    async fn send_heartbeat(&mut self, shard_info: &[u64; 2], seq: Option<u64>) -> Result<()> {
+    pub async fn send_heartbeat(&mut self, shard_info: &[u64; 2], seq: Option<u64>) -> Result<()> {
         trace!("[Shard {:?}] Sending heartbeat d: {:?}", shard_info, seq);
 
         self.send_json(&json!({
@@ -95,7 +145,7 @@ impl WebSocketGatewayClientExt for WsStream {
     }
 
     #[instrument(skip(self, token))]
-    async fn send_identify(
+    pub async fn send_identify(
         &mut self,
         shard_info: &[u64; 2],
         token: &str,
@@ -123,7 +173,7 @@ impl WebSocketGatewayClientExt for WsStream {
     }
 
     #[instrument(skip(self))]
-    async fn send_presence_update(
+    pub async fn send_presence_update(
         &mut self,
         shard_info: &[u64; 2],
         current_presence: &CurrentPresence,
@@ -150,7 +200,7 @@ impl WebSocketGatewayClientExt for WsStream {
     }
 
     #[instrument(skip(self, token))]
-    async fn send_resume(
+    pub async fn send_resume(
         &mut self,
         shard_info: &[u64; 2],
         session_id: &str,
