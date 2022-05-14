0a97c123b3f2164eded3a91b5ed65cbde3cb9e10
Gnome! <45660393+GnomedDev@users.noreply.github.com>
2022-05-14 13:53:07 +0100

    Switch to `Into<String>` when accepting string parameters (#1903)
    This changes instances of `ToString` to `Into<String>` whenever ownership of
    the `String` is necessary. In case it is not, it's simply changed to `&str` to
    lessen the effects of monomorphization on compile times.

--- src/model/channel/message.rs
+++ src/model/channel/message.rs
@@ -651,7 +651,7 @@ impl Message {
     pub async fn reply(
         &self,
         cache_http: impl CacheHttp,
-        content: impl Display,
+        content: impl Into<String>,
     ) -> Result<Message> {
         self._reply(cache_http, content, Some(false)).await
     }
@@ -677,7 +677,7 @@ impl Message {
     pub async fn reply_ping(
         &self,
         cache_http: impl CacheHttp,
-        content: impl Display,
+        content: impl Into<String>,
     ) -> Result<Message> {
         self._reply(cache_http, content, Some(true)).await
     }
@@ -715,7 +715,7 @@ impl Message {
     async fn _reply(
         &self,
         cache_http: impl CacheHttp,
-        content: impl Display,
+        content: impl Into<String>,
         inlined: Option<bool>,
     ) -> Result<Message> {
         #[cfg(feature = "cache")]
