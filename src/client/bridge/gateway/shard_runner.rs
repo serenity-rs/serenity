a23824336086acaaf42cfa77a1357c65403d26d6
Jan <66554238+vaporox@users.noreply.github.com>
2022-05-24 00:52:57 +0200

    Allow having no framework when the `framework` feature is enabled (#1908)

--- src/client/bridge/gateway/shard_runner.rs
+++ src/client/bridge/gateway/shard_runner.rs
@@ -41,7 +41,7 @@ pub struct ShardRunner {
     event_handler: Option<Arc<dyn EventHandler>>,
     raw_event_handler: Option<Arc<dyn RawEventHandler>>,
     #[cfg(feature = "framework")]
-    framework: Arc<dyn Framework + Send + Sync>,
+    framework: Option<Arc<dyn Framework + Send + Sync>>,
     manager_tx: Sender<ShardManagerMessage>,
     // channel to receive messages from the shard manager and dispatches
     runner_rx: Receiver<InterMessage>,
@@ -689,7 +689,7 @@ pub struct ShardRunnerOptions {
     pub event_handler: Option<Arc<dyn EventHandler>>,
     pub raw_event_handler: Option<Arc<dyn RawEventHandler>>,
     #[cfg(feature = "framework")]
-    pub framework: Arc<dyn Framework + Send + Sync>,
+    pub framework: Option<Arc<dyn Framework + Send + Sync>>,
     pub manager_tx: Sender<ShardManagerMessage>,
     pub shard: Shard,
     #[cfg(feature = "voice")]
