f97322475eadf2dc78ea3e6d31b55811b3eb50ba
kangalioo <jannik.a.schaper@web.de>
2022-06-03 02:18:45 +0200

    Add support for slash command localization (#1921)

--- examples/e14_slash_commands/src/main.rs
+++ examples/e14_slash_commands/src/main.rs
@@ -14,6 +14,8 @@ struct Handler;
 impl EventHandler for Handler {
     async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
         if let Interaction::ApplicationCommand(command) = interaction {
+            println!("Received command interaction: {:#?}", command);
+
             let content = match command.data.name.as_str() {
                 "ping" => "Hey, I'm alive!".to_string(),
                 "id" => {
@@ -94,32 +96,45 @@ impl EventHandler for Handler {
                 .create_application_command(|command| {
                     command
                         .name("welcome")
+                        .name_localized("de", "begrüßen")
                         .description("Welcome a user")
+                        .description_localized("de", "Einen Nutzer begrüßen")
                         .create_option(|option| {
                             option
                                 .name("user")
+                                .name_localized("de", "nutzer")
                                 .description("The user to welcome")
+                                .description_localized("de", "Der zu begrüßende Nutzer")
                                 .kind(CommandOptionType::User)
                                 .required(true)
                         })
                         .create_option(|option| {
                             option
                                 .name("message")
+                                .name_localized("de", "nachricht")
                                 .description("The message to send")
+                                .description_localized("de", "Die versendete Nachricht")
                                 .kind(CommandOptionType::String)
                                 .required(true)
-                                .add_string_choice(
+                                .add_string_choice_localized(
                                     "Welcome to our cool server! Ask me if you need help",
                                     "pizza",
+                                    [("de", "Willkommen auf unserem coolen Server! Frag mich, falls du Hilfe brauchst")]
                                 )
-                                .add_string_choice("Hey, do you want a coffee?", "coffee")
-                                .add_string_choice(
+                                .add_string_choice_localized(
+                                    "Hey, do you want a coffee?",
+                                    "coffee",
+                                    [("de", "Hey, willst du einen Kaffee?")],
+                                )
+                                .add_string_choice_localized(
                                     "Welcome to the club, you're now a good person. Well, I hope.",
                                     "club",
+                                    [("de", "Willkommen im Club, du bist jetzt ein guter Mensch. Naja, hoffentlich.")],
                                 )
-                                .add_string_choice(
+                                .add_string_choice_localized(
                                     "I hope that you brought a controller to play together!",
                                     "game",
+                                    [("de", "Ich hoffe du hast einen Controller zum Spielen mitgebracht!")],
                                 )
                         })
                 })
