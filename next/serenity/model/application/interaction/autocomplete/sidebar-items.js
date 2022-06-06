56e0df95a72ceded18afde72dccf06e55a88514f
acdenisSK <acdenisSK@users.noreply.github.com>
2022-06-06 14:20:45 +0000

    deploy: f1048ca2d9c8106abbd8ba0101d83c7049d3e62c

--- next/serenity/model/application/interaction/autocomplete/sidebar-items.js
+++ next/serenity/model/application/interaction/autocomplete/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["AutocompleteInteraction","An interaction received when the user fills in an autocomplete option"]]});
+initSidebarItems({"enum":[["CommandDataOptionValue","The value of an [`CommandDataOption`]."]],"struct":[["AutocompleteData","The autocomplete data payload."],["AutocompleteInteraction","An interaction received when the user fills in an autocomplete option"],["CommandDataOption","A set of a parameter and a value from the user."],["FocusedOption","The focused option return by [`AutocompleteData::focused_option`]."]]});
