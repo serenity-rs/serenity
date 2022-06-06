serenity-rs/serenity

commit f97322475eadf2dc78ea3e6d31b55811b3eb50ba

Add support for slash command localization (#1921)

src/builder/create_application_command.rs
@@ -32,11 +32,51 @@ impl CreateApplicationCommandOption {
         self
     }
 
+    /// Specifies a localized name of the option.
+    ///
+    /// ```rust
+    /// # serenity::builder::CreateApplicationCommandOption::default()
+    /// .name("age")
+    /// .name_localized("zh-CN", "岁数")
+    /// # ;
+    /// ```
+    pub fn name_localized<D: ToString, E: ToString>(&mut self, locale: E, name: D) -> &mut Self {
+        self.0
+            .entry("name_localizations")
+            .or_insert(Value::Object(Default::default()))
+            .as_object_mut()
+            .expect("must be object")
+            .insert(locale.to_string(), Value::String(name.to_string()));
+        self
+    }
+
     /// Sets the description for the option.
     ///
     /// **Note**: Must be between 1 and 100 characters.
     pub fn description<D: ToString>(&mut self, description: D) -> &mut Self {
         self.0.insert("description", Value::from(description.to_string()));
+        self
+    }
+
+    /// Specifies a localized description of the option.
+    ///
+    /// ```rust
+    /// # serenity::builder::CreateApplicationCommandOption::default()
+    /// .description("Wish a friend a happy birthday")
+    /// .description_localized("zh-CN", "祝你朋友生日快乐")
+    /// # ;
+    /// ```
+    pub fn description_localized<D: ToString, E: ToString>(
+        &mut self,
+        locale: E,
+        description: D,
+    ) -> &mut Self {
+        self.0
+            .entry("description_localizations")
+            .or_insert(Value::Object(Default::default()))
+            .as_object_mut()
+            .expect("must be object")
+            .insert(locale.to_string(), Value::String(description.to_string()));
         self
     }
 
@@ -67,6 +107,24 @@ impl CreateApplicationCommandOption {
         self.add_choice(choice)
     }
 
+    /// Adds a localized optional int-choice. See [`Self::add_int_choice`] for more info.
+    pub fn add_int_choice_localized<L: ToString, D: ToString>(
+        &mut self,
+        name: D,
+        value: i32,
+        locales: impl IntoIterator<Item = (L, D)>,
+    ) -> &mut Self {
+        let choice = json!({
+            "name": name.to_string(),
+            "name_localizations": locales
+                .into_iter()
+                .map(|(locale, name)| (locale.to_string(), name.to_string()))
+                .collect::<Value>(),
+            "value" : value,
+        });
+        self.add_choice(choice)
+    }
+
     /// Adds an optional string-choice.
     ///
     /// **Note**: There can be no more than 25 choices set. Name must be between 1 and 100 characters. Value must be up to 100 characters.
@@ -78,6 +136,24 @@ impl CreateApplicationCommandOption {
         self.add_choice(choice)
     }
 
+    /// Adds a localized optional string-choice. See [`Self::add_string_choice`] for more info.
+    pub fn add_string_choice_localized<L: ToString, D: ToString, E: ToString>(
+        &mut self,
+        name: D,
+        value: E,
+        locales: impl IntoIterator<Item = (L, D)>,
+    ) -> &mut Self {
+        let choice = json!({
+            "name": name.to_string(),
+            "name_localizations": locales
+                .into_iter()
+                .map(|(locale, name)| (locale.to_string(), name.to_string()))
+                .collect::<Value>(),
+            "value": value.to_string(),
+        });
+        self.add_choice(choice)
+    }
+
     /// Adds an optional number-choice.
     ///
     /// **Note**: There can be no more than 25 choices set. Name must be between 1 and 100 characters. Value must be between -2^53 and 2^53.
@@ -85,6 +161,24 @@ impl CreateApplicationCommandOption {
         let choice = json!({
             "name": name.to_string(),
             "value" : value
+        });
+        self.add_choice(choice)
+    }
+
+    /// Adds a localized optional number-choice. See [`Self::add_number_choice`] for more info.
+    pub fn add_number_choice_localized<L: ToString, D: ToString>(
+        &mut self,
+        name: D,
+        value: f64,
+        locales: impl IntoIterator<Item = (L, D)>,
+    ) -> &mut Self {
+        let choice = json!({
+            "name": name.to_string(),
+            "name_localizations": locales
+                .into_iter()
+                .map(|(locale, name)| (locale.to_string(), name.to_string()))
+                .collect::<Value>(),
+            "value" : value,
         });
         self.add_choice(choice)
     }
@@ -196,6 +290,25 @@ impl CreateApplicationCommand {
         self
     }
 
+    /// Specifies a localized name of the application command.
+    ///
+    /// ```rust
+    /// # serenity::builder::CreateApplicationCommand::default()
+    /// .name("birthday")
+    /// .name_localized("zh-CN", "生日")
+    /// .name_localized("el", "γενέθλια")
+    /// # ;
+    /// ```
+    pub fn name_localized<D: ToString, E: ToString>(&mut self, locale: E, name: D) -> &mut Self {
+        self.0
+            .entry("name_localizations")
+            .or_insert(Value::Object(Default::default()))
+            .as_object_mut()
+            .expect("must be object")
+            .insert(locale.to_string(), Value::String(name.to_string()));
+        self
+    }
+
     /// Specifies the type of the application command.
     pub fn kind(&mut self, kind: CommandType) -> &mut Self {
         self.0.insert("type", from_number(kind as u8));
@@ -232,6 +345,28 @@ impl CreateApplicationCommand {
     /// **Note**: Must be between 1 and 100 characters long.
     pub fn description<D: ToString>(&mut self, description: D) -> &mut Self {
         self.0.insert("description", Value::from(description.to_string()));
+        self
+    }
+
+    /// Specifies a localized description of the application command.
+    ///
+    /// ```rust
+    /// # serenity::builder::CreateApplicationCommand::default()
+    /// .description("Wish a friend a happy birthday")
+    /// .description_localized("zh-CN", "祝你朋友生日快乐")
+    /// # ;
+    /// ```
+    pub fn description_localized<D: ToString, E: ToString>(
+        &mut self,
+        locale: E,
+        description: D,
+    ) -> &mut Self {
+        self.0
+            .entry("description_localizations")
+            .or_insert(Value::Object(Default::default()))
+            .as_object_mut()
+            .expect("must be object")
+            .insert(locale.to_string(), Value::String(description.to_string()));
         self
     }
 

examples/e14_slash_commands/src/main.rs
@@ -14,6 +14,8 @@ struct Handler;
 impl EventHandler for Handler {
     async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
         if let Interaction::ApplicationCommand(command) = interaction {
+            println!("Received command interaction: {:#?}", command);
+
             let content = match command.data.name.as_str() {
                 "ping" => "Hey, I'm alive!".to_string(),
                 "id" => {
@@ -94,32 +96,45 @@ impl EventHandler for Handler {
                 .create_application_command(|command| {
                     command
                         .name("welcome")
+                        .name_localized("de", "begrüßen")
                         .description("Welcome a user")
+                        .description_localized("de", "Einen Nutzer begrüßen")
                         .create_option(|option| {
                             option
                                 .name("user")
+                                .name_localized("de", "nutzer")
                                 .description("The user to welcome")
+                                .description_localized("de", "Der zu begrüßende Nutzer")
                                 .kind(CommandOptionType::User)
                                 .required(true)
                         })
                         .create_option(|option| {
                             option
                                 .name("message")
+                                .name_localized("de", "nachricht")
                                 .description("The message to send")
+                                .description_localized("de", "Die versendete Nachricht")
                                 .kind(CommandOptionType::String)
                                 .required(true)
-                                .add_string_choice(
+                                .add_string_choice_localized(
                                     "Welcome to our cool server! Ask me if you need help",
                                     "pizza",
+                                    [("de", "Willkommen auf unserem coolen Server! Frag mich, falls du Hilfe brauchst")]
                                 )
-                                .add_string_choice("Hey, do you want a coffee?", "coffee")
-                                .add_string_choice(
+                                .add_string_choice_localized(
+                                    "Hey, do you want a coffee?",
+                                    "coffee",
+                                    [("de", "Hey, willst du einen Kaffee?")],
+                                )
+                                .add_string_choice_localized(
                                     "Welcome to the club, you're now a good person. Well, I hope.",
                                     "club",
+                                    [("de", "Willkommen im Club, du bist jetzt ein guter Mensch. Naja, hoffentlich.")],
                                 )
-                                .add_string_choice(
+                                .add_string_choice_localized(
                                     "I hope that you brought a controller to play together!",
                                     "game",
+                                    [("de", "Ich hoffe du hast einen Controller zum Spielen mitgebracht!")],
                                 )
                         })
                 })

src/model/application/command.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 
 #[cfg(feature = "http")]
@@ -38,8 +40,32 @@ pub struct Command {
     pub guild_id: Option<GuildId>,
     /// The command name.
     pub name: String,
+    /// The localized command name of the selected locale.
+    ///
+    /// If the name is localized, either this field or [`Self::name_localizations`]
+    /// is set, depending on which endpoint this data was retrieved from
+    /// ([source](https://discord.com/developers/docs/interactions/application-commands#retrieving-localized-commands)).
+    pub name_localized: Option<String>,
+    /// All localized command names.
+    ///
+    /// If the name is localized, either this field or [`Self::name_localized`]
+    /// is set, depending on which endpoint this data was retrieved from
+    /// ([source](https://discord.com/developers/docs/interactions/application-commands#retrieving-localized-commands)).
+    pub name_localizations: Option<HashMap<String, String>>,
     /// The command description.
     pub description: String,
+    /// The localized command description of the selected locale.
+    ///
+    /// If the description is localized, either this field or [`Self::description_localizations`]
+    /// is set, depending on which endpoint this data was retrieved from
+    /// ([source](https://discord.com/developers/docs/interactions/application-commands#retrieving-localized-commands)).
+    pub description_localized: Option<String>,
+    /// All localized command descriptions.
+    ///
+    /// If the description is localized, either this field or [`Self::description_localized`]
+    /// is set, depending on which endpoint this data was retrieved from
+    /// ([source](https://discord.com/developers/docs/interactions/application-commands#retrieving-localized-commands)).
+    pub description_localizations: Option<HashMap<String, String>>,
     /// The parameters for the command.
     #[serde(default)]
     pub options: Vec<CommandOption>,

commit 1f661772d23e03902913f7f7b659ce5204a309d0

Fix clippy warnings caused by #1921

src/builder/create_application_command.rs
@@ -43,7 +43,7 @@ impl CreateApplicationCommandOption {
     pub fn name_localized<D: ToString, E: ToString>(&mut self, locale: E, name: D) -> &mut Self {
         self.0
             .entry("name_localizations")
-            .or_insert(Value::Object(Default::default()))
+            .or_insert_with(|| Value::Object(JsonMap::new()))
             .as_object_mut()
             .expect("must be object")
             .insert(locale.to_string(), Value::String(name.to_string()));
@@ -73,7 +73,7 @@ impl CreateApplicationCommandOption {
     ) -> &mut Self {
         self.0
             .entry("description_localizations")
-            .or_insert(Value::Object(Default::default()))
+            .or_insert_with(|| Value::Object(JsonMap::new()))
             .as_object_mut()
             .expect("must be object")
             .insert(locale.to_string(), Value::String(description.to_string()));
@@ -302,7 +302,7 @@ impl CreateApplicationCommand {
     pub fn name_localized<D: ToString, E: ToString>(&mut self, locale: E, name: D) -> &mut Self {
         self.0
             .entry("name_localizations")
-            .or_insert(Value::Object(Default::default()))
+            .or_insert_with(|| Value::Object(JsonMap::new()))
             .as_object_mut()
             .expect("must be object")
             .insert(locale.to_string(), Value::String(name.to_string()));
@@ -363,7 +363,7 @@ impl CreateApplicationCommand {
     ) -> &mut Self {
         self.0
             .entry("description_localizations")
-            .or_insert(Value::Object(Default::default()))
+            .or_insert_with(|| Value::Object(JsonMap::new()))
             .as_object_mut()
             .expect("must be object")
             .insert(locale.to_string(), Value::String(description.to_string()));