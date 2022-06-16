6582cf0e7fddda096f07364cfe44fbde2f9e6cd1
Gnome <david2005thomas@gmail.com>
2022-06-16 18:33:59 +0100

    Whoops, missed a comma

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -36,7 +36,7 @@ use crate::model::misc::EmojiIdentifier;
 #[cfg(feature = "model")]
 pub(crate) fn encode_image(raw: &[u8]) -> String {
     let mut encoded = base64::encode(raw);
-    encoded.insert_str(0, "data:image/png;base64");
+    encoded.insert_str(0, "data:image/png;base64,");
     encoded
 }
 
