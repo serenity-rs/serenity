fd2409c57af26959ce1dcaf7c7d87c233bd2748a
Constantin Nickel <constantin.nickel@gmail.com>
2022-06-17 09:22:02 +0200

    Change `automod::Action` into enum

--- src/model/guild/automod.rs
+++ src/model/guild/automod.rs
@@ -2,7 +2,12 @@
 //!
 //! [Discord docs](https://discord.com/developers/docs/resources/auto-moderation)
 
+use std::fmt;
+
+use serde::de::{Deserializer, Error, IgnoredAny, MapAccess};
+use serde::ser::{SerializeStruct, Serializer};
 use serde::{Deserialize, Serialize};
+use serde_value::{DeserializerError, Value};
 
 use crate::model::id::{ChannelId, GuildId, RoleId, RuleId, UserId};
 
@@ -143,11 +148,142 @@ impl From<KeywordPresentType> for u8 {
 }
 
 /// An action which will execute whenever a rule is triggered.
-#[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct Action {
-    #[serde(rename = "type")]
-    kind: ActionType,
-    metadata: Option<ActionMetadata>,
+#[derive(Clone, Debug, PartialEq)]
+pub enum Action {
+    /// Blocks the content of a message according to the rule.
+    BlockMessage,
+    /// Logs user content to a specified channel.
+    Alert(ChannelId),
+    /// Timeout user for a specified duration.
+    ///
+    /// Maximum of 2419200 seconds (4 weeks).
+    ///
+    /// A `Timeout` action can only be setup for [`Keyword`] rules.
+    /// [`Permissions::MODERATE_MEMBERS`] permission is required to use the `Timeout` action type.
+    ///
+    /// [`Keyword`]: TriggerType::Keyword
+    /// [`Permissions::MODERATE_MEMBERS`]: crate::model::Permissions::MODERATE_MEMBERS
+    Timeout(u64),
+    Unknown(u8),
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(rename = "ActionMetadata")]
+struct Alert {
+    channel_id: ChannelId,
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(rename = "ActionMetadata")]
+struct Timeout {
+    #[serde(rename = "duration_seconds")]
+    duration: u64,
+}
+
+impl<'de> Deserialize<'de> for Action {
+    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        #[derive(Deserialize)]
+        #[serde(field_identifier, rename_all = "snake_case")]
+        enum Field {
+            Type,
+            Metadata,
+            Unknown(String),
+        }
+
+        struct Visitor;
+
+        impl<'de> serde::de::Visitor<'de> for Visitor {
+            type Value = Action;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+                formatter.write_str("automod rule action")
+            }
+
+            fn visit_map<A: MapAccess<'de>>(self, mut map: A) -> Result<Self::Value, A::Error> {
+                let mut kind = None;
+                let mut metadata = None;
+
+                while let Some(key) = map.next_key()? {
+                    match key {
+                        Field::Type => {
+                            if kind.is_some() {
+                                return Err(Error::duplicate_field("type"));
+                            }
+                            kind = Some(map.next_value()?);
+                        },
+                        Field::Metadata => {
+                            if metadata.is_some() {
+                                return Err(Error::duplicate_field("metadata"));
+                            }
+                            metadata = Some(map.next_value::<Value>()?);
+                        },
+                        Field::Unknown(_) => {
+                            map.next_value::<IgnoredAny>()?;
+                        },
+                    }
+                }
+                let kind = kind.ok_or_else(|| Error::missing_field("type"))?;
+                match kind {
+                    ActionType::BlockMessage => Ok(Action::BlockMessage),
+                    ActionType::Alert => {
+                        let alert: Alert = metadata
+                            .ok_or_else(|| Error::missing_field("metadata"))?
+                            .deserialize_into()
+                            .map_err(DeserializerError::into_error)?;
+                        Ok(Action::Alert(alert.channel_id))
+                    },
+                    ActionType::Timeout => {
+                        let timeout: Timeout = metadata
+                            .ok_or_else(|| Error::missing_field("metadata"))?
+                            .deserialize_into()
+                            .map_err(DeserializerError::into_error)?;
+                        Ok(Action::Timeout(timeout.duration))
+                    },
+                    ActionType::Unknown(unknown) => Ok(Action::Unknown(unknown)),
+                }
+            }
+        }
+
+        deserializer.deserialize_any(Visitor)
+    }
+}
+
+impl Serialize for Action {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        let has_metadata = matches!(self, Self::Alert(_) | Self::Timeout(_));
+
+        let len = 1 + usize::from(has_metadata);
+        let mut s = serializer.serialize_struct("Action", len)?;
+
+        s.serialize_field("type", &self.kind())?;
+        match *self {
+            Self::Alert(channel_id) => {
+                s.serialize_field("metadata", &Alert {
+                    channel_id,
+                })?;
+            },
+            Self::Timeout(duration) => {
+                s.serialize_field("metadata", &Timeout {
+                    duration,
+                })?;
+            },
+            _ => {},
+        }
+
+        s.end()
+    }
+}
+
+impl Action {
+    #[must_use]
+    pub fn kind(&self) -> ActionType {
+        match self {
+            Self::BlockMessage => ActionType::BlockMessage,
+            Self::Alert(_) => ActionType::Alert,
+            Self::Timeout(_) => ActionType::Timeout,
+            Self::Unknown(unknown) => ActionType::Unknown(*unknown),
+        }
+    }
 }
 
 /// Type of [`Action`].
@@ -192,10 +328,76 @@ impl From<ActionType> for u8 {
     }
 }
 
-/// Additional metadata needed during execution for a specific action type.
-#[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct ActionMetadata {
-    channel_id: Option<ChannelId>,
-    #[serde(rename = "duration_seconds")]
-    duration: Option<u32>,
+#[cfg(test)]
+mod tests {
+    use serde_test::Token;
+
+    use super::*;
+
+    #[test]
+    fn action_serde() {
+        let value = Action::BlockMessage;
+
+        serde_test::assert_tokens(&value, &[
+            Token::Struct {
+                name: "Action",
+                len: 1,
+            },
+            Token::Str("type"),
+            Token::U8(1),
+            Token::StructEnd,
+        ]);
+
+        let value = Action::Alert(ChannelId(123));
+        serde_test::assert_tokens(&value, &[
+            Token::Struct {
+                name: "Action",
+                len: 2,
+            },
+            Token::Str("type"),
+            Token::U8(2),
+            Token::Str("metadata"),
+            Token::Struct {
+                name: "ActionMetadata",
+                len: 1,
+            },
+            Token::Str("channel_id"),
+            Token::NewtypeStruct {
+                name: "ChannelId",
+            },
+            Token::Str("123"),
+            Token::StructEnd,
+            Token::StructEnd,
+        ]);
+
+        let value = Action::Timeout(1024);
+        serde_test::assert_tokens(&value, &[
+            Token::Struct {
+                name: "Action",
+                len: 2,
+            },
+            Token::Str("type"),
+            Token::U8(3),
+            Token::Str("metadata"),
+            Token::Struct {
+                name: "ActionMetadata",
+                len: 1,
+            },
+            Token::Str("duration_seconds"),
+            Token::U64(1024),
+            Token::StructEnd,
+            Token::StructEnd,
+        ]);
+
+        let value = Action::Unknown(123);
+        serde_test::assert_tokens(&value, &[
+            Token::Struct {
+                name: "Action",
+                len: 1,
+            },
+            Token::Str("type"),
+            Token::U8(123),
+            Token::StructEnd,
+        ]);
+    }
 }
