46d8d9cb674f800e3e70820508c8b68614935dab
Gnome! <45660393+GnomedDev@users.noreply.github.com>
2022-06-20 14:41:45 +0100

    Use `CacheRef` for `CurrentUser` (#1996)
    This removes `Cache::current_user_id` and `current_user_field` and makes
    `Cache::current_user` return a `CacheRef` by reworking the internals of
    `CacheRef` to hold an enum of different guard types, currently only a dashmap
    `Ref` or a `parking_lot::RwLockReadGuard`. This prevents unnecessary cloning of
    the current user and monomorphization bloat from the `_field` method.

--- src/model/channel/message.rs
+++ src/model/channel/message.rs
@@ -161,7 +161,7 @@ impl Message {
         #[cfg(feature = "cache")]
         {
             if let Some(cache) = cache_http.cache() {
-                if self.author.id != cache.current_user_id() && self.guild_id.is_some() {
+                if self.author.id != cache.current_user().id && self.guild_id.is_some() {
                     utils::user_has_perms_cache(
                         cache,
                         self.channel_id,
@@ -222,7 +222,7 @@ impl Message {
         #[cfg(feature = "cache")]
         {
             if let Some(cache) = cache_http.cache() {
-                if self.author.id != cache.current_user_id() {
+                if self.author.id != cache.current_user().id {
                     utils::user_has_perms_cache(
                         cache,
                         self.channel_id,
@@ -342,7 +342,7 @@ impl Message {
         #[cfg(feature = "cache")]
         {
             if let Some(cache) = cache_http.cache() {
-                if self.author.id != cache.current_user_id() {
+                if self.author.id != cache.current_user().id {
                     return Err(Error::Model(ModelError::InvalidUser));
                 }
             }
@@ -571,7 +571,7 @@ impl Message {
                     )?;
                 }
 
-                user_id = Some(cache.current_user_id());
+                user_id = Some(cache.current_user().id);
             }
         }
 
@@ -736,7 +736,7 @@ impl Message {
                     Permissions::MANAGE_MESSAGES,
                 )?;
 
-                if self.author.id != cache.current_user_id() {
+                if self.author.id != cache.current_user().id {
                     return Err(Error::Model(ModelError::NotAuthor));
                 }
             }
@@ -775,7 +775,7 @@ impl Message {
         #[cfg(feature = "cache")]
         {
             if let Some(cache) = cache_http.cache() {
-                return Ok(self.mentions_user_id(cache.current_user_id()));
+                return Ok(self.mentions_user_id(cache.current_user().id));
             }
         }
 
