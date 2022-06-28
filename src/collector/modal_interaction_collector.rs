272a3073cccf819b25a88fb66666d807602aa821
Gnome! <45660393+GnomedDev@users.noreply.github.com>
2022-06-28 16:03:28 +0100

    Clean up collector module by replacing duplication with generics (#2011)

--- src/collector/modal_interaction_collector.rs
+++ src/collector/modal_interaction_collector.rs
@@ -1,49 +1,16 @@
-use std::fmt;
-use std::future::Future;
 use std::num::NonZeroU64;
-use std::pin::Pin;
 use std::sync::Arc;
-use std::task::{Context as FutContext, Poll};
 
-use futures::future::BoxFuture;
-use futures::stream::{Stream, StreamExt};
 use tokio::sync::mpsc::{
     unbounded_channel,
     UnboundedReceiver as Receiver,
     UnboundedSender as Sender,
 };
-use tokio::time::Sleep;
 
 use crate::client::bridge::gateway::ShardMessenger;
 use crate::collector::macros::*;
-use crate::collector::{FilterFn, LazyArc};
+use crate::collector::{CommonFilterOptions, LazyArc};
 use crate::model::application::interaction::modal::ModalSubmitInteraction;
-
-macro_rules! impl_modal_interaction_collector {
-    ($($name:ident;)*) => {
-        $(
-            impl $name {
-                /// Sets a filter function where interactions passed to the function must
-                /// return `true`, otherwise the interaction won't be collected.
-                /// This is the last instance to pass for an interaction to count as *collected*.
-                ///
-                /// This function is intended to be an interaction filter.
-                pub fn filter<F: Fn(&ModalSubmitInteraction) -> bool + 'static + Send + Sync>(mut self, function: F) -> Self {
-                    self.filter.as_mut().unwrap().filter = Some(FilterFn(Arc::new(function)));
-
-                    self
-                }
-
-                impl_collect_limit!("Limits how many interactions can be collected. An interaction is considered *collected*, if the interaction passes all the requirements.");
-                impl_channel_id!("Sets the channel on which the interaction must occur. If an interaction is not on a message with this channel ID, it won't be received.");
-                impl_guild_id!("Sets the guild in which the interaction must occur. If an interaction is not on a message with this guild ID, it won't be received.");
-                impl_message_id!("Sets the message on which the interaction must occur. If an interaction is not on a message with this ID, it won't be received.");
-                impl_author_id!("Sets the required author ID of an interaction. If an interaction is not triggered by a user with this ID, it won't be received");
-                impl_timeout!("Sets a `duration` for how long the collector shall receive interactions.");
-            }
-        )*
-    }
-}
 
 /// Filters events on the shard's end and sends them to the collector.
 #[derive(Clone, Debug)]
@@ -52,11 +19,15 @@ pub struct ModalInteractionFilter {
     collected: u32,
     options: FilterOptions,
     sender: Sender<Arc<ModalSubmitInteraction>>,
+    common_options: CommonFilterOptions<ModalSubmitInteraction>,
 }
 
 impl ModalInteractionFilter {
     /// Creates a new filter
-    fn new(options: FilterOptions) -> (Self, Receiver<Arc<ModalSubmitInteraction>>) {
+    fn new(
+        options: FilterOptions,
+        common_options: CommonFilterOptions<ModalSubmitInteraction>,
+    ) -> (Self, Receiver<Arc<ModalSubmitInteraction>>) {
         let (sender, receiver) = unbounded_channel();
 
         let filter = Self {
@@ -64,6 +35,7 @@ impl ModalInteractionFilter {
             collected: 0,
             sender,
             options,
+            common_options,
         };
 
         (filter, receiver)
@@ -99,161 +71,54 @@ impl ModalInteractionFilter {
                 .map_or(true, |id| Some(id) == interaction.message.as_ref().map(|m| m.id.0))
             && self.options.channel_id.map_or(true, |id| id == interaction.channel_id.as_ref().0)
             && self.options.author_id.map_or(true, |id| id == interaction.user.id.0)
-            && self.options.filter.as_ref().map_or(true, |f| f.0(interaction))
+            && self.common_options.filter.as_ref().map_or(true, |f| f.0(interaction))
     }
 
     /// Checks if the filter is within set receive and collect limits.
     /// An interaction is considered *received* even when it does not meet the
     /// constraints.
     fn is_within_limits(&self) -> bool {
-        self.options.filter_limit.map_or(true, |limit| self.filtered < limit)
-            && self.options.collect_limit.map_or(true, |limit| self.collected < limit)
+        self.common_options.filter_limit.map_or(true, |limit| self.filtered < limit)
+            && self.common_options.collect_limit.map_or(true, |limit| self.collected < limit)
     }
 }
 
-#[derive(Clone, Default)]
-struct FilterOptions {
-    filter_limit: Option<u32>,
-    collect_limit: Option<u32>,
-    filter: Option<FilterFn<ModalSubmitInteraction>>,
+#[derive(Clone, Debug, Default)]
+pub struct FilterOptions {
     channel_id: Option<NonZeroU64>,
     guild_id: Option<NonZeroU64>,
     author_id: Option<NonZeroU64>,
     message_id: Option<NonZeroU64>,
 }
 
-impl fmt::Debug for FilterOptions {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("ModalInteractionFilter")
-            .field("collect_limit", &self.collect_limit)
-            .field("filter", &"Option<Arc<dyn Fn(&Arc<Reaction>) -> bool + 'static + Send + Sync>>")
-            .field("channel_id", &self.channel_id)
-            .field("guild_id", &self.guild_id)
-            .field("author_id", &self.author_id)
-            .finish()
+impl super::CollectorBuilder<'_, ModalSubmitInteraction> {
+    impl_channel_id!("Sets the channel on which the interaction must occur. If an interaction is not on a message with this channel ID, it won't be received.");
+    impl_guild_id!("Sets the guild in which the interaction must occur. If an interaction is not on a message with this guild ID, it won't be received.");
+    impl_message_id!("Sets the message on which the interaction must occur. If an interaction is not on a message with this ID, it won't be received.");
+    impl_author_id!("Sets the required author ID of an interaction. If an interaction is not triggered by a user with this ID, it won't be received");
+}
+
+impl super::FilterOptions<ModalSubmitInteraction> for FilterOptions {
+    type FilterItem = ModalSubmitInteraction;
+
+    fn build(
+        self,
+        messenger: &ShardMessenger,
+        common_options: CommonFilterOptions<Self::FilterItem>,
+    ) -> Receiver<Arc<ModalSubmitInteraction>> {
+        let (filter, recv) = ModalInteractionFilter::new(self, common_options);
+        messenger.set_modal_interaction_filter(filter);
+
+        recv
     }
 }
-
-// Implement the common setters for all modal interaction collector types.
-// This avoids using a trait that the user would need to import in
-// order to use any of these methods.
-impl_modal_interaction_collector! {
-    CollectModalInteraction;
-    ModalInteractionCollectorBuilder;
+impl super::Collectable for ModalSubmitInteraction {
+    type FilterOptions = FilterOptions;
 }
 
-#[must_use = "Builders do nothing unless built"]
-pub struct ModalInteractionCollectorBuilder {
-    filter: Option<FilterOptions>,
-    shard: Option<ShardMessenger>,
-    timeout: Option<Pin<Box<Sleep>>>,
-}
+/// A modal interaction collector receives interactions matching a the given filter for a set duration.
+pub type ModalInteractionCollector = super::Collector<ModalSubmitInteraction>;
+pub type ModalInteractionCollectorBuilder<'a> = super::CollectorBuilder<'a, ModalSubmitInteraction>;
 
-impl ModalInteractionCollectorBuilder {
-    pub fn new(shard_messenger: impl AsRef<ShardMessenger>) -> Self {
-        Self {
-            filter: Some(FilterOptions::default()),
-            shard: Some(shard_messenger.as_ref().clone()),
-            timeout: None,
-        }
-    }
-
-    /// Use the given configuration to build the [`ModalInteractionCollector`].
-    #[allow(clippy::unwrap_used)]
-    #[must_use]
-    pub fn build(self) -> ModalInteractionCollector {
-        let shard_messenger = self.shard.unwrap();
-        let (filter, receiver) = ModalInteractionFilter::new(self.filter.unwrap());
-        let timeout = self.timeout;
-
-        shard_messenger.set_modal_interaction_filter(filter);
-
-        ModalInteractionCollector {
-            receiver: Box::pin(receiver),
-            timeout,
-        }
-    }
-}
-
-#[must_use = "builders do nothing unless awaited"]
-pub struct CollectModalInteraction {
-    filter: Option<FilterOptions>,
-    shard: Option<ShardMessenger>,
-    timeout: Option<Pin<Box<Sleep>>>,
-    fut: Option<BoxFuture<'static, Option<Arc<ModalSubmitInteraction>>>>,
-}
-
-impl CollectModalInteraction {
-    pub fn new(shard_messenger: impl AsRef<ShardMessenger>) -> Self {
-        Self {
-            filter: Some(FilterOptions::default()),
-            shard: Some(shard_messenger.as_ref().clone()),
-            timeout: None,
-            fut: None,
-        }
-    }
-}
-
-impl Future for CollectModalInteraction {
-    type Output = Option<Arc<ModalSubmitInteraction>>;
-    #[allow(clippy::unwrap_used)]
-    fn poll(mut self: Pin<&mut Self>, ctx: &mut FutContext<'_>) -> Poll<Self::Output> {
-        if self.fut.is_none() {
-            let shard_messenger = self.shard.take().unwrap();
-            let (filter, receiver) = ModalInteractionFilter::new(self.filter.take().unwrap());
-            let timeout = self.timeout.take();
-
-            self.fut = Some(Box::pin(async move {
-                shard_messenger.set_modal_interaction_filter(filter);
-
-                ModalInteractionCollector {
-                    receiver: Box::pin(receiver),
-                    timeout,
-                }
-                .next()
-                .await
-            }));
-        }
-
-        self.fut.as_mut().unwrap().as_mut().poll(ctx)
-    }
-}
-
-/// A modal interaction collector receives interactions matching a the given filter for a
-/// set duration.
-pub struct ModalInteractionCollector {
-    receiver: Pin<Box<Receiver<Arc<ModalSubmitInteraction>>>>,
-    timeout: Option<Pin<Box<Sleep>>>,
-}
-
-impl ModalInteractionCollector {
-    /// Stops collecting, this will implicitly be done once the
-    /// collector drops.
-    /// In case the drop does not appear until later, it is preferred to
-    /// stop the collector early.
-    pub fn stop(mut self) {
-        self.receiver.close();
-    }
-}
-
-impl Stream for ModalInteractionCollector {
-    type Item = Arc<ModalSubmitInteraction>;
-    fn poll_next(mut self: Pin<&mut Self>, ctx: &mut FutContext<'_>) -> Poll<Option<Self::Item>> {
-        if let Some(ref mut timeout) = self.timeout {
-            match timeout.as_mut().poll(ctx) {
-                Poll::Ready(_) => {
-                    return Poll::Ready(None);
-                },
-                Poll::Pending => (),
-            }
-        }
-
-        self.receiver.as_mut().poll_recv(ctx)
-    }
-}
-
-impl Drop for ModalInteractionCollector {
-    fn drop(&mut self) {
-        self.receiver.close();
-    }
-}
+#[deprecated = "Use ModalInteractionCollectorBuilder::collect_single"]
+pub type CollectModalInteraction<'a> = ModalInteractionCollectorBuilder<'a>;
