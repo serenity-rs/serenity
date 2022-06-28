fbf32257384f2ec80b8e83bcb6727c560b3162e1
Gnome! <45660393+GnomedDev@users.noreply.github.com>
2022-06-28 15:53:48 +0100

    Remove `InterMessage::Json` (#2009)

--- src/client/bridge/gateway/shard_runner.rs
+++ src/client/bridge/gateway/shard_runner.rs
@@ -453,10 +453,6 @@ impl ShardRunner {
                     },
                 },
             },
-            InterMessage::Json(value) => {
-                // Value must be forwarded over the websocket
-                self.shard.client.send_json(&value).await.is_ok()
-            },
         }
     }
 
