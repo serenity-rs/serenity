4846dfde43ec4506e0f2b84d85e37aeed5fd1bc8
Gnome! <45660393+GnomedDev@users.noreply.github.com>
2022-07-12 21:47:52 +0100

    Constify some functions that could be used in a `const` context (#2047)

--- src/model/channel/mod.rs
+++ src/model/channel/mod.rs
@@ -200,7 +200,7 @@ impl Channel {
     /// [`PrivateChannel`].
     #[inline]
     #[must_use]
-    pub fn id(&self) -> ChannelId {
+    pub const fn id(&self) -> ChannelId {
         match self {
             Self::Guild(ch) => ch.id,
             Self::Private(ch) => ch.id,
@@ -214,7 +214,7 @@ impl Channel {
     /// If other channel types are used it will return None.
     #[inline]
     #[must_use]
-    pub fn position(&self) -> Option<i64> {
+    pub const fn position(&self) -> Option<i64> {
         match self {
             Self::Guild(channel) => Some(channel.position),
             Self::Category(category) => Some(category.position),
@@ -320,7 +320,7 @@ enum_number! {
 impl ChannelType {
     #[inline]
     #[must_use]
-    pub fn name(&self) -> &str {
+    pub const fn name(&self) -> &str {
         match *self {
             Self::Private => "private",
             Self::Text => "text",
