dbc037d823b0b926f1d9c17633f58ebc0273ecf1
Alex M. M <acdenissk69@gmail.com>
2022-07-17 21:40:42 +0200

    Revert "Clean up dispatch and unnecessary clones in client (#2019)"
    This reverts commit de05e5bb3a99b45010b5aa2ebaa64a32db2d2e97 because it
    broke shard code to the point where it just crashes, preventing from
    even a simple ping-pong bot from running.

--- examples/e15_simple_dashboard/src/main.rs
+++ examples/e15_simple_dashboard/src/main.rs
@@ -269,7 +269,9 @@ impl EventHandler for Handler {
                     let shard_manager = data_read.get::<ShardManagerContainer>().unwrap();
 
                     let manager = shard_manager.lock().await;
-                    let runner = manager.runners.get(&ShardId(ctx.shard_id)).unwrap();
+                    let runners = manager.runners.lock().await;
+
+                    let runner = runners.get(&ShardId(ctx.shard_id)).unwrap();
 
                     if let Some(duration) = runner.latency {
                         duration.as_millis() as f64
@@ -462,7 +464,9 @@ async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
         let shard_manager = data_read.get::<ShardManagerContainer>().unwrap();
 
         let manager = shard_manager.lock().await;
-        let runner = manager.runners.get(&ShardId(ctx.shard_id)).unwrap();
+        let runners = manager.runners.lock().await;
+
+        let runner = runners.get(&ShardId(ctx.shard_id)).unwrap();
 
         if let Some(duration) = runner.latency {
             format!("{:.2}ms", duration.as_millis())
