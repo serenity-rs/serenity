5d032716def2a35e03298d74be12cddb663c5937
Michael Krasnitski <42564254+mkrasnitski@users.noreply.github.com>
2022-07-30 12:02:52 -0400

    Remove unneeded and deprecated application commands builders. (#2078)

--- src/model/guild/partial_guild.rs
+++ src/model/guild/partial_guild.rs
@@ -6,7 +6,6 @@ use tracing::{error, warn};
 use crate::builder::{
     CreateApplicationCommand,
     CreateApplicationCommandPermissionsData,
-    CreateApplicationCommands,
     CreateChannel,
     CreateSticker,
     EditAutoModRule,
@@ -476,13 +475,13 @@ impl PartialGuild {
     ///
     /// # Errors
     ///
-    /// See [`CreateApplicationCommands::execute`] for a list of possible errors.
+    /// Returns the same errors as [`Self::create_application_command`].
     pub async fn set_application_commands(
         &self,
         http: impl AsRef<Http>,
-        builder: CreateApplicationCommands,
+        commands: Vec<CreateApplicationCommand>,
     ) -> Result<Vec<Command>> {
-        self.id.set_application_commands(http, builder).await
+        self.id.set_application_commands(http, commands).await
     }
 
     /// Create a guild specific [`CommandPermission`].
@@ -499,21 +498,6 @@ impl PartialGuild {
         builder: CreateApplicationCommandPermissionsData,
     ) -> Result<CommandPermission> {
         self.id.create_application_command_permission(http, command_id, builder).await
-    }
-
-    /// Override permissions for all guild application commands.
-    ///
-    /// # Errors
-    ///
-    /// See [`CreateApplicationCommandsPermissions::execute`] for a list of possible errors.
-    #[deprecated(note = "use `create_appliction_command_permission`.")]
-    #[allow(deprecated)]
-    pub async fn set_application_commands_permissions(
-        &self,
-        http: impl AsRef<Http>,
-        builder: crate::builder::CreateApplicationCommandsPermissions,
-    ) -> Result<Vec<CommandPermission>> {
-        self.id.set_application_commands_permissions(http, builder).await
     }
 
     /// Get all guild application commands.
