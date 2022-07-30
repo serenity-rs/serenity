3c60295034ac7dc04a5651df3020d6c94b60e506
Michael Krasnitski <42564254+mkrasnitski@users.noreply.github.com>
2022-07-30 12:02:52 -0400

    Remove unneeded and deprecated application commands builders. (#2078)

--- src/builder/mod.rs
+++ src/builder/mod.rs
@@ -54,15 +54,10 @@ pub use self::create_allowed_mentions::{CreateAllowedMentions, ParseValue};
 pub use self::create_application_command::{
     CreateApplicationCommand,
     CreateApplicationCommandOption,
-    CreateApplicationCommands,
 };
-// Remove deprecated types and this allow() attribute in next breaking release
-#[allow(deprecated)]
 pub use self::create_application_command_permission::{
     CreateApplicationCommandPermissionData,
-    CreateApplicationCommandPermissions,
     CreateApplicationCommandPermissionsData,
-    CreateApplicationCommandsPermissions,
 };
 pub use self::create_channel::CreateChannel;
 pub use self::create_components::{
