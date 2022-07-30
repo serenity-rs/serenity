8969163b0da60a0d8a34aae50ad7f828f90a4b8b
Gnome! <45660393+GnomedDev@users.noreply.github.com>
2022-07-30 07:12:44 +0100

    Move `Colour` to `models` and remove feature guards (#2073)

--- command_attr/src/structures.rs
+++ command_attr/src/structures.rs
@@ -414,7 +414,7 @@ impl Colour {
 impl ToTokens for Colour {
     fn to_tokens(&self, stream: &mut TokenStream2) {
         let value = self.0;
-        let path = quote!(serenity::utils::Colour);
+        let path = quote!(serenity::model::colour::Colour);
 
         stream.extend(quote! {
             #path(#value)
