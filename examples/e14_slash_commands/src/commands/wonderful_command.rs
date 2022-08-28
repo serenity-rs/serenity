cd0b0ab8dd73c2370fc0449abbbe2400bc255171
Michael Krasnitski <42564254+mkrasnitski@users.noreply.github.com>
2022-08-28 12:04:48 -0400

    Make `description` field optional when creating slash commands (#2110)

--- examples/e14_slash_commands/src/commands/wonderful_command.rs
+++ examples/e14_slash_commands/src/commands/wonderful_command.rs
@@ -1,5 +1,5 @@
 use serenity::builder::CreateApplicationCommand;
 
 pub fn register() -> CreateApplicationCommand {
-    CreateApplicationCommand::new("wonderful_command", "An amazing command")
+    CreateApplicationCommand::new("wonderful_command").description("An amazing command")
 }
