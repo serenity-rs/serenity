17143edd39c7a8b803b00f4074f07e9f4d3b12f9
kangalioo <jannik.a.schaper@web.de>
2022-09-08 20:57:55 +0200

    Fix doctest

--- src/builder/create_allowed_mentions.rs
+++ src/builder/create_allowed_mentions.rs
@@ -26,7 +26,7 @@ enum ParseValue {
 /// # let http = Http::new("token");
 /// # let b = CreateMessage::new();
 /// # let msg = ChannelId::new(7).message(&http, MessageId::new(8)).await?;
-/// use serenity::builder::{CreateAllowedMentions as Am, ParseValue};
+/// use serenity::builder::CreateAllowedMentions as Am;
 ///
 /// // Mention only the user 110372470472613888
 /// # let m = b.clone();
@@ -34,25 +34,25 @@ enum ParseValue {
 ///
 /// // Mention all users and the role 182894738100322304
 /// # let m = b.clone();
-/// m.allowed_mentions(Am::new().parse(ParseValue::Users).roles(vec![182894738100322304]));
+/// m.allowed_mentions(Am::new().all_users(true).roles(vec![182894738100322304]));
 ///
 /// // Mention all roles and nothing else
 /// # let m = b.clone();
-/// m.allowed_mentions(Am::new().parse(ParseValue::Roles));
+/// m.allowed_mentions(Am::new().all_roles(true));
 ///
 /// // Mention all roles and users, but not everyone
 /// # let m = b.clone();
-/// m.allowed_mentions(Am::new().parse(ParseValue::Users).parse(ParseValue::Roles));
+/// m.allowed_mentions(Am::new().all_users(true).all_roles(true));
 ///
 /// // Mention everyone and the users 182891574139682816, 110372470472613888
 /// # let m = b.clone();
 /// m.allowed_mentions(
-///     Am::new().parse(ParseValue::Everyone).users(vec![182891574139682816, 110372470472613888]),
+///     Am::new().everyone(true).users(vec![182891574139682816, 110372470472613888]),
 /// );
 ///
 /// // Mention everyone and the message author.
 /// # let m = b.clone();
-/// m.allowed_mentions(Am::new().parse(ParseValue::Everyone).users(vec![msg.author.id]));
+/// m.allowed_mentions(Am::new().everyone(true).users(vec![msg.author.id]));
 /// # Ok(())
 /// # }
 /// ```
