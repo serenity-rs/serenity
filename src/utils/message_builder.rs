d95dcbff300a76b17ca9e845f356593635d8e7bc
Gnome <david2005thomas@gmail.com>
2022-09-28 00:27:14 +0100

    Fix clippy warnings

--- src/utils/message_builder.rs
+++ src/utils/message_builder.rs
@@ -1150,17 +1150,19 @@ fn normalize(text: &str) -> String {
         .replace("discordservers.com", "discordservers\u{2024}com")
         .replace("discord.com/invite", "discord\u{2024}com/invite")
         .replace("discordapp.com/invite", "discordapp\u{2024}com/invite")
-        // Remove right-to-left override and other similar annoying symbols
-        .replace('\u{202E}', " ") // RTL Override
-        .replace('\u{200F}', " ") // RTL Mark
-        .replace('\u{202B}', " ") // RTL Embedding
-        .replace('\u{200B}', " ") // Zero-width space
-        .replace('\u{200D}', " ") // Zero-width joiner
-        .replace('\u{200C}', " ") // Zero-width non-joiner
         // Remove everyone and here mentions. Has to be put after ZWS replacement
         // because it utilises it itself.
         .replace("@everyone", "@\u{200B}everyone")
         .replace("@here", "@\u{200B}here")
+        // Remove right-to-left override and other similar annoying symbols
+        .replace([
+            '\u{202E}', // RTL Override
+            '\u{200F}', // RTL Mark
+            '\u{202B}', // RTL Embedding
+            '\u{200B}', // Zero-width space
+            '\u{200D}', // Zero-width joiner
+            '\u{200C}', // Zero-width non-joiner
+        ], " ")
 }
 
 #[cfg(test)]
