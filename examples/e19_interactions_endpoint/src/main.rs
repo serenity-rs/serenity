f39d374f1d63a459a903929c71bdae3e9858abc2
kangalioo <jannik.a.schaper@web.de>
2022-09-27 19:02:56 +0200

    Support interactions endpoint URL

---
+++ examples/e19_interactions_endpoint/src/main.rs
@@ -0,0 +1,75 @@
+use serenity::builder::*;
+use serenity::interactions_endpoint::Verifier;
+use serenity::model::application::interaction::application_command::*;
+use serenity::model::application::interaction::*;
+
+type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
+
+fn handle_command(
+    interaction: ApplicationCommandInteraction,
+) -> CreateInteractionResponse<'static> {
+    CreateInteractionResponse::new().interaction_response_data(
+        CreateInteractionResponseData::new().content(format!(
+            "Hello from interactions webhook HTTP server! <@{}>",
+            interaction.user.id
+        )),
+    )
+}
+
+fn handle_request(
+    mut request: tiny_http::Request,
+    body: &mut Vec<u8>,
+    verifier: &Verifier,
+) -> Result<(), Error> {
+    println!("Received request from {:?}", request.remote_addr());
+
+    // Read the request body (containing the interaction JSON)
+    body.clear();
+    request.as_reader().read_to_end(body)?;
+
+    // Reject request if it fails cryptographic verification
+    // Discord rejects the interaction endpoints URL if this check is not done
+    // (This part is very specific to your HTTP server crate of choice, so serenity cannot abstract
+    // away the boilerplate)
+    let find_header =
+        |name| Some(request.headers().iter().find(|h| h.field.equiv(name))?.value.as_str());
+    let signature = find_header("X-Signature-Ed25519").ok_or("missing signature header")?;
+    let timestamp = find_header("X-Signature-Timestamp").ok_or("missing timestamp header")?;
+    if verifier.verify(signature, timestamp, body).is_err() {
+        request.respond(tiny_http::Response::empty(401))?;
+        return Ok(());
+    }
+
+    // Build Discord response
+    let response = match serde_json::from_slice::<Interaction>(body)? {
+        // Discord rejects the interaction endpoints URL if pings are not acknowledged
+        Interaction::Ping(_) => {
+            CreateInteractionResponse::new().kind(InteractionResponseType::Pong)
+        },
+        Interaction::ApplicationCommand(interaction) => handle_command(interaction),
+        _ => return Ok(()),
+    };
+
+    // Send the Discord response back via HTTP
+    request.respond(
+        tiny_http::Response::from_data(serde_json::to_vec(&response)?)
+            .with_header("Content-Type: application/json".parse::<tiny_http::Header>().unwrap()),
+    )?;
+
+    Ok(())
+}
+
+fn main() -> Result<(), Error> {
+    let verifier =
+        Verifier::new("67c6bd767ca099e79efac9fcce4d2022a63bf7dea780e7f3d813f694c1597089");
+
+    // Setup an HTTP server and listen for incoming interaction requests
+    let server = tiny_http::Server::http("0.0.0.0:8787")?;
+    let mut body = Vec::new();
+    loop {
+        let request = server.recv()?;
+        if let Err(e) = handle_request(request, &mut body, &verifier) {
+            eprintln!("Error while handling request: {}", e);
+        }
+    }
+}
---
+++ src/interactions_endpoint.rs
@@ -0,0 +1,110 @@
+//! Tools related to Interactions Endpoint URL Discord feature
+//!
+//! > You can optionally configure an interactions endpoint to receive interactions via HTTP POSTs
+//! > rather than over Gateway with a bot user.
+//!
+//! https://discord.com/developers/docs/tutorials/upgrading-to-application-commands#adding-an-interactions-endpoint-url
+
+/// Parses a hex string into an array of `[u8]`
+fn parse_hex<const N: usize>(s: &str) -> Option<[u8; N]> {
+    if s.len() != N * 2 {
+        return None;
+    }
+
+    let mut res = [0; N];
+    for i in 0..N {
+        res[i] = u8::from_str_radix(s.get(2 * i..2 * (i + 1))?, 16).ok()?;
+    }
+    Some(res)
+}
+
+/// The byte array couldn't be parsed into a valid cryptographic public key.
+#[derive(Debug)]
+pub struct InvalidKey(ed25519_dalek::SignatureError);
+impl std::fmt::Display for InvalidKey {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "invalid bot public key: {}", self.0)
+    }
+}
+impl std::error::Error for InvalidKey {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        Some(&self.0)
+    }
+}
+
+/// Used to cryptographically verify incoming interactions HTTP request for authenticity.
+///
+/// If incoming requests are not verified, Discord will reject the URL for security reasons.
+///
+/// ```rust
+/// use serenity::interactions_endpoint::Verifier;
+///
+/// let verifier =
+///     Verifier::new("67c6bd767ca099e79efac9fcce4d2022a63bf7dea780e7f3d813f694c1597089");
+///
+/// // When receiving an HTTP request:
+/// # let http_headers = std::collections::HashMap::from([("X-Signature-Ed25519", ""), ("X-Signature-Timestamp", "")]);
+/// # let request_body = &[];
+/// let signature = http_headers["X-Signature-Ed25519"];
+/// let timestamp = http_headers["X-Signature-Timestamp"];
+/// if verifier.verify(signature, timestamp, request_body).is_err() {
+///     // Send HTTP 401 Unauthorized response
+/// }
+/// ```
+pub struct Verifier {
+    public_key: ed25519_dalek::PublicKey,
+}
+
+impl Verifier {
+    /// Creates a new [`Verifier`] from the given public key hex string.
+    ///
+    /// Panics if the given key is invalid. For a low-level, non-panicking variant, see
+    /// [`Self::try_new()`].
+    pub fn new(public_key: &str) -> Self {
+        Self::try_new(parse_hex(public_key).expect("public key must be a 64 digit hex string"))
+            .expect("invalid public key")
+    }
+
+    /// Creates a new [`Verifier`] from the public key bytes.
+    pub fn try_new(public_key: [u8; 32]) -> Result<Self, InvalidKey> {
+        Ok(Self {
+            public_key: ed25519_dalek::PublicKey::from_bytes(&public_key).map_err(InvalidKey)?,
+        })
+    }
+
+    /// Verifies a Discord request for authenticity, given the `X-Signature-Ed25519` HTTP header,
+    /// `X-Signature-Timestamp` HTTP headers and request body.
+    pub fn verify(&self, signature: &str, timestamp: &str, body: &[u8]) -> Result<(), ()> {
+        use ed25519_dalek::Verifier as _;
+
+        // Extract and parse signature
+        let signature = parse_hex::<64>(signature).ok_or(())?;
+        let signature = ed25519_dalek::Signature::from_bytes(&signature).map_err(|_| ())?;
+
+        // Verify
+        let message_to_verify = [timestamp.as_bytes(), body].concat();
+        self.public_key.verify(&message_to_verify, &signature).map_err(|_| ())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_hex() {
+        assert_eq!(parse_hex::<4>("bf7dea78"), Some([0xBF, 0x7D, 0xEA, 0x78]));
+        assert_eq!(parse_hex::<4>("bf7dea7"), None);
+        assert_eq!(parse_hex::<4>("bf7dea789"), None);
+        assert_eq!(parse_hex::<4>("bf7dea7x"), None);
+        assert_eq!(parse_hex(""), Some([]));
+        assert_eq!(
+            parse_hex("67c6bd767ca099e79efac9fcce4d2022a63bf7dea780e7f3d813f694c1597089"),
+            Some([
+                0x67, 0xC6, 0xBD, 0x76, 0x7C, 0xA0, 0x99, 0xE7, 0x9E, 0xFA, 0xC9, 0xFC, 0xCE, 0x4D,
+                0x20, 0x22, 0xA6, 0x3B, 0xF7, 0xDE, 0xA7, 0x80, 0xE7, 0xF3, 0xD8, 0x13, 0xF6, 0x94,
+                0xC1, 0x59, 0x70, 0x89
+            ])
+        );
+    }
+}
