05328c5a0a505bc6c5771e62454f0bc8f0381e40
Michael Krasnitski <42564254+mkrasnitski@users.noreply.github.com>
2022-10-04 16:57:56 -0400

    Clean up the codebase related to the MSRV (#2208)

--- src/http/client.rs
+++ src/http/client.rs
@@ -835,13 +835,10 @@ impl Http {
     /// **Note**: Requires the [Manage Emojis and Stickers] permission.
     ///
     /// [Manage Emojis and Stickers]: Permissions::MANAGE_EMOJIS_AND_STICKERS
-    // We take a `Vec<(String, String)>` rather than `Vec<(&'static str, String)>` to avoid a compiler
-    // bug around `async fn` and lifetime unification. TODO: change this back once MSRV is on 1.58.
-    // Relevant issue: https://github.com/rust-lang/rust/issues/63033
     pub async fn create_sticker<'a>(
         &self,
         guild_id: GuildId,
-        map: Vec<(String, String)>,
+        map: Vec<(&'static str, String)>,
         file: CreateAttachment,
         audit_log_reason: Option<&str>,
     ) -> Result<Sticker> {
