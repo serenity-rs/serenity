23ad23287facd0f4af3784502f79d87cd741c6b8
acdenisSK <acdenisSK@users.noreply.github.com>
2022-10-11 20:48:34 +0000

    deploy: c206f2f85bb8f8321caab98bf0789ba9cb30166b

---
+++ next/implementors/core/future/into_future/trait.IntoFuture.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"serenity":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/future/into_future/trait.IntoFuture.html\" title=\"trait core::future::into_future::IntoFuture\">IntoFuture</a> for <a class=\"struct\" href=\"serenity/client/struct.ClientBuilder.html\" title=\"struct serenity::client::ClientBuilder\">ClientBuilder</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
