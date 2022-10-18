a3cfc81dc1af2db44d2502c6193e93ac9664aad5
Gnome! <45660393+GnomedDev@users.noreply.github.com>
2022-10-18 11:02:08 +0100

    Implement `custom_ids` filter for `CollectorBuilder` (#2217)

--- src/collector/component_interaction_collector.rs
+++ src/collector/component_interaction_collector.rs
@@ -17,6 +17,11 @@ impl FilterTrait<MessageComponentInteraction> for Filter<MessageComponentInterac
         interaction: &mut LazyArc<'_, MessageComponentInteraction>,
     ) -> bool {
         self.options.guild_id.map_or(true, |id| Some(id) == interaction.guild_id)
+            && self
+                .options
+                .custom_ids
+                .as_ref()
+                .map_or(true, |id| id.contains(&interaction.data.custom_id))
             && self.options.message_id.map_or(true, |id| interaction.message.id == id)
             && self.options.channel_id.map_or(true, |id| id == interaction.channel_id)
             && self.options.author_id.map_or(true, |id| id == interaction.user.id)
@@ -30,13 +35,15 @@ pub struct FilterOptions {
     guild_id: Option<GuildId>,
     author_id: Option<UserId>,
     message_id: Option<MessageId>,
+    custom_ids: Option<Vec<String>>,
 }
 
 impl super::CollectorBuilder<'_, MessageComponentInteraction> {
     impl_channel_id!("Sets the channel on which the interaction must occur. If an interaction is not on a message with this channel ID, it won't be received.");
     impl_guild_id!("Sets the guild in which the interaction must occur. If an interaction is not on a message with this guild ID, it won't be received.");
     impl_message_id!("Sets the message on which the interaction must occur. If an interaction is not on a message with this ID, it won't be received.");
-    impl_author_id!("Sets the required author ID of an interaction. If an interaction is not triggered by a user with this ID, it won't be received");
+    impl_custom_ids!("Sets acceptable custom IDs for the interaction. If an interaction does not contain one of the custom IDs, it won't be received.");
+    impl_author_id!("Sets the required author ID of an interaction. If an interaction is not triggered by a user with this ID, it won't be received.");
 }
 
 #[nougat::gat]
