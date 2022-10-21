0c173738c806faede4c5fd068f58188b9aa6f8c7
kangalioo <jannik.a.schaper@web.de>
2022-10-21 22:09:25 +0200

    Add `CacheSettings::{time_to_live, cache_guilds, cache_channels, cache_users}` (#2210)
    The idea is that even users on resource contrained devices can enable cache
    feature, but just disable all the individual caching flags. What remains is the
    cached fixed-size data: `CachedShardData` and more importantly `CurrentUser`.

--- src/cache/settings.rs
+++ src/cache/settings.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 /// Settings for the cache.
 ///
 /// # Examples
@@ -7,45 +9,42 @@
 /// ```rust
 /// use serenity::cache::Settings as CacheSettings;
 ///
-/// let mut settings = CacheSettings::new();
-/// settings.max_messages(10);
+/// let mut settings = CacheSettings::default();
+/// settings.max_messages = 10;
 /// ```
-#[derive(Clone, Debug, Default)]
+#[derive(Clone, Debug)]
 #[non_exhaustive]
 pub struct Settings {
     /// The maximum number of messages to store in a channel's message cache.
     ///
     /// Defaults to 0.
     pub max_messages: usize,
+    /// How long temporarily-cached data should be stored before being thrown out.
+    ///
+    /// Defaults to one hour.
+    pub time_to_live: Duration,
+    /// Whether to cache guild data received from gateway.
+    ///
+    /// Defaults to true.
+    pub cache_guilds: bool,
+    /// Whether to cache channel data received from gateway.
+    ///
+    /// Defaults to true.
+    pub cache_channels: bool,
+    /// Whether to cache user and presence data received from gateway.
+    ///
+    /// Defaults to true.
+    pub cache_users: bool,
 }
 
-impl Settings {
-    /// Creates new settings to be used with a cache.
-    #[inline]
-    #[must_use]
-    pub fn new() -> Self {
-        Self::default()
-    }
-
-    /// Sets the maximum number of messages to cache in a channel.
-    ///
-    /// Refer to [`max_messages`] for more information.
-    ///
-    /// # Examples
-    ///
-    /// Set the maximum number of messages to cache:
-    ///
-    /// ```rust
-    /// use serenity::cache::Settings;
-    ///
-    /// let mut settings = Settings::new();
-    /// settings.max_messages(10);
-    /// ```
-    ///
-    /// [`max_messages`]: #structfield.max_messages
-    pub fn max_messages(&mut self, max: usize) -> &mut Self {
-        self.max_messages = max;
-
-        self
+impl Default for Settings {
+    fn default() -> Self {
+        Self {
+            max_messages: 0,
+            time_to_live: Duration::from_secs(60 * 60),
+            cache_guilds: true,
+            cache_channels: true,
+            cache_users: true,
+        }
     }
 }
