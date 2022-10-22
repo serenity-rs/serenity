3e20b9b43f3b18fdd65409a676bbf1c6a9d6a8df
acdenisSK <acdenisSK@users.noreply.github.com>
2022-10-22 20:15:16 +0000

    deploy: 360571081529bfab29e75ef00c0c1880d3db1900

--- next/serenity/model/prelude/application/interaction/message_component/sidebar-items.js
+++ next/serenity/model/prelude/application/interaction/message_component/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":[["MessageComponentInteraction","An interaction triggered by a message component."],["MessageComponentInteractionData","A message component interaction data, provided by [`MessageComponentInteraction::data`]"]]};
+window.SIDEBAR_ITEMS = {"enum":[["MessageComponentInteractionDataKind",""]],"struct":[["MessageComponentInteraction","An interaction triggered by a message component."],["MessageComponentInteractionData","A message component interaction data, provided by [`MessageComponentInteraction::data`]"]]};
