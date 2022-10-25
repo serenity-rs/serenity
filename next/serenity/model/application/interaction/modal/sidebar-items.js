270d0c86e854fb6d8b790b8282be983afff30f8c
acdenisSK <acdenisSK@users.noreply.github.com>
2022-10-25 21:52:39 +0000

    deploy: 1d7af681930f90b8f2a413587e5dee8e601136d3

--- next/serenity/model/application/interaction/modal/sidebar-items.js
+++ next/serenity/model/application/interaction/modal/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":[["ModalSubmitInteraction","An interaction triggered by a modal submit."],["ModalSubmitInteractionData","A modal submit interaction data, provided by [`ModalSubmitInteraction::data`]"]]};
+window.SIDEBAR_ITEMS = {"struct":[["ModalInteraction","An interaction triggered by a modal submit."],["ModalInteractionData","A modal submit interaction data, provided by [`ModalInteraction::data`]"]]};
