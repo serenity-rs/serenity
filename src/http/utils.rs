5b66cc46802204b35be0d51ec67f13c1118991bf
kangalioo <jannik.a.schaper@web.de>
2022-10-27 15:38:15 +0200

    Don't panic in loop_errors

--- src/http/utils.rs
+++ src/http/utils.rs
@@ -1,4 +1,4 @@
-use serde::de::{Deserialize, Deserializer};
+use serde::de::{Deserialize, Deserializer, Error as _};
 
 use crate::http::error::DiscordJsonSingleError;
 use crate::internal::prelude::*;
@@ -15,47 +15,53 @@ pub fn deserialize_errors<'de, D: Deserializer<'de>>(
 
     let mut errors = Vec::new();
     let mut path = Vec::new();
-    loop_errors(&map, &mut errors, &mut path);
+    loop_errors(&map, &mut errors, &mut path).map_err(D::Error::custom)?;
 
     Ok(errors)
 }
 
-fn make_error(errors_value: &Value, errors: &mut Vec<DiscordJsonSingleError>, path: &[&str]) {
-    let found_errors = errors_value.as_array().expect("expected array").clone();
+fn make_error(
+    errors_value: &Value,
+    errors: &mut Vec<DiscordJsonSingleError>,
+    path: &[&str],
+) -> StdResult<(), &'static str> {
+    let found_errors = errors_value.as_array().ok_or("expected array")?;
 
     for error in found_errors {
-        let error_object = error.as_object().expect("expected object");
+        let error_object = error.as_object().ok_or("expected object")?;
 
         errors.push(DiscordJsonSingleError {
             code: error_object
                 .get("code")
-                .expect("expected code")
+                .ok_or("expected code")?
                 .as_str()
-                .expect("expected string")
+                .ok_or("expected string")?
                 .to_owned(),
             message: error_object
                 .get("message")
-                .expect("expected message")
+                .ok_or("expected message")?
                 .as_str()
-                .expect("expected string")
+                .ok_or("expected string")?
                 .to_owned(),
             path: path.join("."),
         });
     }
+    Ok(())
 }
 
 fn loop_errors<'a>(
     value: &'a Value,
     errors: &mut Vec<DiscordJsonSingleError>,
     path: &mut Vec<&'a str>,
-) {
-    for (key, value) in value.as_object().expect("expected object") {
+) -> StdResult<(), &'static str> {
+    for (key, value) in value.as_object().ok_or("expected object")? {
         if key == "_errors" {
-            make_error(value, errors, path);
+            make_error(value, errors, path)?;
         } else {
             path.push(key);
-            loop_errors(value, errors, path);
+            loop_errors(value, errors, path)?;
             path.pop();
         }
     }
+    Ok(())
 }
