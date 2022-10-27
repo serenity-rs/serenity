ea9611db641d6e0dbf4d636336c5877374876b3e
kangalioo <jannik.a.schaper@web.de>
2022-10-27 15:28:49 +0200

    Fix loop_errors
    Now it also works with a top-level _errors instead of crashing

--- src/http/utils.rs
+++ src/http/utils.rs
@@ -14,51 +14,48 @@ pub fn deserialize_errors<'de, D: Deserializer<'de>>(
     }
 
     let mut errors = Vec::new();
-
-    loop_errors(&map, &mut errors, &[]);
+    let mut path = Vec::new();
+    loop_errors(&map, &mut errors, &mut path);
 
     Ok(errors)
 }
 
-fn loop_errors(value: &Value, errors: &mut Vec<DiscordJsonSingleError>, path: &[String]) {
-    for (key, looped) in value.as_object().expect("expected object").iter() {
-        let object = looped.as_object().expect("expected object");
-        if object.contains_key("_errors") {
-            let found_errors = object
-                .get("_errors")
-                .expect("expected _errors")
-                .as_array()
-                .expect("expected array")
-                .clone();
+fn make_error(errors_value: &Value, errors: &mut Vec<DiscordJsonSingleError>, path: &[&str]) {
+    let found_errors = errors_value.as_array().expect("expected array").clone();
 
-            for error in found_errors {
-                let error_object = error.as_object().expect("expected object");
-                let mut object_path = path.to_owned();
+    for error in found_errors {
+        let error_object = error.as_object().expect("expected object");
 
-                object_path.push(key.to_string());
+        errors.push(DiscordJsonSingleError {
+            code: error_object
+                .get("code")
+                .expect("expected code")
+                .as_str()
+                .expect("expected string")
+                .to_owned(),
+            message: error_object
+                .get("message")
+                .expect("expected message")
+                .as_str()
+                .expect("expected string")
+                .to_owned(),
+            path: path.join("."),
+        });
+    }
+}
 
-                errors.push(DiscordJsonSingleError {
-                    code: error_object
-                        .get("code")
-                        .expect("expected code")
-                        .as_str()
-                        .expect("expected string")
-                        .to_owned(),
-                    message: error_object
-                        .get("message")
-                        .expect("expected message")
-                        .as_str()
-                        .expect("expected string")
-                        .to_owned(),
-                    path: object_path.join("."),
-                });
-            }
-            continue;
+fn loop_errors<'a>(
+    value: &'a Value,
+    errors: &mut Vec<DiscordJsonSingleError>,
+    path: &mut Vec<&'a str>,
+) {
+    for (key, value) in value.as_object().expect("expected object") {
+        if key == "_errors" {
+            make_error(value, errors, path);
+        } else {
+            path.push(key);
+            loop_errors(value, errors, path);
+            path.pop();
         }
-
-        let mut new_path = path.to_owned();
-        new_path.push(key.to_string());
-
-        loop_errors(looped, errors, &new_path);
     }
 }
