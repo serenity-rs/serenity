a38f95a4de5198556b2ac46da1d791300f15d344
kangalioo <jannik.a.schaper@web.de>
2022-11-06 19:13:41 +0100

    Hide hasher implementation behind newtype (#2263)

--- src/cache/wrappers.rs
+++ src/cache/wrappers.rs
@@ -5,21 +5,20 @@ use std::hash::Hash;
 use dashmap::mapref::multiple::RefMulti;
 use dashmap::mapref::one::{Ref, RefMut};
 use dashmap::DashMap;
-use fxhash::FxBuildHasher;
 
 #[derive(Debug)]
 /// A wrapper around Option<DashMap<K, V>> to ease disabling specific cache fields.
-pub(crate) struct MaybeMap<K: Eq + Hash, V>(pub(super) Option<DashMap<K, V, FxBuildHasher>>);
+pub(crate) struct MaybeMap<K: Eq + Hash, V>(pub(super) Option<DashMap<K, V, BuildHasher>>);
 impl<K: Eq + Hash, V> MaybeMap<K, V> {
-    pub fn iter(&self) -> impl Iterator<Item = RefMulti<'_, K, V, FxBuildHasher>> {
+    pub fn iter(&self) -> impl Iterator<Item = RefMulti<'_, K, V, BuildHasher>> {
         Option::iter(&self.0).flat_map(DashMap::iter)
     }
 
-    pub fn get(&self, k: &K) -> Option<Ref<'_, K, V, FxBuildHasher>> {
+    pub fn get(&self, k: &K) -> Option<Ref<'_, K, V, BuildHasher>> {
         self.0.as_ref()?.get(k)
     }
 
-    pub fn get_mut(&self, k: &K) -> Option<RefMut<'_, K, V, FxBuildHasher>> {
+    pub fn get_mut(&self, k: &K) -> Option<RefMut<'_, K, V, BuildHasher>> {
         self.0.as_ref()?.get_mut(k)
     }
 
@@ -43,17 +42,37 @@ impl<K: Eq + Hash, V> MaybeMap<K, V> {
 #[derive(Clone, Copy, Debug)]
 /// A wrapper around a reference to a MaybeMap, allowing for public inspection of the underlying map
 /// without allowing mutation of internal cache fields, which could cause issues.
-pub struct ReadOnlyMapRef<'a, K: Eq + Hash, V>(Option<&'a DashMap<K, V, FxBuildHasher>>);
+pub struct ReadOnlyMapRef<'a, K: Eq + Hash, V>(Option<&'a DashMap<K, V, BuildHasher>>);
 impl<'a, K: Eq + Hash, V> ReadOnlyMapRef<'a, K, V> {
-    pub fn iter(&self) -> impl Iterator<Item = RefMulti<'_, K, V, FxBuildHasher>> {
+    pub fn iter(&self) -> impl Iterator<Item = RefMulti<'_, K, V, BuildHasher>> {
         self.0.into_iter().flat_map(DashMap::iter)
     }
 
-    pub fn get(&self, k: &K) -> Option<Ref<'_, K, V, FxBuildHasher>> {
+    pub fn get(&self, k: &K) -> Option<Ref<'_, K, V, BuildHasher>> {
         self.0?.get(k)
     }
 
     pub fn len(&self) -> usize {
         self.0.map_or(0, DashMap::len)
+    }
+}
+
+pub struct Hasher(fxhash::FxHasher);
+impl std::hash::Hasher for Hasher {
+    fn finish(&self) -> u64 {
+        self.0.finish()
+    }
+
+    fn write(&mut self, bytes: &[u8]) {
+        self.0.write(bytes);
+    }
+}
+#[derive(Clone, Default)]
+pub struct BuildHasher(fxhash::FxBuildHasher);
+impl std::hash::BuildHasher for BuildHasher {
+    type Hasher = Hasher;
+
+    fn build_hasher(&self) -> Self::Hasher {
+        Hasher(self.0.build_hasher())
     }
 }
