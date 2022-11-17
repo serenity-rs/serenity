cc8dd825e951dafb2111f6add31b838580397610
Michael Krasnitski <42564254+mkrasnitski@users.noreply.github.com>
2022-11-17 09:49:14 -0500

    Overhaul HTTP routing (#2288)
    This replaces the combination of `RouteInfo` and `Route` with a single
    `Route` struct, responsible for both the endpoint url and ratelimiting.
    
    A `routes!` macro is introduced to easily generate code for each endpoint.
    
    And `LightMethod` is inlined for each endpoint into the `Request` struct,
    as well as any query parameters.

--- src/http/ratelimiting.rs
+++ src/http/ratelimiting.rs
@@ -51,8 +51,7 @@ use tokio::sync::{Mutex, RwLock};
 use tokio::time::{sleep, Duration};
 use tracing::{debug, instrument};
 
-pub use super::routing::Route;
-use super::routing::RouteInfo;
+pub use super::routing::RatelimitingBucket;
 use super::{HttpError, LightMethod, Request};
 use crate::internal::prelude::*;
 
@@ -92,7 +91,7 @@ pub struct Ratelimiter {
     global: Arc<Mutex<()>>,
     // When futures is implemented, make tasks clear out their respective entry
     // when the 'reset' passes.
-    routes: Arc<RwLock<HashMap<Route, Arc<Mutex<Ratelimit>>>>>,
+    routes: Arc<RwLock<HashMap<RatelimitingBucket, Arc<Mutex<Ratelimit>>>>>,
     token: String,
     ratelimit_callback: Box<dyn Fn(RatelimitInfo) + Send + Sync>,
     absolute_ratelimits: bool,
@@ -142,7 +141,7 @@ impl Ratelimiter {
         self.absolute_ratelimits = absolute_ratelimits;
     }
 
-    /// The routes mutex is a HashMap of each [`Route`] and their respective
+    /// The routes mutex is a HashMap of each [`RatelimitingBucket`] and their respective
     /// ratelimit information.
     ///
     /// See the documentation for [`Ratelimit`] for more information on how the
@@ -163,7 +162,10 @@ impl Ratelimiter {
     /// let reader = routes.read().await;
     ///
     /// let channel_id = ChannelId::new(7);
-    /// if let Some(route) = reader.get(&Route::ChannelsId(channel_id)) {
+    /// let route = Route::Channel {
+    ///     channel_id,
+    /// };
+    /// if let Some(route) = reader.get(&route.ratelimiting_bucket()) {
     ///     if let Some(reset) = route.lock().await.reset() {
     ///         println!("Reset time at: {:?}", reset);
     ///     }
@@ -172,7 +174,7 @@ impl Ratelimiter {
     /// # }
     /// ```
     #[must_use]
-    pub fn routes(&self) -> Arc<RwLock<HashMap<Route, Arc<Mutex<Ratelimit>>>>> {
+    pub fn routes(&self) -> Arc<RwLock<HashMap<RatelimitingBucket, Arc<Mutex<Ratelimit>>>>> {
         Arc::clone(&self.routes)
     }
 
@@ -185,18 +187,6 @@ impl Ratelimiter {
             // This will block if another thread hit the global ratelimit.
             drop(self.global.lock().await);
 
-            // Destructure the tuple instead of retrieving the third value to
-            // take advantage of the type system. If `RouteInfo::deconstruct`
-            // returns a different number of tuple elements in the future,
-            // directly accessing a certain index
-            // (e.g. `req.route.deconstruct().1`) would mean this code would not
-            // indicate it might need to be updated for the new tuple element
-            // amount.
-            //
-            // This isn't normally important, but might be for ratelimiting.
-            let (method, route, path) = req.route.deconstruct();
-            let path = path.to_string();
-
             // Perform pre-checking here:
             //
             // - get the route's relevant rate
@@ -205,9 +195,11 @@ impl Ratelimiter {
             // - get the global rate;
             // - sleep if there is 0 remaining
             // - then, perform the request
-            let bucket = Arc::clone(self.routes.write().await.entry(route).or_default());
+            let ratelimiting_bucket = req.route.ratelimiting_bucket();
+            let bucket =
+                Arc::clone(self.routes.write().await.entry(ratelimiting_bucket).or_default());
 
-            bucket.lock().await.pre_hook(&req.route, &self.ratelimit_callback).await;
+            bucket.lock().await.pre_hook(&req, &self.ratelimit_callback).await;
 
             let request = req.clone().build(&self.client, &self.token, None)?.build()?;
 
@@ -226,7 +218,7 @@ impl Ratelimiter {
             // It _may_ be possible for the limit to be raised at any time,
             // so check if it did from the value of the 'x-ratelimit-limit'
             // header. If the limit was 5 and is now 7, add 2 to the 'remaining'
-            if route == Route::None {
+            if ratelimiting_bucket.is_none() {
                 return Ok(response);
             }
 
@@ -237,12 +229,15 @@ impl Ratelimiter {
                     if let Some(retry_after) =
                         parse_header::<f64>(response.headers(), "retry-after")?
                     {
-                        debug!("Ratelimited on route {:?} for {:?}s", route, retry_after);
+                        debug!(
+                            "Ratelimited on route {:?} for {:?}s",
+                            ratelimiting_bucket, retry_after
+                        );
                         (self.ratelimit_callback)(RatelimitInfo {
                             timeout: Duration::from_secs_f64(retry_after),
                             limit: 50,
-                            method,
-                            path,
+                            method: req.method,
+                            path: req.route.path().to_string(),
                             global: true,
                         });
                         sleep(Duration::from_secs_f64(retry_after)).await;
@@ -256,12 +251,7 @@ impl Ratelimiter {
                 bucket
                     .lock()
                     .await
-                    .post_hook(
-                        &response,
-                        &req.route,
-                        &self.ratelimit_callback,
-                        self.absolute_ratelimits,
-                    )
+                    .post_hook(&response, &req, &self.ratelimit_callback, self.absolute_ratelimits)
                     .await
             };
 
@@ -273,7 +263,7 @@ impl Ratelimiter {
 }
 
 /// A set of data containing information about the ratelimits for a particular
-/// [`Route`], which is stored in [`Http`].
+/// [`RatelimitingBucket`], which is stored in [`Http`].
 ///
 /// See the [Discord docs] on ratelimits for more information.
 ///
@@ -298,7 +288,7 @@ impl Ratelimit {
     #[instrument(skip(ratelimit_callback))]
     pub async fn pre_hook(
         &mut self,
-        route: &RouteInfo<'_>,
+        req: &Request<'_>,
         ratelimit_callback: &(dyn Fn(RatelimitInfo) + Send + Sync),
     ) {
         if self.limit() == 0 {
@@ -325,14 +315,16 @@ impl Ratelimit {
         };
 
         if self.remaining() == 0 {
-            let (method, route, path) = route.deconstruct();
-
-            debug!("Pre-emptive ratelimit on route {:?} for {}ms", route, delay.as_millis(),);
+            debug!(
+                "Pre-emptive ratelimit on route {:?} for {}ms",
+                req.route.ratelimiting_bucket(),
+                delay.as_millis(),
+            );
             ratelimit_callback(RatelimitInfo {
                 timeout: delay,
                 limit: self.limit,
-                method,
-                path: path.to_string(),
+                method: req.method,
+                path: req.route.path().to_string(),
                 global: false,
             });
 
@@ -348,7 +340,7 @@ impl Ratelimit {
     pub async fn post_hook(
         &mut self,
         response: &Response,
-        route: &RouteInfo<'_>,
+        req: &Request<'_>,
         ratelimit_callback: &(dyn Fn(RatelimitInfo) + Send + Sync),
         absolute_ratelimits: bool,
     ) -> Result<bool> {
@@ -379,14 +371,16 @@ impl Ratelimit {
         Ok(if response.status() != StatusCode::TOO_MANY_REQUESTS {
             false
         } else if let Some(retry_after) = parse_header::<f64>(response.headers(), "retry-after")? {
-            let (method, route, path) = route.deconstruct();
-
-            debug!("Ratelimited on route {:?} for {:?}s", route, retry_after);
+            debug!(
+                "Ratelimited on route {:?} for {:?}s",
+                req.route.ratelimiting_bucket(),
+                retry_after
+            );
             ratelimit_callback(RatelimitInfo {
                 timeout: Duration::from_secs_f64(retry_after),
                 limit: self.limit,
-                method,
-                path: path.to_string(),
+                method: req.method,
+                path: req.route.path().to_string(),
                 global: false,
             });
 
