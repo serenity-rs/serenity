fd03bfdd12927ada8bb9ac687df81270937bdd52
kangalioo <jannik.a.schaper@web.de>
2022-12-12 18:18:14 +0100

    Make collector API backwards compatible
    By
    - renaming foo_collector back to await_foo and await_foos
    - adding IntoFuture impl on collector types
    - and a deprecated `.build()` method that just redirects to `.stream()`

--- src/model/guild/partial_guild.rs
+++ src/model/guild/partial_guild.rs
@@ -1534,14 +1534,29 @@ impl PartialGuild {
 
     /// Returns a builder which can be awaited to obtain a message or stream of messages in this guild.
     #[cfg(feature = "collector")]
-    pub fn reply_collector(&self, shard_messenger: &ShardMessenger) -> MessageCollector {
+    pub fn await_reply(&self, shard_messenger: impl AsRef<ShardMessenger>) -> MessageCollector {
         MessageCollector::new(shard_messenger).guild_id(self.id)
+    }
+
+    /// Same as [`Self::await_reply`].
+    #[cfg(feature = "collector")]
+    pub fn await_replies(&self, shard_messenger: impl AsRef<ShardMessenger>) -> MessageCollector {
+        self.await_reply(shard_messenger)
     }
 
     /// Returns a builder which can be awaited to obtain a message or stream of reactions sent in this guild.
     #[cfg(feature = "collector")]
-    pub fn reaction_collector(&self, shard_messenger: &ShardMessenger) -> ReactionCollector {
+    pub fn await_reaction(&self, shard_messenger: impl AsRef<ShardMessenger>) -> ReactionCollector {
         ReactionCollector::new(shard_messenger).guild_id(self.id)
+    }
+
+    /// Same as [`Self::await_reaction`].
+    #[cfg(feature = "collector")]
+    pub fn await_reactions(
+        &self,
+        shard_messenger: impl AsRef<ShardMessenger>,
+    ) -> ReactionCollector {
+        self.await_reaction(shard_messenger)
     }
 
     /// Gets the guild active threads.
