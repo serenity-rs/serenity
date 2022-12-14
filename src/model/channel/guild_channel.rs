943293d9d88ac5d8bdb76e2379c982e24d0b1dbd
kangalioo <jannik.a.schaper@web.de>
2022-12-14 23:02:57 +0100

    Make collector API backwards compatible (#2309)
    By:
    - renaming foo_collector back to await_foo and await_foos
    - adding IntoFuture impl on collector types
    - and a deprecated `.build()` method that just redirects to `.stream()`

--- src/model/channel/guild_channel.rs
+++ src/model/channel/guild_channel.rs
@@ -1104,14 +1104,29 @@ impl GuildChannel {
 
     /// Returns a builder which can be awaited to obtain a message or stream of messages sent in this guild channel.
     #[cfg(feature = "collector")]
-    pub fn reply_collector(&self, shard_messenger: &ShardMessenger) -> MessageCollector {
+    pub fn await_reply(&self, shard_messenger: impl AsRef<ShardMessenger>) -> MessageCollector {
         MessageCollector::new(shard_messenger).channel_id(self.id)
+    }
+
+    /// Same as [`Self::await_reply`].
+    #[cfg(feature = "collector")]
+    pub fn await_replies(&self, shard_messenger: impl AsRef<ShardMessenger>) -> MessageCollector {
+        self.await_reply(shard_messenger)
     }
 
     /// Returns a stream builder which can be awaited to obtain a reaction or stream of reactions sent by this guild channel.
     #[cfg(feature = "collector")]
-    pub fn reaction_collector(&self, shard_messenger: &ShardMessenger) -> ReactionCollector {
+    pub fn await_reaction(&self, shard_messenger: impl AsRef<ShardMessenger>) -> ReactionCollector {
         ReactionCollector::new(shard_messenger).channel_id(self.id)
+    }
+
+    /// Same as [`Self::await_reaction`].
+    #[cfg(feature = "collector")]
+    pub fn await_reactions(
+        &self,
+        shard_messenger: impl AsRef<ShardMessenger>,
+    ) -> ReactionCollector {
+        self.await_reaction(shard_messenger)
     }
 
     /// Creates a webhook in the channel.
