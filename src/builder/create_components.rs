33adfaf49e7dbbd2ff23a5b3fb9e961f59164737
Forest Anderson <forestkzanderson@gmail.com>
2022-12-27 19:54:50 -0500

    Add custom_id method to CreateButton builder

--- src/builder/create_components.rs
+++ src/builder/create_components.rs
@@ -71,6 +71,19 @@ impl CreateButton {
         })
     }
 
+    /// Sets the custom id of the button, a developer-defined identifier. Replaces the current value
+    /// as set in [`Self::new`].
+    pub fn custom_id(mut self, id: impl Into<String>) -> Self {
+        // Make sure we're not a link button
+        if let ButtonKind::NonLink {
+            custom_id, ..
+        } = &mut self.0.data
+        {
+            *custom_id = id.into();
+        }
+        self
+    }
+
     /// Sets the style of this button.
     ///
     /// Has no effect on link buttons.
