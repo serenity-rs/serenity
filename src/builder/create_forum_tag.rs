1517f6156bb97358a803aad018623ca929797a3f
kangalio <jannik.a.schaper@web.de>
2023-04-19 13:20:12 +0200

    Support editing a forum's available tags and a forum post's applied tags (#2385)

---
+++ src/builder/create_forum_tag.rs
@@ -0,0 +1,42 @@
+use crate::model::prelude::*;
+
+#[must_use]
+#[derive(Clone, Debug, Serialize)]
+pub struct CreateForumTag {
+    name: String,
+    moderated: bool,
+    emoji_id: Option<EmojiId>,
+    emoji_name: Option<String>,
+}
+
+impl CreateForumTag {
+    pub fn new(name: impl Into<String>) -> Self {
+        Self {
+            name: name.into(),
+            moderated: false,
+            emoji_id: None,
+            emoji_name: None,
+        }
+    }
+
+    pub fn moderated(mut self, moderated: bool) -> Self {
+        self.moderated = moderated;
+        self
+    }
+
+    pub fn emoji(mut self, emoji: impl Into<ReactionType>) -> Self {
+        match emoji.into() {
+            ReactionType::Custom {
+                id, ..
+            } => {
+                self.emoji_id = Some(id);
+                self.emoji_name = None;
+            },
+            ReactionType::Unicode(unicode_emoji) => {
+                self.emoji_id = None;
+                self.emoji_name = Some(unicode_emoji);
+            },
+        }
+        self
+    }
+}
