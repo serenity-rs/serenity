b7d338faa8983d73d9295f0904f120b342f63356
kangalioo <jannik.a.schaper@web.de>
2023-04-21 10:22:19 +0200

    Update ApplicationFlags flags
    Incorporated Discord doc text
    Added APPLICATION_AUTO_MODERATION_RULE_CREATE_BADGE and APPLICATION_COMMAND_BADGE
    Fixed bitflag integers for VERIFICATION_PENDING_GUILD_LIMIT, EMBEDDED, GATEWAY_MESSAGE_CONTENT, GATEWAY_MESSAGE_CONTENT_LIMITED

--- src/model/application/mod.rs
+++ src/model/application/mod.rs
@@ -125,14 +125,32 @@ bitflags! {
     /// [Discord docs](https://discord.com/developers/docs/resources/application#application-object-application-flags).
     #[derive(Default)]
     pub struct ApplicationFlags: u64 {
+        /// Indicates if an app uses the Auto Moderation API
+        const APPLICATION_AUTO_MODERATION_RULE_CREATE_BADGE = 1 << 6;
+        /// Intent required for bots in 100 or more servers to receive presence_update events
         const GATEWAY_PRESENCE = 1 << 12;
+        /// Intent required for bots in under 100 servers to receive presence_update events, found
+        /// on the Bot page in your app's settings
         const GATEWAY_PRESENCE_LIMITED = 1 << 13;
+        /// Intent required for bots in 100 or more servers to receive member-related events like
+        /// guild_member_add. See the list of member-related events under [GUILD_MEMBERS](https://discord.com/developers/docs/topics/gateway#list-of-intents)
         const GATEWAY_GUILD_MEMBERS = 1 << 14;
+        /// Intent required for bots in under 100 servers to receive member-related events like
+        /// guild_member_add, found on the Bot page in your app's settings. See the list of
+        /// member-related events under [GUILD_MEMBERS](https://discord.com/developers/docs/topics/gateway#list-of-intents)
         const GATEWAY_GUILD_MEMBERS_LIMITED = 1 << 15;
-        const VERIFICATION_PENDING_GUILD_LIMIT = 2 << 16;
-        const EMBEDDED = 2 << 17;
-        const GATEWAY_MESSAGE_CONTENT = 2 << 18;
-        const GATEWAY_MESSAGE_CONTENT_LIMITED = 2 << 19;
+        /// Indicates unusual growth of an app that prevents verification
+        const VERIFICATION_PENDING_GUILD_LIMIT = 1 << 16;
+        /// Indicates if an app is embedded within the Discord client (currently unavailable
+        /// publicly)
+        const EMBEDDED = 1 << 17;
+        /// Intent required for bots in 100 or more servers to receive [message content](https://support-dev.discord.com/hc/en-us/articles/4404772028055).
+        const GATEWAY_MESSAGE_CONTENT = 1 << 18;
+        /// Intent required for bots in under 100 servers to receive [message content](https://support-dev.discord.com/hc/en-us/articles/4404772028055),
+        /// found on the Bot page in your app's settings
+        const GATEWAY_MESSAGE_CONTENT_LIMITED = 1 << 19;
+        /// Indicates if an app has registered global application commands
+        const APPLICATION_COMMAND_BADGE = 1 << 19;
     }
 }
 
