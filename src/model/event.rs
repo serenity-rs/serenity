6ce6671cc25e911c0984e2990c920e0fc73a844f
kangalioo <jannik.a.schaper@web.de>
2023-04-21 04:20:05 +0200

    Add 5 new InviteCreateEvent fields
    And integrate Discord docs text
    
    New fields: created_at, target_type, target_user, target_application, uses

--- src/model/event.rs
+++ src/model/event.rs
@@ -367,13 +367,31 @@ pub struct GuildStickersUpdateEvent {
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[non_exhaustive]
 pub struct InviteCreateEvent {
+    /// Whether or not the invite is temporary (invited users will be kicked on disconnect unless
+    /// Channel the invite is for.
     pub channel_id: ChannelId,
+    /// Unique invite [code](Invite::code).
     pub code: String,
+    /// Time at which the invite was created.
+    pub created_at: Timestamp,
+    /// Guild of the invite.
     pub guild_id: Option<GuildId>,
+    /// User that created the invite.
     pub inviter: Option<User>,
+    /// How long the invite is valid for (in seconds).
     pub max_age: u64,
+    /// Maximum number of times the invite can be used.
     pub max_uses: u64,
+    /// Type of target for this voice channel invite.
+    pub target_type: Option<InviteTargetType>,
+    /// User whose stream to display for this voice channel stream invite.
+    pub target_user: Option<User>,
+    /// Embedded application to open for this voice channel embedded application invite.
+    pub target_application: Option<serde_json::Value>,
+    /// they're assigned a role).
     pub temporary: bool,
+    /// How many times the invite has been used (always will be 0).
+    pub uses: u64,
 }
 
 /// Requires [`GatewayIntents::GUILD_INVITES`].
