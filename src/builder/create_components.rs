4a88111f676a2a987f7000dc85d60e7ccd2d994d
kangalio <jannik.a.schaper@web.de>
2023-05-10 23:50:50 +0200

    Make `InputText::{label, style}` optional (#2423)
    Discord says they are required, implied for both send and receive direction, but they're only required for sending. When receiving, they are apparently missing.
    
    https://github.com/discord/discord-api-docs/issues/6141

--- src/builder/create_components.rs
+++ src/builder/create_components.rs
@@ -301,8 +301,8 @@ impl CreateInputText {
         custom_id: impl Into<String>,
     ) -> Self {
         Self(InputText {
-            style,
-            label: label.into(),
+            style: Some(style),
+            label: Some(label.into()),
             custom_id: custom_id.into(),
 
             placeholder: None,
@@ -317,13 +317,13 @@ impl CreateInputText {
 
     /// Sets the style of this input text. Replaces the current value as set in [`Self::new`].
     pub fn style(mut self, kind: InputTextStyle) -> Self {
-        self.0.style = kind;
+        self.0.style = Some(kind);
         self
     }
 
     /// Sets the label of this input text. Replaces the current value as set in [`Self::new`].
     pub fn label(mut self, label: impl Into<String>) -> Self {
-        self.0.label = label.into();
+        self.0.label = Some(label.into());
         self
     }
 
