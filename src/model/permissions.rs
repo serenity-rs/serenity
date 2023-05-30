df4f6509be467fd5d4d0f4c1c9c0547e1650f6a4
Michael Krasnitski <42564254+mkrasnitski@users.noreply.github.com>
2023-05-30 19:21:49 +0900

    Add `Permissions::USE_EXTERNAL_SOUNDS` (#2451)

--- src/model/permissions.rs
+++ src/model/permissions.rs
@@ -343,6 +343,8 @@ bitflags::bitflags! {
         const VIEW_CREATOR_MONETIZATION_ANALYTICS = 1 << 41;
         /// Allows for using soundboard in a voice channel.
         const USE_SOUNDBOARD = 1 << 42;
+        /// Allows the usage of custom soundboard sounds from other servers.
+        const USE_EXTERNAL_SOUNDS = 1 << 45;
         /// Allows sending voice messages.
         const SEND_VOICE_MESSAGES = 1 << 46;
     }
