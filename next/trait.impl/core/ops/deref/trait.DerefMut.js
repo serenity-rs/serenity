c94658ed9cbdecac3c67e69bfb4777ee37d557ad
kangalio <kangalio@users.noreply.github.com>
2023-11-01 13:47:07 +0000

    deploy: 2a81062598cfc999bcc0f2e9ccdfbc700f09f2a0

---
+++ next/trait.impl/core/ops/deref/trait.DerefMut.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"serenity":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"serenity/model/user/struct.CurrentUser.html\" title=\"struct serenity::model::user::CurrentUser\">CurrentUser</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
