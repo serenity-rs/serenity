serenity-rs/serenity

commit a4439f45d3d94d42701cc8e3c71e593a27486a6e

Remove duplicate channels map (#2602)
When a channel was received from the gateway it was cloned to be inserted into
`cache.channels` and the `cache.guild.id.channels` maps, and since channels are
the most cached model (around 7.3m channel models cached in my 230k server bot)
this lead to a massive waste of memory (11.8gb of wasted ram in my extreme
case).

This commit changes that map to only store the guild id that the channel
originated from, which reduces ram usage by around half on my bot.

To accomplish this, I added in `CacheRefInner::MappedDashRef`, which stores a
mapped dashmap ref, which in future can be used to remove the remaining
`_field` methods.

I also took the liberty to remove the `category` methods that were cloning tons
of channels and to implement using this method would cause massive cache
contention for no real world use?

src/utils/argument_convert/channel.rs
@@ -49,17 +49,17 @@ async fn lookup_channel_global(
         return channel_id.to_channel(&ctx).await.map_err(ChannelParseError::Http);
     }
 
+    let guild_id = guild_id.ok_or(ChannelParseError::NotFoundOrMalformed)?;
+
     #[cfg(feature = "cache")]
     if let Some(cache) = ctx.cache() {
-        if let Some(channel) = cache.channels.iter().find_map(|m| {
-            let channel = m.value();
-            channel.name.eq_ignore_ascii_case(s).then(|| channel.clone())
-        }) {
-            return Ok(Channel::Guild(channel));
+        if let Some(guild) = cache.guild(guild_id) {
+            let channel = guild.channels.values().find(|c| c.name.eq_ignore_ascii_case(s));
+            if let Some(channel) = channel {
+                return Ok(Channel::Guild(channel.clone()));
+            }
         }
-    }
-
-    if let Some(guild_id) = guild_id {
+    } else {
         let channels = ctx.http().get_channels(guild_id).await.map_err(ChannelParseError::Http)?;
         if let Some(channel) =
             channels.into_iter().find(|channel| channel.name.eq_ignore_ascii_case(s))

src/cache/event.rs
@@ -50,8 +50,7 @@ impl CacheUpdate for ChannelCreateEvent {
                     .get_mut(&guild_id)
                     .and_then(|mut g| g.channels.insert(channel_id, channel.clone()));
 
-                cache.channels.insert(channel_id, channel.clone());
-
+                cache.channels.insert(channel_id, channel.guild_id);
                 old_channel.map(Channel::Guild)
             },
             Channel::Private(ref mut channel) => {
@@ -114,14 +113,12 @@ impl CacheUpdate for ChannelUpdateEvent {
 
         match &self.channel {
             Channel::Guild(channel) => {
-                let (guild_id, channel_id) = (channel.guild_id, channel.id);
-
-                cache.channels.insert(channel_id, channel.clone());
+                cache.channels.insert(channel.id, channel.guild_id);
 
                 cache
                     .guilds
-                    .get_mut(&guild_id)
-                    .map(|mut g| g.channels.insert(channel_id, channel.clone()));
+                    .get_mut(&channel.guild_id)
+                    .map(|mut g| g.channels.insert(channel.id, channel.clone()));
             },
             Channel::Private(channel) => {
                 if let Some(mut c) = cache.private_channels.get_mut(&channel.id) {
@@ -138,7 +135,7 @@ impl CacheUpdate for ChannelPinsUpdateEvent {
     type Output = ();
 
     fn update(&mut self, cache: &Cache) -> Option<()> {
-        if let Some(mut channel) = cache.channels.get_mut(&self.channel_id) {
+        if let Some(mut channel) = cache.guild_channel_mut(self.channel_id) {
             channel.last_pin_timestamp = self.last_pin_timestamp;
 
             return None;
@@ -168,11 +165,10 @@ impl CacheUpdate for GuildCreateEvent {
             }
         }
 
-        for (id, channel) in guild.channels.clone() {
-            cache.channels.insert(id, channel);
-        }
-
         cache.guilds.insert(self.guild.id, guild);
+        for channel_id in self.guild.channels.keys() {
+            cache.channels.insert(*channel_id, self.guild.id);
+        }
 
         None
     }
@@ -630,7 +626,7 @@ impl CacheUpdate for VoiceChannelStatusUpdateEvent {
     type Output = String;
 
     fn update(&mut self, cache: &Cache) -> Option<Self::Output> {
-        if let Some(mut channel) = cache.channels.get_mut(&self.id) {
+        if let Some(mut channel) = cache.guild_channel_mut(self.id) {
             let old = channel.status.clone();
             channel.status = self.status.clone();
             // Discord updates topic but doesn't fire ChannelUpdate.

src/cache/mod.rs
@@ -34,7 +34,7 @@ use std::sync::Arc;
 use std::time::Duration;
 
 use dashmap::mapref::entry::Entry;
-use dashmap::mapref::one::Ref;
+use dashmap::mapref::one::{MappedRef, MappedRefMut, Ref};
 use dashmap::DashMap;
 #[cfg(feature = "temp_cache")]
 use mini_moka::sync::Cache as MokaCache;
@@ -56,20 +56,21 @@ type MessageCache = DashMap<ChannelId, HashMap<MessageId, Message>, BuildHasher>
 
 struct NotSend;
 
-enum CacheRefInner<'a, K, V> {
+enum CacheRefInner<'a, K, V, T> {
     #[cfg(feature = "temp_cache")]
     Arc(Arc<V>),
     DashRef(Ref<'a, K, V, BuildHasher>),
+    DashMappedRef(MappedRef<'a, K, T, V, BuildHasher>),
     ReadGuard(parking_lot::RwLockReadGuard<'a, V>),
 }
 
-pub struct CacheRef<'a, K, V> {
-    inner: CacheRefInner<'a, K, V>,
+pub struct CacheRef<'a, K, V, T = ()> {
+    inner: CacheRefInner<'a, K, V, T>,
     phantom: std::marker::PhantomData<*const NotSend>,
 }
 
-impl<'a, K, V> CacheRef<'a, K, V> {
-    fn new(inner: CacheRefInner<'a, K, V>) -> Self {
+impl<'a, K, V, T> CacheRef<'a, K, V, T> {
+    fn new(inner: CacheRefInner<'a, K, V, T>) -> Self {
         Self {
             inner,
             phantom: std::marker::PhantomData,
@@ -85,12 +86,16 @@ impl<'a, K, V> CacheRef<'a, K, V> {
         Self::new(CacheRefInner::DashRef(inner))
     }
 
+    fn from_mapped_ref(inner: MappedRef<'a, K, T, V, BuildHasher>) -> Self {
+        Self::new(CacheRefInner::DashMappedRef(inner))
+    }
+
     fn from_guard(inner: parking_lot::RwLockReadGuard<'a, V>) -> Self {
         Self::new(CacheRefInner::ReadGuard(inner))
     }
 }
 
-impl<K: Eq + Hash, V> std::ops::Deref for CacheRef<'_, K, V> {
+impl<K: Eq + Hash, V, T> std::ops::Deref for CacheRef<'_, K, V, T> {
     type Target = V;
 
     fn deref(&self) -> &Self::Target {
@@ -98,6 +103,7 @@ impl<K: Eq + Hash, V> std::ops::Deref for CacheRef<'_, K, V> {
             #[cfg(feature = "temp_cache")]
             CacheRefInner::Arc(inner) => inner,
             CacheRefInner::DashRef(inner) => inner.value(),
+            CacheRefInner::DashMappedRef(inner) => inner.value(),
             CacheRefInner::ReadGuard(inner) => inner,
         }
     }
@@ -106,8 +112,8 @@ impl<K: Eq + Hash, V> std::ops::Deref for CacheRef<'_, K, V> {
 pub type UserRef<'a> = CacheRef<'a, UserId, User>;
 pub type GuildRef<'a> = CacheRef<'a, GuildId, Guild>;
 pub type CurrentUserRef<'a> = CacheRef<'a, (), CurrentUser>;
-pub type GuildChannelRef<'a> = CacheRef<'a, ChannelId, GuildChannel>;
 pub type PrivateChannelRef<'a> = CacheRef<'a, ChannelId, PrivateChannel>;
+pub type GuildChannelRef<'a> = CacheRef<'a, GuildId, GuildChannel, Guild>;
 pub type ChannelMessagesRef<'a> = CacheRef<'a, ChannelId, HashMap<MessageId, Message>>;
 
 #[derive(Debug)]
@@ -153,12 +159,8 @@ pub struct Cache {
     pub(crate) temp_users: MokaCache<UserId, Arc<User>, BuildHasher>,
 
     // Channels cache:
-    // ---
-    /// A map of channels in [`Guild`]s that the current user has received data for.
-    ///
-    /// When a [`Event::GuildDelete`] is received and processed by the cache, the relevant channels
-    /// are also removed from this map.
-    pub(crate) channels: MaybeMap<ChannelId, GuildChannel>,
+    /// A map of channel ids to the guilds in which the channel data is stored.
+    pub(crate) channels: MaybeMap<ChannelId, GuildId>,
     /// A map of direct message channels that the current user has open with other users.
     pub(crate) private_channels: MaybeMap<ChannelId, PrivateChannel>,
 
@@ -400,9 +402,8 @@ impl Cache {
     }
 
     fn _channel(&self, id: ChannelId) -> Option<Channel> {
-        if let Some(channel) = self.channels.get(&id) {
-            let channel = channel.clone();
-            return Some(Channel::Guild(channel));
+        if let Some(channel) = self.guild_channel(id) {
+            return Some(Channel::Guild(channel.clone()));
         }
 
         #[cfg(feature = "temp_cache")]
@@ -523,7 +524,18 @@ impl Cache {
     }
 
     fn _guild_channel(&self, id: ChannelId) -> Option<GuildChannelRef<'_>> {
-        self.channels.get(&id).map(CacheRef::from_ref)
+        let guild_id = *self.channels.get(&id)?;
+        let guild_ref = self.guilds.get(&guild_id)?;
+        guild_ref.try_map(|g| g.channels.get(&id)).ok().map(CacheRef::from_mapped_ref)
+    }
+
+    pub(super) fn guild_channel_mut(
+        &self,
+        id: ChannelId,
+    ) -> Option<MappedRefMut<'_, GuildId, Guild, GuildChannel, BuildHasher>> {
+        let guild_id = *self.channels.get(&id)?;
+        let guild_ref = self.guilds.get_mut(&guild_id)?;
+        guild_ref.try_map(|g| g.channels.get_mut(&id)).ok()
     }
 
     /// Retrieves a [`Guild`]'s member from the cache based on the guild's and user's given Ids.
@@ -861,31 +873,19 @@ impl Cache {
         CacheRef::from_guard(self.user.read())
     }
 
-    /// Clones all channel categories and returns them.
-    pub fn categories(&self) -> DashMap<ChannelId, GuildChannel> {
-        self.channels
-            .iter()
-            .filter(|channel| channel.value().kind == ChannelType::Category)
-            .map(|channel| (*channel.key(), channel.value().clone()))
-            .collect()
-    }
-
-    /// Returns the number of channel categories.
-    pub fn category_count(&self) -> usize {
-        self.channels.iter().filter(|channel| channel.value().kind == ChannelType::Category).count()
-    }
-
-    /// Clones a channel category matching the given ID and returns it.
-    pub fn category(&self, channel_id: ChannelId) -> Option<CacheRef<'_, ChannelId, GuildChannel>> {
-        match self.channels.get(&channel_id) {
-            Some(c) if c.kind == ChannelType::Category => Some(CacheRef::from_ref(c)),
-            _ => None,
+    /// Returns a channel category matching the given ID
+    pub fn category(&self, channel_id: ChannelId) -> Option<GuildChannelRef<'_>> {
+        let channel = self.guild_channel(channel_id)?;
+        if channel.kind == ChannelType::Category {
+            Some(channel)
+        } else {
+            None
         }
     }
 
     /// Returns the parent category of the given channel ID.
     pub fn channel_category_id(&self, channel_id: ChannelId) -> Option<ChannelId> {
-        self.channels.get(&channel_id)?.parent_id
+        self.guild_channel(channel_id)?.parent_id
     }
 
     /// Clones all channel categories in the given guild and returns them.

src/utils/content_safe.rs
@@ -313,11 +313,12 @@ mod tests {
 
         let cache = Arc::new(Cache::default());
 
+        guild.channels.insert(channel.id, channel.clone());
         guild.members.insert(user.id, member.clone());
         guild.roles.insert(role.id, role);
         cache.users.insert(user.id, user.clone());
         cache.guilds.insert(guild.id, guild.clone());
-        cache.channels.insert(channel.id, channel);
+        cache.channels.insert(channel.id, guild.id);
 
         let with_user_mentions = "<@!100000000000000000> <@!000000000000000000> <@123> <@!123> \
         <@!123123123123123123123> <@123> <@123123123123123123> <@!invalid> \

commit 74548686b3f07ff2fe77e84116205f0d9bb6cd37

Fix `Channel`'s `argument_convert` impl when cache is disabled (#2627)
Broken in #2602, that PR moved the `#[cfg(feature = "cache")]` around
the entire if else block, which meant the else statement didn't get
compiled in if cache was disabled. Caught this by running `cargo check`
with cache disabled and seeing that `guild_id` was unused.

src/utils/argument_convert/channel.rs
@@ -59,16 +59,16 @@ async fn lookup_channel_global(
                 return Ok(Channel::Guild(channel.clone()));
             }
         }
-    } else {
-        let channels = ctx.http().get_channels(guild_id).await.map_err(ChannelParseError::Http)?;
-        if let Some(channel) =
-            channels.into_iter().find(|channel| channel.name.eq_ignore_ascii_case(s))
-        {
-            return Ok(Channel::Guild(channel));
-        }
+
+        return Err(ChannelParseError::NotFoundOrMalformed);
     }
 
-    Err(ChannelParseError::NotFoundOrMalformed)
+    let channels = ctx.http().get_channels(guild_id).await.map_err(ChannelParseError::Http)?;
+    if let Some(channel) = channels.into_iter().find(|c| c.name.eq_ignore_ascii_case(s)) {
+        Ok(Channel::Guild(channel))
+    } else {
+        Err(ChannelParseError::NotFoundOrMalformed)
+    }
 }
 
 /// Look up a Channel by a string case-insensitively.