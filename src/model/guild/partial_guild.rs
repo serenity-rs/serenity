ad58db6a69b4c0abe06e718a9e138e797e84d020
Gnome! <david2005thomas@Gmail.com>
2023-12-03 19:33:06 +0000

    Remove deprecated items (#2645)

--- src/model/guild/partial_guild.rs
+++ src/model/guild/partial_guild.rs
@@ -1361,17 +1361,6 @@ impl PartialGuild {
         Guild::_user_permissions_in(Some(channel), member, &self.roles, self.owner_id, self.id)
     }
 
-    /// Calculate a [`Role`]'s permissions in a given channel in the guild.
-    ///
-    /// # Errors
-    ///
-    /// Returns [`Error::Model`] if the [`Role`] or [`Channel`] is not from this [`Guild`].
-    #[inline]
-    #[deprecated = "this function ignores other roles the user may have as well as user-specific permissions; use user_permissions_in instead"]
-    pub fn role_permissions_in(&self, channel: &GuildChannel, role: &Role) -> Result<Permissions> {
-        Guild::_role_permissions_in(channel, role, self.id)
-    }
-
     /// Gets the number of [`Member`]s that would be pruned with the given number of days.
     ///
     /// Requires the [Kick Members] permission.
