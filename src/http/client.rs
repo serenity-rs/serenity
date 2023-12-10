02a461fde2239a65f9c44de9f64c5eaff1ac40cf
Gnome! <david2005thomas@Gmail.com>
2023-12-10 22:36:23 +0000

    Replace `Vec` and `String` with `FixedArray` and `FixedString` for all models (#2656)
    This shrinks type sizes by a lot; however, it makes the user experience slightly
    different:
    
    - `FixedString` must be converted to String with `.into()` or `.into_string()`
      before it can be pushed to, but dereferences to `&str` as is.
    - `FixedArray` must be converted to `Vec` with `.into()` or `.into_vec()`
      before it can be pushed to, but dereferences to `&[T]` as is.
    
    The crate of these types is currently a Git dependency, but this is fine for
    the `next` branch. It needs some basic testing, which Serenity is perfect for,
    before a release will be made to crates.io.

--- src/http/client.rs
+++ src/http/client.rs
@@ -985,7 +985,7 @@ impl Http {
             route: Route::Entitlements {
                 application_id: self.try_application_id()?,
             },
-            params: None,
+            params: [].into(),
         })
         .await
     }
@@ -1494,7 +1494,7 @@ impl Http {
                 application_id: self.try_application_id()?,
                 entitlement_id,
             },
-            params: None,
+            params: [].into(),
         })
         .await
     }
@@ -3392,7 +3392,8 @@ impl Http {
         guild_id: Option<GuildId>,
         exclude_ended: Option<bool>,
     ) -> Result<Vec<Entitlement>> {
-        let mut params = vec![];
+        let mut params = ArrayVec::<_, 7>::new();
+
         if let Some(user_id) = user_id {
             params.push(("user_id", user_id.to_string()));
         }
@@ -3426,7 +3427,7 @@ impl Http {
             route: Route::Entitlements {
                 application_id: self.try_application_id()?,
             },
-            params: Some(params),
+            params,
         })
         .await
     }
@@ -4295,7 +4296,7 @@ impl Http {
             route: Route::Skus {
                 application_id: self.try_application_id()?,
             },
-            params: None,
+            params: [].into(),
         })
         .await
     }
