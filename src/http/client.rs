e3392270824b76e9c233b11a286f0a6b840dd567
Clément Renault <renault.cle@gmail.com>
2023-12-22 18:17:13 +0100

    Encode timestamps in RFC3339 (#2665)
    Fixes #2664

--- src/http/client.rs
+++ src/http/client.rs
@@ -3005,7 +3005,7 @@ impl Http {
     pub async fn get_channel_archived_public_threads(
         &self,
         channel_id: ChannelId,
-        before: Option<u64>,
+        before: Option<Timestamp>,
         limit: Option<u64>,
     ) -> Result<ThreadsData> {
         let mut params = ArrayVec::<_, 2>::new();
@@ -3033,7 +3033,7 @@ impl Http {
     pub async fn get_channel_archived_private_threads(
         &self,
         channel_id: ChannelId,
-        before: Option<u64>,
+        before: Option<Timestamp>,
         limit: Option<u64>,
     ) -> Result<ThreadsData> {
         let mut params = ArrayVec::<_, 2>::new();
@@ -3061,7 +3061,7 @@ impl Http {
     pub async fn get_channel_joined_archived_private_threads(
         &self,
         channel_id: ChannelId,
-        before: Option<u64>,
+        before: Option<ChannelId>,
         limit: Option<u64>,
     ) -> Result<ThreadsData> {
         let mut params = ArrayVec::<_, 2>::new();
