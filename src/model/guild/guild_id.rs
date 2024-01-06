763567c38bd894cfb86fa14eb1d01a184ecdd913
Gnome! <david2005thomas@Gmail.com>
2024-01-06 00:14:25 +0000

    Rewrite builders to take `Cow`s (#2688)

--- src/model/guild/guild_id.rs
+++ src/model/guild/guild_id.rs
@@ -170,7 +170,7 @@ impl GuildId {
         self,
         cache_http: impl CacheHttp,
         user_id: impl Into<UserId>,
-        builder: AddMember,
+        builder: AddMember<'_>,
     ) -> Result<Option<Member>> {
         builder.execute(cache_http, (self, user_id.into())).await
     }
@@ -1523,7 +1523,7 @@ impl GuildId {
     pub async fn create_command(
         self,
         cache_http: impl CacheHttp,
-        builder: CreateCommand,
+        builder: CreateCommand<'_>,
     ) -> Result<Command> {
         builder.execute(cache_http, (Some(self), None)).await
     }
@@ -1536,7 +1536,7 @@ impl GuildId {
     pub async fn set_commands(
         self,
         http: impl AsRef<Http>,
-        commands: Vec<CreateCommand>,
+        commands: &[CreateCommand<'_>],
     ) -> Result<Vec<Command>> {
         http.as_ref().create_guild_commands(self, &commands).await
     }
@@ -1552,7 +1552,7 @@ impl GuildId {
         self,
         cache_http: impl CacheHttp,
         command_id: CommandId,
-        builder: EditCommandPermissions,
+        builder: EditCommandPermissions<'_>,
     ) -> Result<CommandPermissions> {
         builder.execute(cache_http, (self, command_id)).await
     }
@@ -1600,7 +1600,7 @@ impl GuildId {
         self,
         cache_http: impl CacheHttp,
         command_id: CommandId,
-        builder: CreateCommand,
+        builder: CreateCommand<'_>,
     ) -> Result<Command> {
         builder.execute(cache_http, (Some(self), Some(command_id))).await
     }
