d865b5f351259c485f514b3d2abfb856749bb2d0
Gnome! <david2005thomas@Gmail.com>
2024-01-06 00:14:25 +0000

    Rewrite builders to take `Cow`s (#2688)

--- src/model/guild/mod.rs
+++ src/model/guild/mod.rs
@@ -573,7 +573,7 @@ impl Guild {
         &self,
         cache_http: impl CacheHttp,
         user_id: impl Into<UserId>,
-        builder: AddMember,
+        builder: AddMember<'_>,
     ) -> Result<Option<Member>> {
         self.id.add_member(cache_http, user_id, builder).await
     }
@@ -753,7 +753,7 @@ impl Guild {
     pub async fn create_command(
         &self,
         cache_http: impl CacheHttp,
-        builder: CreateCommand,
+        builder: CreateCommand<'_>,
     ) -> Result<Command> {
         self.id.create_command(cache_http, builder).await
     }
@@ -766,7 +766,7 @@ impl Guild {
     pub async fn set_commands(
         &self,
         http: impl AsRef<Http>,
-        commands: Vec<CreateCommand>,
+        commands: &[CreateCommand<'_>],
     ) -> Result<Vec<Command>> {
         self.id.set_commands(http, commands).await
     }
@@ -784,7 +784,7 @@ impl Guild {
         &self,
         cache_http: impl CacheHttp,
         command_id: CommandId,
-        builder: EditCommandPermissions,
+        builder: EditCommandPermissions<'_>,
     ) -> Result<CommandPermissions> {
         self.id.edit_command_permissions(cache_http, command_id, builder).await
     }
@@ -834,7 +834,7 @@ impl Guild {
         &self,
         cache_http: impl CacheHttp,
         command_id: CommandId,
-        builder: CreateCommand,
+        builder: CreateCommand<'_>,
     ) -> Result<Command> {
         self.id.edit_command(cache_http, command_id, builder).await
     }
--- src/model/guild/partial_guild.rs
+++ src/model/guild/partial_guild.rs
@@ -505,7 +505,7 @@ impl PartialGuild {
     pub async fn create_command(
         &self,
         cache_http: impl CacheHttp,
-        builder: CreateCommand,
+        builder: CreateCommand<'_>,
     ) -> Result<Command> {
         self.id.create_command(cache_http, builder).await
     }
@@ -518,7 +518,7 @@ impl PartialGuild {
     pub async fn set_commands(
         &self,
         http: impl AsRef<Http>,
-        commands: Vec<CreateCommand>,
+        commands: &[CreateCommand<'_>],
     ) -> Result<Vec<Command>> {
         self.id.set_commands(http, commands).await
     }
@@ -536,7 +536,7 @@ impl PartialGuild {
         &self,
         cache_http: impl CacheHttp,
         command_id: CommandId,
-        builder: EditCommandPermissions,
+        builder: EditCommandPermissions<'_>,
     ) -> Result<CommandPermissions> {
         self.id.edit_command_permissions(cache_http, command_id, builder).await
     }
@@ -586,7 +586,7 @@ impl PartialGuild {
         &self,
         cache_http: impl CacheHttp,
         command_id: CommandId,
-        builder: CreateCommand,
+        builder: CreateCommand<'_>,
     ) -> Result<Command> {
         self.id.edit_command(cache_http, command_id, builder).await
     }
