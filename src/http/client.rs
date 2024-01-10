0d4374fb6d52bf78b48fcaf9e63c74d184e2b2ef
Gnome! <david2005thomas@Gmail.com>
2024-01-10 21:11:34 +0000

    Get rid of `IntoIterator` generics wherever they are inefficent (#2698)
    This removes inefficient `IntoIterator` generics and instead takes what is
    actually required. I also reworked `reorder_channels` to allow for keeping the
    generic, as it actually does only just need iterator semantics.

--- src/http/client.rs
+++ src/http/client.rs
@@ -948,7 +948,7 @@ impl Http {
     pub async fn create_sticker(
         &self,
         guild_id: GuildId,
-        map: impl IntoIterator<Item = (&'static str, Cow<'static, str>)>,
+        fields: Vec<(Cow<'static, str>, Cow<'static, str>)>,
         file: CreateAttachment<'_>,
         audit_log_reason: Option<&str>,
     ) -> Result<Sticker> {
@@ -956,8 +956,8 @@ impl Http {
             body: None,
             multipart: Some(Multipart {
                 upload: MultipartUpload::File(file),
-                fields: map.into_iter().map(|(k, v)| (k.into(), v)).collect(),
                 payload_json: None,
+                fields,
             }),
             headers: audit_log_reason.map(reason_into_header),
             method: LightMethod::Post,
@@ -1270,7 +1270,7 @@ impl Http {
     pub async fn delete_messages(
         &self,
         channel_id: ChannelId,
-        map: &Value,
+        map: &impl serde::Serialize,
         audit_log_reason: Option<&str>,
     ) -> Result<()> {
         self.wind(204, Request {
@@ -1858,7 +1858,7 @@ impl Http {
     pub async fn edit_guild_channel_positions(
         &self,
         guild_id: GuildId,
-        value: &Value,
+        value: &impl serde::Serialize,
     ) -> Result<()> {
         let body = to_vec(value)?;
 
