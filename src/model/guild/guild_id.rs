4e15608edc8f6d2d8f1b550af9047d332ef9038c
Gnome! <david2005thomas@Gmail.com>
2024-01-10 21:11:34 +0000

    Get rid of `IntoIterator` generics wherever they are inefficent (#2698)
    This removes inefficient `IntoIterator` generics and instead takes what is
    actually required. I also reworked `reorder_channels` to allow for keeping the
    generic, as it actually does only just need iterator semantics.

--- src/model/guild/guild_id.rs
+++ src/model/guild/guild_id.rs
@@ -1256,18 +1256,18 @@ impl GuildId {
         http: impl AsRef<Http>,
         channels: impl IntoIterator<Item = (ChannelId, u64)>,
     ) -> Result<()> {
-        let items = channels
-            .into_iter()
-            .map(|(id, pos)| {
-                json!({
-                    "id": id,
-                    "position": pos,
-                })
-            })
-            .collect::<Vec<_>>()
-            .into();
+        #[derive(serde::Serialize)]
+        struct ChannelPosEdit {
+            id: ChannelId,
+            position: u64,
+        }
 
-        http.as_ref().edit_guild_channel_positions(self, &items).await
+        let iter = channels.into_iter().map(|(id, position)| ChannelPosEdit {
+            id,
+            position,
+        });
+
+        http.as_ref().edit_guild_channel_positions(self, &SerializeIter::new(iter)).await
     }
 
     /// Returns a list of [`Member`]s in a [`Guild`] whose username or nickname starts with a
