a24ca3505e7599eb4605de2deb7cdffb50dbc409
Gnome! <david2005thomas@Gmail.com>
2024-01-16 18:34:10 +0000

    Remove typemap without making everything generic (#2720)
    Replaces the typemap with `dyn Any`, which is what typemap used
    internally anyway. This gives the user more control over locking,
    removes boilerplate, and is less confusing for beginners to learn.

--- examples/e12_global_data/src/main.rs
+++ examples/e12_global_data/src/main.rs
@@ -2,11 +2,11 @@
 //! And how to use locks correctly to avoid deadlocking the bot.
 #![allow(deprecated)] // We recommend migrating to poise, instead of using the standard command framework.
 
-use std::collections::HashMap;
 use std::env;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
 
+use dashmap::DashMap;
 use serenity::async_trait;
 use serenity::framework::standard::macros::{command, group, hook};
 use serenity::framework::standard::{Args, CommandResult, Configuration, StandardFramework};
@@ -17,24 +17,9 @@ use serenity::prelude::*;
 // A container type is created for inserting into the Client's `data`, which allows for data to be
 // accessible across all events and framework commands, or anywhere else that has a copy of the
 // `data` Arc. These places are usually where either Context or Client is present.
-//
-// Documentation about TypeMap can be found here:
-// https://docs.rs/typemap_rev/0.1/typemap_rev/struct.TypeMap.html
-struct CommandCounter;
-
-impl TypeMapKey for CommandCounter {
-    type Value = Arc<RwLock<HashMap<String, u64>>>;
-}
-
-struct MessageCount;
-
-impl TypeMapKey for MessageCount {
-    // While you will be using RwLock or Mutex most of the time you want to modify data, sometimes
-    // it's not required; like for example, with static data, or if you are using other kinds of
-    // atomic operators.
-    //
-    // Arc should stay, to allow for the data lock to be closed early.
-    type Value = Arc<AtomicUsize>;
+struct UserData {
+    message_count: AtomicUsize,
+    command_counter: DashMap<String, u64>,
 }
 
 #[group]
@@ -45,28 +30,16 @@ struct General;
 async fn before(ctx: &Context, msg: &Message, command_name: &str) -> bool {
     println!("Running command '{}' invoked by '{}'", command_name, msg.author.tag());
 
-    let counter_lock = {
-        // While data is a RwLock, it's recommended that you always open the lock as read. This is
-        // mainly done to avoid Deadlocks for having a possible writer waiting for multiple readers
-        // to close.
-        let data_read = ctx.data.read().await;
-
-        // Since the CommandCounter Value is wrapped in an Arc, cloning will not duplicate the
-        // data, instead the reference is cloned.
-        // We wrap every value on in an Arc, as to keep the data lock open for the least time
-        // possible, to again, avoid deadlocking it.
-        data_read.get::<CommandCounter>().expect("Expected CommandCounter in TypeMap.").clone()
-    };
-
-    // Just like with client.data in main, we want to keep write locks open the least time
-    // possible, so we wrap them on a block so they get automatically closed at the end.
+    // We want to keep write locks open the least time possible, so we wrap them on a block so they
+    // get automatically closed at the end.
     {
-        // The HashMap of CommandCounter is wrapped in an RwLock; since we want to write to it, we
-        // will open the lock in write mode.
-        let mut counter = counter_lock.write().await;
+        // We have to provide the Data type each time we access Context::data.
+        let counter = &ctx.data::<UserData>().command_counter;
+
+        // The DashMap provides interior mutability, meaning we can write to it with a & reference.
+        let mut entry = counter.entry(command_name.to_string()).or_insert(0);
 
         // And we write the amount of times the command has been called to it.
-        let entry = counter.entry(command_name.to_string()).or_insert(0);
         *entry += 1;
     }
 
@@ -83,10 +56,7 @@ impl EventHandler for Handler {
             && msg.content.to_lowercase().contains("owo")
         {
             // Since data is located in Context, this means you are able to use it within events!
-            let count = {
-                let data_read = ctx.data.read().await;
-                data_read.get::<MessageCount>().expect("Expected MessageCount in TypeMap.").clone()
-            };
+            let count = &ctx.data::<UserData>().message_count;
 
             // Here, we are checking how many "owo" there are in the message content.
             let owo_in_msg = msg.content.to_ascii_lowercase().matches("owo").count();
@@ -107,6 +77,13 @@ impl EventHandler for Handler {
 async fn main() {
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
 
+    // We setup the initial value for our user data, which we will use throughout the rest of our
+    // program.
+    let data = UserData {
+        message_count: AtomicUsize::new(0),
+        command_counter: DashMap::new(),
+    };
+
     let framework = StandardFramework::new().before(before).group(&GENERAL_GROUP);
     framework.configure(Configuration::new().with_whitespace(true).prefix("~"));
 
@@ -116,36 +93,10 @@ async fn main() {
     let mut client = Client::builder(&token, intents)
         .event_handler(Handler)
         .framework(framework)
+        // We have to use `as` to turn our UserData into `dyn Any`.
+        .data(Arc::new(data) as _)
         .await
         .expect("Err creating client");
-
-    // This is where we can initially insert the data we desire into the "global" data TypeMap.
-    // client.data is wrapped on a RwLock, and since we want to insert to it, we have to open it in
-    // write mode, but there's a small thing catch: There can only be a single writer to a given
-    // lock open in the entire application, this means you can't open a new write lock until the
-    // previous write lock has closed. This is not the case with read locks, read locks can be open
-    // indefinitely, BUT as soon as you need to open the lock in write mode, all the read locks
-    // must be closed.
-    //
-    // You can find more information about deadlocks in the Rust Book, ch16-03:
-    // https://doc.rust-lang.org/book/ch16-03-shared-state.html
-    //
-    // All of this means that we have to keep locks open for the least time possible, so we put
-    // them inside a block, so they get closed automatically when dropped. If we don't do this, we
-    // would never be able to open the data lock anywhere else.
-    //
-    // Alternatively, you can also use `ClientBuilder::type_map_insert` or
-    // `ClientBuilder::type_map` to populate the global TypeMap without dealing with the RwLock.
-    {
-        // Open the data lock in write mode, so keys can be inserted to it.
-        let mut data = client.data.write().await;
-
-        // The CommandCounter Value has the type: Arc<RwLock<HashMap<String, u64>>>
-        // So, we have to insert the same type to it.
-        data.insert::<CommandCounter>(Arc::new(RwLock::new(HashMap::default())));
-
-        data.insert::<MessageCount>(Arc::new(AtomicUsize::new(0)));
-    }
 
     if let Err(why) = client.start().await {
         eprintln!("Client error: {why:?}");
@@ -173,22 +124,11 @@ async fn command_usage(ctx: &Context, msg: &Message, mut args: Args) -> CommandR
 
     // Yet again, we want to keep the locks open for the least time possible.
     let amount = {
-        // Since we only want to read the data and not write to it, we open it in read mode, and
-        // since this is open in read mode, it means that there can be multiple locks open at the
-        // same time, and as mentioned earlier, it's heavily recommended that you only open the
-        // data lock in read mode, as it will avoid a lot of possible deadlocks.
-        let data_read = ctx.data.read().await;
+        // We again, have to provide the UserData type.
+        let data = ctx.data::<UserData>();
 
-        // Then we obtain the value we need from data, in this case, we want the command counter.
-        // The returned value from get() is an Arc, so the reference will be cloned, rather than
-        // the data.
-        let command_counter_lock =
-            data_read.get::<CommandCounter>().expect("Expected CommandCounter in TypeMap.").clone();
-
-        let command_counter = command_counter_lock.read().await;
-        // And we return a usable value from it.
-        // This time, the value is not Arc, so the data will be cloned.
-        command_counter.get(&command_name).map_or(0, |x| *x)
+        // We fetch the value our of the map, temporarily holding a dashmap read lock.
+        data.command_counter.get(&command_name).map_or(0, |x| *x)
     };
 
     if amount == 0 {
@@ -206,12 +146,8 @@ async fn command_usage(ctx: &Context, msg: &Message, mut args: Args) -> CommandR
 
 #[command]
 async fn owo_count(ctx: &Context, msg: &Message) -> CommandResult {
-    let raw_count = {
-        let data_read = ctx.data.read().await;
-        data_read.get::<MessageCount>().expect("Expected MessageCount in TypeMap.").clone()
-    };
-
-    let count = raw_count.load(Ordering::Relaxed);
+    let data = ctx.data::<UserData>();
+    let count = data.message_count.load(Ordering::Relaxed);
 
     if count == 1 {
         msg.reply(
