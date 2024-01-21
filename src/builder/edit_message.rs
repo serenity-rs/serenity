f6b2b1b154287ba446b7bc19de93cc6835e9a26e
Michael Krasnitski <42564254+mkrasnitski@users.noreply.github.com>
2024-01-20 21:01:28 -0500

    Remove some pedantic lints from the whitelist (#2728)
    This also removes the impls of `From<{i32,u64}> for Colour`, as it only
    makes sense to convert infallibly from `u32`.

--- src/builder/edit_message.rs
+++ src/builder/edit_message.rs
@@ -251,7 +251,7 @@ impl Builder for EditMessage<'_> {
             }
         }
 
-        let files = self.attachments.as_mut().map_or(Vec::new(), |a| a.take_files());
+        let files = self.attachments.as_mut().map_or(Vec::new(), EditAttachments::take_files);
 
         cache_http.http().edit_message(ctx.0, ctx.1, &self, files).await
     }
