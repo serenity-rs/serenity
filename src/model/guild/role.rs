b8d734b5bf8abdb1758e5f88b30f86f3d6f6860b
Gnome! <david2005thomas@Gmail.com>
2024-01-26 12:16:34 +0000

    Fix RoleTags deserialisation after removal of simd-json (#2742)
    There was a comment here that said it was called by simd-json, so I
    removed the function, but it turns out that serde_json also calls it.

--- src/model/guild/role.rs
+++ src/model/guild/role.rs
@@ -221,6 +221,10 @@ mod bool_as_option_unit {
         fn visit_none<E: Error>(self) -> Result<Self::Value, E> {
             Ok(true)
         }
+
+        fn visit_unit<E: Error>(self) -> Result<Self::Value, E> {
+            Ok(true)
+        }
     }
 }
 
