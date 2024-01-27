f360efd81679f537af5b2e516e308fc1cfa2cb98
Gnome! <david2005thomas@Gmail.com>
2024-01-27 15:21:53 +0000

    Avoid cloning FullEvent in dispatch code (#2740)
    Avoids cloning entire event structs when dispatching. To reduce code
    size and allocations all the state is bundled up into a temporary struct
    called DispatchContext, but this isn't user facing at all. This
    regresses code size a bit, but is worth it for runtime performance, and
    probably reduces actual code sizes as `FullEvent` doesn't have to be
    moved around as much.

--- examples/e10_collectors/src/main.rs
+++ examples/e10_collectors/src/main.rs
@@ -44,7 +44,7 @@ struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn ready(&self, _: Context, ready: Ready) {
+    async fn ready(&self, _: &Context, ready: &Ready) {
         println!("{} is connected!", ready.user.name);
     }
 }
