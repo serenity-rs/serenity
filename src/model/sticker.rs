ad0cbb3f43f1848c53d7ebf09295108d568cc9e1
Gnome! <david2005thomas@Gmail.com>
2024-01-29 18:45:59 +0000

    Convert enum number into wrapper struct to save type sizes (#2746)
    Saves a couple bytes in each model by replacing the enums with a simpler
    and more accurate representation, a newtype with named statics.
    https://www.diffchecker.com/ohzayL9Z/

--- src/model/sticker.rs
+++ src/model/sticker.rs
@@ -214,7 +214,6 @@ enum_number! {
     /// [Discord docs](https://discord.com/developers/docs/resources/sticker#sticker-object-sticker-types).
     #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Deserialize, Serialize)]
     #[cfg_attr(feature = "typesize", derive(typesize::derive::TypeSize))]
-    #[serde(from = "u8", into = "u8")]
     #[non_exhaustive]
     pub enum StickerType {
         /// An official sticker in a pack, part of Nitro or in a removed purchasable pack.
@@ -231,7 +230,6 @@ enum_number! {
     /// [Discord docs](https://discord.com/developers/docs/resources/sticker#sticker-object-sticker-format-types).
     #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Deserialize, Serialize)]
     #[cfg_attr(feature = "typesize", derive(typesize::derive::TypeSize))]
-    #[serde(from = "u8", into = "u8")]
     #[non_exhaustive]
     pub enum StickerFormatType {
         /// A PNG format sticker.
@@ -252,7 +250,7 @@ fn sticker_url(sticker_id: StickerId, sticker_format_type: StickerFormatType) ->
         StickerFormatType::Png | StickerFormatType::Apng => "png",
         StickerFormatType::Lottie => "json",
         StickerFormatType::Gif => "gif",
-        StickerFormatType::Unknown(_) => return None,
+        StickerFormatType(_) => return None,
     };
 
     Some(cdn!("/stickers/{}.{}", sticker_id, ext))
