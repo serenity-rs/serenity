9cf7ffdd7d60f0743e628b6f965d62b25b98c617
Gnome! <david2005thomas@Gmail.com>
2024-01-29 19:29:23 +0000

    Remove standard framework (#2731)
    This PR carries out the removal after the deprecation made on current.

---
+++ examples/e06_env_logging/src/main.rs
@@ -0,0 +1,57 @@
+use std::env;
+
+use serenity::async_trait;
+use serenity::model::event::ResumedEvent;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+use tracing::{debug, error, info, instrument};
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: &Context, ready: &Ready) {
+        // Log at the INFO level. This is a macro from the `tracing` crate.
+        info!("{} is connected!", ready.user.name);
+    }
+
+    // For instrument to work, all parameters must implement Debug.
+    //
+    // Handler doesn't implement Debug here, so we specify to skip that argument.
+    // Context doesn't implement Debug either, so it is also skipped.
+    #[instrument(skip(self, _ctx))]
+    async fn resume(&self, _ctx: &Context, _resume: &ResumedEvent) {
+        // Log at the DEBUG level.
+        //
+        // In this example, this will not show up in the logs because DEBUG is
+        // below INFO, which is the set debug level.
+        debug!("Resumed");
+    }
+}
+
+#[tokio::main]
+#[instrument]
+async fn main() {
+    // Call tracing_subscriber's initialize function, which configures `tracing` via environment
+    // variables.
+    //
+    // For example, you can say to log all levels INFO and up via setting the environment variable
+    // `RUST_LOG` to `INFO`.
+    //
+    // This environment variable is already preset if you use cargo-make to run the example.
+    tracing_subscriber::fmt::init();
+
+    // Configure the client with your Discord bot token in the environment.
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client =
+        Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        error!("Client error: {:?}", why);
+    }
+}
