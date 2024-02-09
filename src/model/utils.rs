ebb3156d55482e217b629ca35e980c97f83aecc7
GnomedDev <david2005thomas@gmail.com>
2024-02-09 15:58:03 +0000

    Fix breakage caused by rebase

--- src/model/utils.rs
+++ src/model/utils.rs
@@ -336,6 +336,7 @@ pub mod stickers {
 pub mod comma_separated_string {
     use serde::{Deserialize, Deserializer, Serializer};
 
+    use super::CowStr;
     use crate::internal::prelude::*;
 
     pub fn deserialize<'de, D: Deserializer<'de>>(
