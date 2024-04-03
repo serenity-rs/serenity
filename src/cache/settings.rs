16aa8082d03d317accf83cf88e046ba98fe0d5a7
Gnome! <david2005thomas@Gmail.com>
2024-04-03 06:08:02 +0100

    Only include `cache::Settings::time_to_live` if temp cache is enabled (#2833)

--- src/cache/settings.rs
+++ src/cache/settings.rs
@@ -1,5 +1,3 @@
-use std::time::Duration;
-
 /// Settings for the cache.
 ///
 /// # Examples
@@ -16,14 +14,15 @@ use std::time::Duration;
 #[derive(Clone, Debug)]
 #[non_exhaustive]
 pub struct Settings {
+    /// How long temporarily-cached data should be stored before being thrown out.
+    ///
+    /// Defaults to one hour.
+    #[cfg(feature = "temp_cache")]
+    pub time_to_live: std::time::Duration,
     /// The maximum number of messages to store in a channel's message cache.
     ///
     /// Defaults to 0.
     pub max_messages: usize,
-    /// How long temporarily-cached data should be stored before being thrown out.
-    ///
-    /// Defaults to one hour.
-    pub time_to_live: Duration,
     /// Whether to cache guild data received from gateway.
     ///
     /// Defaults to true.
@@ -41,8 +40,9 @@ pub struct Settings {
 impl Default for Settings {
     fn default() -> Self {
         Self {
+            #[cfg(feature = "temp_cache")]
+            time_to_live: std::time::Duration::from_secs(60 * 60),
             max_messages: 0,
-            time_to_live: Duration::from_secs(60 * 60),
             cache_guilds: true,
             cache_channels: true,
             cache_users: true,
