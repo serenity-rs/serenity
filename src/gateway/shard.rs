33412034e3c63bb34eee4df0775db06065e93470
Gnome! <david2005thomas@Gmail.com>
2024-04-06 01:40:08 +0100

    Add resume_gateway_url support (#2832)
    This announcement was missed (around August 2022) and probably the cause
    of a lot of the disconnections.

--- src/gateway/shard.rs
+++ src/gateway/shard.rs
@@ -76,6 +76,7 @@ pub struct Shard {
     pub started: Instant,
     token: Secret<Token>,
     ws_url: Arc<str>,
+    resume_ws_url: Option<FixedString>,
     pub intents: GatewayIntents,
 }
 
@@ -154,6 +155,7 @@ impl Shard {
             session_id,
             info,
             ws_url,
+            resume_ws_url: None,
             intents,
         })
     }
@@ -284,6 +286,7 @@ impl Shard {
             Event::Ready(ready) => {
                 debug!("[{:?}] Received Ready", self.info);
 
+                self.resume_ws_url = Some(ready.ready.resume_gateway_url.clone());
                 self.session_id = Some(ready.ready.session_id.clone());
                 self.stage = ConnectionStage::Connected;
 
@@ -679,15 +682,19 @@ impl Shard {
         Ok(())
     }
 
-    /// Initializes a new WebSocket client.
+    /// Reinitializes an existing WebSocket client, replacing it.
     ///
     /// This will set the stage of the shard before and after instantiation of the client.
+    ///
     /// # Errors
     ///
     /// Errors if unable to establish a websocket connection.
     #[cfg_attr(feature = "tracing_instrument", instrument(skip(self)))]
-    pub async fn initialize(&mut self) -> Result<WsClient> {
+    pub async fn reinitialize(&mut self) -> Result<WsClient> {
         debug!("[{:?}] Initializing.", self.info);
+
+        // Reconnect to the resume URL if possible, otherwise use the generic URL.
+        let ws_url = self.resume_ws_url.as_deref().unwrap_or(&self.ws_url);
 
         // We need to do two, sort of three things here:
         // - set the stage of the shard as opening the websocket connection
@@ -698,7 +705,7 @@ impl Shard {
         // Hello is received.
         self.stage = ConnectionStage::Connecting;
         self.started = Instant::now();
-        let client = connect(&self.ws_url).await?;
+        let client = connect(ws_url).await?;
         self.stage = ConnectionStage::Handshake;
 
         Ok(client)
@@ -722,7 +729,7 @@ impl Shard {
     pub async fn resume(&mut self) -> Result<()> {
         debug!("[{:?}] Attempting to resume", self.info);
 
-        self.client = self.initialize().await?;
+        self.client = self.reinitialize().await?;
         self.stage = ConnectionStage::Resuming;
 
         match &self.session_id {
@@ -743,7 +750,7 @@ impl Shard {
         info!("[{:?}] Attempting to reconnect", self.shard_info());
 
         self.reset();
-        self.client = self.initialize().await?;
+        self.client = self.reinitialize().await?;
 
         Ok(())
     }
