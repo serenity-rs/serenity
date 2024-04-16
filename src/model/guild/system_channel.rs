bc5de51b544911d7284717880b4be4103c7fbae6
bendn <bend.n@outlook.com>
2024-04-16 14:32:20 +0700

    system flags

--- src/model/guild/system_channel.rs
+++ src/model/guild/system_channel.rs
@@ -1,21 +1,52 @@
-bitflags! {
-    /// Describes a system channel flags.
-    ///
-    /// [Discord docs](https://discord.com/developers/docs/resources/guild#guild-object-system-channel-flags).
-    #[cfg_attr(feature = "typesize", derive(typesize::derive::TypeSize))]
-    #[derive(Copy, Clone, Default, Debug, Eq, Hash, PartialEq)]
-    pub struct SystemChannelFlags: u64 {
-        /// Suppress member join notifications.
-        const SUPPRESS_JOIN_NOTIFICATIONS = 1 << 0;
-        /// Suppress server boost notifications.
-        const SUPPRESS_PREMIUM_SUBSCRIPTIONS = 1 << 1;
-        /// Suppress server setup tips.
-        const SUPPRESS_GUILD_REMINDER_NOTIFICATIONS = 1 << 2;
-        /// Hide member join sticker reply buttons.
-        const SUPPRESS_JOIN_NOTIFICATION_REPLIES = 1 << 3;
-        /// Suppress role subscription purchase and renewal notifications.
-        const SUPPRESS_ROLE_SUBSCRIPTION_PURCHASE_NOTIFICATIONS = 1 << 4;
-        /// Hide role subscription sticker reply buttons.
-        const SUPPRESS_ROLE_SUBSCRIPTION_PURCHASE_NOTIFICATION_REPLIES = 1 << 5;
-    }
+macro_rules! system_channel {
+    ($(#[doc = $doc:literal] $x:ident = $y:literal: $name:literal)+) => {
+        /// Describes a system channel flags.
+        ///
+        /// [Discord docs](https://discord.com/developers/docs/resources/guild#guild-object-system-channel-flags).
+        #[cfg_attr(feature = "typesize", derive(typesize::derive::TypeSize))]
+        #[derive(Copy, Clone, serde::Serialize, serde::Deserialize, Default, Debug, Eq, Hash, PartialEq)]
+        pub struct SystemChannelFlags(u64);
+
+        bitflags::bitflags! {
+            impl SystemChannelFlags: u64 {
+                $(
+                    #[doc = $doc]
+                    const $x = 1 << $y;
+                )+
+            }
+        }
+
+        impl SystemChannelFlags {
+            /// Returns a list of names of all contained system channel settings.
+            #[must_use]
+            pub fn names(self) -> Vec<&'static str> {
+                let mut names = Vec::with_capacity(self.0.count_ones() as _);
+
+                $(
+                    if self.contains(SystemChannelFlags::$x) {
+                        names.push($name);
+                    }
+                )*
+
+                names
+            }
+        }
+    };
+}
+
+// why are your flags all negative anyways
+// wish i could add a custom ser/de impl that applys `!`
+system_channel! {
+    /// Suppress member join notifications
+    SUPPRESS_JOIN_NOTIFICATIONS = 0: "suppress welcome messages"
+    /// Suppress server boost notifications
+    SUPPRESS_PREMIUM_SUBSCRIPTIONS = 1: "suppress boost notifications"
+    /// Suppress server setup tips
+    SUPPRESS_GUILD_REMINDER_NOTIFICATIONS = 2: "suppress server setup tips"
+    /// Hide member join sticker reply buttons
+    SUPPRESS_JOIN_NOTIFICATION_REPLYS = 3: "suppress welcome stickers"
+    /// Suppress role subscription purchase and renewal notifications
+    SUPPRESS_ROLE_SUBSCRIPTION_PURCHASE_NOTIFICATIONS = 4: "suppress subscription messages" // ive guessed these, the others come from discord's own audit log (though i added suppress)
+    /// Hide role subscription sticker reply buttons
+    SUPPRESS_ROLE_SUBSCRIPTION_PURCHASE_NOTIFICATION_REPLIES = 5: "suppress subscription stickers"
 }
