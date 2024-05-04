21f7bf219c73f0fb44e90ae34678f079f916abd2
Sanandan Sashikumar <cheesycod@outlook.com>
2024-05-04 13:25:54 +0000

    Add greater_member_hierarchy_in to PartialGuild

--- src/model/guild/partial_guild.rs
+++ src/model/guild/partial_guild.rs
@@ -1338,6 +1338,89 @@ impl PartialGuild {
     pub async fn get_active_threads(&self, http: &Http) -> Result<ThreadsData> {
         self.id.get_active_threads(http).await
     }
+
+    /// Gets the highest role a [`Member`] of this Guild has.
+    ///
+    /// Returns None if the member has no roles or the member from this guild.
+    pub fn member_highest_role_in<'a>(pg: &'a PartialGuild, member: &Member) -> Option<&'a Role> {
+        let mut highest: Option<&Role> = None;
+
+        for role_id in &member.roles {
+            if let Some(role) = pg.roles.get(role_id) {
+                // Skip this role if this role in iteration has:
+                // - a position less than the recorded highest
+                // - a position equal to the recorded, but a higher ID
+                if let Some(highest) = highest {
+                    if role.position < highest.position
+                        || (role.position == highest.position && role.id > highest.id)
+                    {
+                        continue;
+                    }
+                }
+
+                highest = Some(role);
+            }
+        }
+
+        highest
+    }
+
+    /// Returns which of two [`User`]s has a higher [`Member`] hierarchy.
+    ///
+    /// Hierarchy is essentially who has the [`Role`] with the highest [`position`].
+    ///
+    /// Returns [`None`] if at least one of the given users' member instances is not present.
+    /// Returns [`None`] if the users have the same hierarchy, as neither are greater than the
+    /// other.
+    ///
+    /// If both user IDs are the same, [`None`] is returned. If one of the users is the guild
+    /// owner, their ID is returned.
+    ///
+    /// [`position`]: Role::position
+    pub fn greater_member_hierarchy_in(pg: &PartialGuild, lhs: &Member, rhs: &Member) -> Option<UserId> {
+        // Check that the IDs are the same. If they are, neither is greater.
+        if lhs.user.id == rhs.user.id {
+            return None;
+        }
+
+        // Check if either user is the guild owner.
+        if lhs.user.id == pg.owner_id {
+            return Some(lhs.user.id);
+        } else if rhs.user.id == pg.owner_id {
+            return Some(rhs.user.id);
+        }
+
+        let lhs_role = Self::member_highest_role_in(pg, lhs)
+            .map_or((RoleId::new(1), 0), |r| (r.id, r.position));
+
+        let rhs_role = Self::member_highest_role_in(pg, rhs)
+            .map_or((RoleId::new(1), 0), |r| (r.id, r.position));
+
+        // If LHS and RHS both have no top position or have the same role ID, then no one wins.
+        if (lhs_role.1 == 0 && rhs_role.1 == 0) || (lhs_role.0 == rhs_role.0) {
+            return None;
+        }
+
+        // If LHS's top position is higher than RHS, then LHS wins.
+        if lhs_role.1 > rhs_role.1 {
+            return Some(lhs.user.id);
+        }
+
+        // If RHS's top position is higher than LHS, then RHS wins.
+        if rhs_role.1 > lhs_role.1 {
+            return Some(rhs.user.id);
+        }
+
+        // If LHS and RHS both have the same position, but LHS has the lower role ID, then LHS
+        // wins.
+        //
+        // If RHS has the higher role ID, then RHS wins.
+        if lhs_role.1 == rhs_role.1 && lhs_role.0 < rhs_role.0 {
+            Some(lhs.user.id)
+        } else {
+            Some(rhs.user.id)
+        }
+    }
 }
 
 // Manual impl needed to insert guild_id into Role's
