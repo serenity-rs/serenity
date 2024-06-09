9ad74d41209380591a3458afbc9e21ba1cec2132
GnomedDev <david2005thomas@gmail.com>
2024-06-09 22:13:32 +0100

    Revert the addition of `CreateEmbed::into_inner` (#2888)
    This reverts commit 8c84b4f967f6fee4305202301e1925a8903cce7e as it is not easily possible on the next branch.

--- src/builder/create_embed.rs
+++ src/builder/create_embed.rs
@@ -198,13 +198,6 @@ impl CreateEmbed {
         super::check_overflow(length, crate::constants::EMBED_MAX_LENGTH)
             .map_err(|overflow| Error::Model(ModelError::EmbedTooLarge(overflow)))
     }
-
-    /// Consumes the [`CreateEmbed`], returning the wrapped [`Embed`].
-    #[inline]
-    #[must_use]
-    pub fn into_inner(self) -> Embed {
-        self.0
-    }
 }
 
 impl Default for CreateEmbed {
