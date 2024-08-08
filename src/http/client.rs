a7ca62a894aeda6e445b6641af02ee01026cd5b6
Valentine Briese <valentinegb@icloud.com>
2024-08-08 09:27:34 -0700

    Add `OAuth2Token`, `OAuth2TokenRevocation`, and `OAuth2AuthorizationCurrent` to `Route` (#2935)
    This adds routes to `Route` that are related to OAuth2 and token exchanges.
    
    This is a step towards resolving #762 and #2475.

--- src/http/client.rs
+++ src/http/client.rs
@@ -2995,7 +2995,7 @@ impl Http {
             multipart: None,
             headers: None,
             method: LightMethod::Get,
-            route: Route::Oauth2ApplicationCurrent,
+            route: Route::OAuth2ApplicationCurrent,
             params: None,
         })
         .await
