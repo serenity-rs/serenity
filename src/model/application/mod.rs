63cf4067aae010c06879400f55d8b59b09185189
valgul <valgulnecron@gmail.com>
2024-08-16 02:39:34 +0200

    Added approximate_guild_count and approximate_user_install_count
    Added approximate_guild_count and approximate_user_install_count to CurrentApplicationInfo https://discord.com/developers/docs/resources/application#application-object-application-structure

--- src/model/application/mod.rs
+++ src/model/application/mod.rs
@@ -80,6 +80,8 @@ pub struct CurrentApplicationInfo {
     #[serde(default)]
     pub integration_types_config:
         std::collections::HashMap<InstallationContext, InstallationContextConfig>,
+    pub approximate_guild_count: Option<u64>,
+    pub approximate_user_install_count: Option<u64>,
 }
 
 impl CurrentApplicationInfo {
