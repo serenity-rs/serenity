133aaed95ca98cfd9ff2dd110782c2af22322761
GnomedDev <GnomedDev@users.noreply.github.com>
2024-10-21 16:06:46 +0000

    deploy: 5b6a1d4344804460a6c39140c2fa62aa10507bbf

---
+++ next/trait.impl/zeroize/trait.Zeroize.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["serenity",[["impl Zeroize for <a class=\"struct\" href=\"serenity/secret_string/struct.SecretString.html\" title=\"struct serenity::secret_string::SecretString\">SecretString</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[184]}
