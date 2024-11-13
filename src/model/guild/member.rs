338e24ab9c8b7f5560a3fc00ad070df60a1dcde4
Michael Krasnitski <42564254+mkrasnitski@users.noreply.github.com>
2024-11-13 01:42:49 -0800

    Improve some docs (#3027)
    Improved wording and removed some sentences that are no longer relevant.
    These docs are mostly the ones that were touched by #2988.

--- src/model/guild/member.rs
+++ src/model/guild/member.rs
@@ -593,8 +593,10 @@ pub struct PartialThreadMember {
 
 /// A model representing a user in a Guild Thread.
 ///
-/// [Discord docs](https://docs.discord.com/developers/resources/channel#thread-member-object),
-/// [extra fields](https://docs.discord.com/developers/events/gateway-events#thread-member-update-thread-member-update-event-extra-fields).
+/// [Discord docs], [extra fields].
+///
+/// [Discord docs]: https://docs.discord.com/developers/resources/channel#thread-member-object
+/// [extra fields]: https://docs.discord.com/developers/events/gateway-events#thread-member-update-thread-member-update-event-extra-fields
 #[cfg_attr(feature = "typesize", derive(typesize::derive::TypeSize))]
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[non_exhaustive]
