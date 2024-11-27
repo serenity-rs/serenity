92ae0f728172cf3d27de8df7aa3fca0238430d62
Michael Krasnitski <42564254+mkrasnitski@users.noreply.github.com>
2024-11-27 13:53:32 -0800

    Factor out collector methods on models into traits (#3055)
    Moves the feature-gated collector methods on `UserId`, `MessageId`, etc.
    into four traits:
    - `CollectMessages`
    - `CollectReactions`
    - `CollectModalInteractions`
    - `CollectComponentInteractions`
    
    This also moves the quick modal machinery into the collector module and defines
    a `QuickModal` trait.
    
    This fully removes any collector feature gates from the model types.

--- examples/e05_sample_bot_structure/src/commands/modal.rs
+++ examples/e05_sample_bot_structure/src/commands/modal.rs
@@ -1,7 +1,7 @@
 use serenity::builder::*;
+use serenity::collector::{CreateQuickModal, QuickModal};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
-use serenity::utils::CreateQuickModal;
 
 pub async fn run(ctx: &Context, interaction: &CommandInteraction) -> Result<(), serenity::Error> {
     let modal = CreateQuickModal::new("About you")
--- src/model/application/command_interaction.rs
+++ src/model/application/command_interaction.rs
@@ -12,14 +12,10 @@ use crate::builder::{
     CreateInteractionResponseMessage,
     EditInteractionResponse,
 };
-#[cfg(feature = "collector")]
-use crate::gateway::client::Context;
 #[cfg(feature = "model")]
 use crate::http::Http;
 use crate::internal::utils::lending_for_each;
 use crate::model::prelude::*;
-#[cfg(all(feature = "collector", feature = "utils"))]
-use crate::utils::{CreateQuickModal, QuickModalResponse};
 
 /// An interaction when a user invokes a slash command.
 ///
@@ -203,20 +199,6 @@ impl CommandInteraction {
             CreateInteractionResponseMessage::new().ephemeral(true),
         );
         self.create_response(http, builder).await
-    }
-
-    /// See [`CreateQuickModal`].
-    ///
-    /// # Errors
-    ///
-    /// See [`CreateQuickModal::execute()`].
-    #[cfg(all(feature = "collector", feature = "utils"))]
-    pub async fn quick_modal(
-        &self,
-        ctx: &Context,
-        builder: CreateQuickModal<'_>,
-    ) -> Result<Option<QuickModalResponse>> {
-        builder.execute(ctx, self.id, &self.token).await
     }
 }
 
