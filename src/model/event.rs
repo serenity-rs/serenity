9758a6c68376deb3658212ba1cc8b67cce06e803
jamesbt365 <jamesbt365@gmail.com>
2024-12-19 19:06:36 +0000

    Add `newly_created` bool field to the `ThreadCreate` event (#3084)
    https://discord.com/developers/docs/events/gateway-events#thread-create

--- src/model/event.rs
+++ src/model/event.rs
@@ -862,10 +862,11 @@ pub struct StageInstanceDeleteEvent {
 /// [Discord docs](https://discord.com/developers/docs/topics/gateway-events#thread-create).
 #[cfg_attr(feature = "typesize", derive(typesize::derive::TypeSize))]
 #[derive(Clone, Debug, Deserialize, Serialize)]
-#[serde(transparent)]
 #[non_exhaustive]
 pub struct ThreadCreateEvent {
+    #[serde(flatten)]
     pub thread: GuildChannel,
+    pub newly_created: Option<bool>,
 }
 
 /// Requires [`GatewayIntents::GUILDS`].
