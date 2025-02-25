7e25a4cefde21376c88b3be17b975bd6162dce8d
Michael Krasnitski <42564254+mkrasnitski@users.noreply.github.com>
2025-02-25 14:02:40 -0500

    Bump MSRV to 1.85 and switch to the 2024 edition (#3126)
    Running `cargo fix --edition` produces a lot of noise, and the relative
    drop order change shouldn't affect us because we don't use any unsafe
    code. What I did was manually change the edition and then just fix the
    compiler errors.

--- src/http/typing.rs
+++ src/http/typing.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 
 use tokio::sync::oneshot::error::TryRecvError;
 use tokio::sync::oneshot::{self, Sender};
-use tokio::time::{sleep, Duration};
+use tokio::time::{Duration, sleep};
 
 use crate::http::Http;
 use crate::internal::prelude::*;
--- voice-model/benches/de.rs
+++ voice-model/benches/de.rs
@@ -1,4 +1,4 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{Criterion, black_box, criterion_group, criterion_main};
 use serenity_voice_model::Event;
 
 pub fn json_deser(c: &mut Criterion) {
