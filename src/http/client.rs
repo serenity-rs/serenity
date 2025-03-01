9f9b2ad8420fd35af9bcf31dc0fc0aa0f620d7e4
Gnome! <daisy2005thomas@gmail.com>
2025-03-01 22:59:30 +0000

    Remove implicit invite parsing in `Invite::get` (#3118)
    This was undocumented and done twice. Let's just get rid of it as users had to
    use the `serenity::util` function if they were following the documentation
    anyway.

--- src/http/client.rs
+++ src/http/client.rs
@@ -3799,9 +3799,6 @@ impl Http {
         event_id: Option<ScheduledEventId>,
     ) -> Result<Invite> {
         let (member_counts_str, expiration_str, event_id_str);
-        #[cfg(feature = "utils")]
-        let code = crate::utils::parse_invite(code);
-
         let mut params = ArrayVec::<_, 3>::new();
 
         member_counts_str = member_counts.to_arraystring();
