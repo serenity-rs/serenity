c0eeca99ee17a4175f69d2dd9ebec9e7d5dd706b
jamesbt365 <jamesbt365@gmail.com>
2025-04-16 13:10:40 +0100

    clippy warnings

--- src/model/guild/mod.rs
+++ src/model/guild/mod.rs
@@ -666,7 +666,6 @@ impl Guild {
     /// [`Shard`]: crate::gateway::Shard
     /// [whitelist]: https://discord.com/developers/docs/resources/guild#create-guild
     #[deprecated = "This endpoint has been deprecated by Discord and will stop functioning after July 15, 2025. For more information, see: https://discord.com/developers/docs/change-log#deprecating-guild-creation-by-apps"]
-
     pub async fn create(
         http: impl AsRef<Http>,
         name: &str,
@@ -677,6 +676,7 @@ impl Guild {
             "name": name,
         });
 
+        #[allow(deprecated)]
         http.as_ref().create_guild(&map).await
     }
 
