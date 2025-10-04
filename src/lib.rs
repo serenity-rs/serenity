aa58fe3d1e901bceefe1b3109ea26ec34d98c4b2
Michael Krasnitski <michael.krasnitski@gmail.com>
2025-10-04 15:05:05 -0400

    Change from `doc_auto_cfg` to `doc_cfg` feature

--- src/lib.rs
+++ src/lib.rs
@@ -49,7 +49,7 @@
 //! [examples]: https://github.com/serenity-rs/serenity/tree/current/examples
 //! [gateway docs]: crate::gateway
 #![doc(html_root_url = "https://docs.rs/serenity/*")]
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 #![warn(
     unused,
     rust_2018_idioms,
