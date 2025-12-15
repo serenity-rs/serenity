23d4f12a2d3560bf603ca7ff1d44684aefe90dd8
Michael Krasnitski <42564254+mkrasnitski@users.noreply.github.com>
2025-12-15 12:15:32 -0500

    Refactor `FullEvent` cache update function (#3457)
    Separate out virtual events from the rest of the full event logic to make it
    clear which virtual events are possible and when.
    
    Also get rid of `&mut self` in CacheUpdate as raw events do not ever need to
    carry any mutable state.

--- src/gateway/client/dispatch.rs
+++ src/gateway/client/dispatch.rs
@@ -3,13 +3,12 @@ use std::sync::Arc;
 use super::event_handler::{EventHandler, RawEventHandler};
 use super::{Context, FullEvent};
 #[cfg(feature = "cache")]
-use crate::cache::{Cache, CacheUpdate};
+use crate::cache::CacheUpdate;
 #[cfg(feature = "framework")]
 use crate::framework::Framework;
 use crate::internal::tokio::spawn_named;
 use crate::model::channel::ChannelType;
 use crate::model::event::Event;
-use crate::model::guild::Member;
 
 #[cfg(feature = "cache")]
 macro_rules! if_cache {
@@ -27,19 +26,19 @@ macro_rules! if_cache {
 
 #[cfg(feature = "cache")]
 macro_rules! update_cache {
-    ($cache:ident, $event:ident) => {
+    ($cache:expr, $event:ident) => {
         $event.update($cache)
     };
 }
 
 #[cfg(not(feature = "cache"))]
 macro_rules! update_cache {
-    ($cache:ident, $event:ident) => {};
+    ($cache:expr, $event:ident) => {};
 }
 
 /// Calls the user's event handlers and the framework handler.
 pub(crate) async fn dispatch_model(
-    event: Box<Event>,
+    event: Event,
     context: Context,
     #[cfg(feature = "framework")] framework: Option<Arc<dyn Framework>>,
     event_handler: Option<Arc<dyn EventHandler>>,
@@ -49,11 +48,8 @@ pub(crate) async fn dispatch_model(
         raw_handler.raw_event(context.clone(), &event).await;
     }
 
-    let (full_event, extra_event) = update_cache_with_event(
-        #[cfg(feature = "cache")]
-        &context.cache,
-        event,
-    );
+    let extra_event = get_virtual_event(&context, &event);
+    let full_event = update_cache_with_event(&context, event);
 
     spawn_named("dispatch::user", async move {
         #[cfg(feature = "framework")]
@@ -99,18 +95,8 @@ async fn dispatch_event_handler(
 }
 
 /// Updates the cache with the incoming event data and builds the full event data out of it.
-///
-/// Can return a secondary [`FullEvent`] for "virtual" events like [`FullEvent::CacheReady`] or
-/// [`FullEvent::ShardsReady`]. Secondary events are traditionally dispatched first.
-///
-/// Can return `None` if an event is unknown.
-#[cfg_attr(not(feature = "cache"), allow(unused_mut))]
-fn update_cache_with_event(
-    #[cfg(feature = "cache")] cache: &Cache,
-    event: Box<Event>,
-) -> (FullEvent, Option<FullEvent>) {
-    let mut extra_event = None;
-    let event = match *event {
+fn update_cache_with_event(ctx: &Context, event: Event) -> FullEvent {
+    match event {
         Event::CommandPermissionsUpdate(event) => FullEvent::CommandPermissionsUpdate {
             permission: event.permission,
         },
@@ -126,8 +112,8 @@ fn update_cache_with_event(
         Event::AutoModActionExecution(event) => FullEvent::AutoModActionExecution {
             execution: event.execution,
         },
-        Event::ChannelCreate(mut event) => {
-            update_cache!(cache, event);
+        Event::ChannelCreate(event) => {
+            update_cache!(&ctx.cache, event);
 
             let channel = event.channel;
             if channel.base.kind == ChannelType::Category {
@@ -140,8 +126,8 @@ fn update_cache_with_event(
                 }
             }
         },
-        Event::ChannelDelete(mut event) => {
-            let cached_messages = if_cache!(update_cache!(cache, event));
+        Event::ChannelDelete(event) => {
+            let cached_messages = if_cache!(update_cache!(&ctx.cache, event));
 
             let channel = event.channel;
             if channel.base.kind == ChannelType::Category {
@@ -158,8 +144,8 @@ fn update_cache_with_event(
         Event::ChannelPinsUpdate(event) => FullEvent::ChannelPinsUpdate {
             pin: event,
         },
-        Event::ChannelUpdate(mut event) => {
-            let old_channel = if_cache!(update_cache!(cache, event));
+        Event::ChannelUpdate(event) => {
+            let old_channel = if_cache!(update_cache!(&ctx.cache, event));
 
             FullEvent::ChannelUpdate {
                 old: old_channel,
@@ -178,31 +164,25 @@ fn update_cache_with_event(
             guild_id: event.guild_id,
             unbanned_user: event.user,
         },
-        Event::GuildCreate(mut event) => {
-            let is_new = if_cache!(Some(!cache.unavailable_guilds().contains(&event.guild.id)));
-
-            #[cfg(feature = "cache")]
-            if let Some(guilds) = update_cache!(cache, event) {
-                extra_event = Some(FullEvent::CacheReady {
-                    guilds,
-                });
-            }
+        Event::GuildCreate(event) => {
+            let is_new =
+                if_cache!(Some(!&ctx.cache.unavailable_guilds().contains(&event.guild.id)));
 
             FullEvent::GuildCreate {
                 guild: event.guild,
                 is_new,
             }
         },
-        Event::GuildDelete(mut event) => {
-            let full = if_cache!(update_cache!(cache, event));
+        Event::GuildDelete(event) => {
+            let full = if_cache!(update_cache!(&ctx.cache, event));
 
             FullEvent::GuildDelete {
                 incomplete: event.guild,
                 full,
             }
         },
-        Event::GuildEmojisUpdate(mut event) => {
-            update_cache!(cache, event);
+        Event::GuildEmojisUpdate(event) => {
+            update_cache!(&ctx.cache, event);
 
             FullEvent::GuildEmojisUpdate {
                 guild_id: event.guild_id,
@@ -212,15 +192,15 @@ fn update_cache_with_event(
         Event::GuildIntegrationsUpdate(event) => FullEvent::GuildIntegrationsUpdate {
             guild_id: event.guild_id,
         },
-        Event::GuildMemberAdd(mut event) => {
-            update_cache!(cache, event);
+        Event::GuildMemberAdd(event) => {
+            update_cache!(&ctx.cache, event);
 
             FullEvent::GuildMemberAddition {
                 new_member: event.member,
             }
         },
-        Event::GuildMemberRemove(mut event) => {
-            let member = if_cache!(update_cache!(cache, event));
+        Event::GuildMemberRemove(event) => {
+            let member = if_cache!(update_cache!(&ctx.cache, event));
 
             FullEvent::GuildMemberRemoval {
                 guild_id: event.guild_id,
@@ -228,12 +208,13 @@ fn update_cache_with_event(
                 member_data_if_available: member,
             }
         },
-        Event::GuildMemberUpdate(mut event) => {
-            let before = if_cache!(update_cache!(cache, event));
-            let after: Option<Member> = if_cache!({
-                let guild = cache.guild(event.guild_id);
-                guild.and_then(|g| g.members.get(&event.user.id).cloned())
-            });
+        Event::GuildMemberUpdate(event) => {
+            let before = if_cache!(update_cache!(&ctx.cache, event));
+            let after = if_cache!(
+                ctx.cache
+                    .guild(event.guild_id)
+                    .and_then(|g| g.members.get(&event.user.id).cloned())
+            );
 
             FullEvent::GuildMemberUpdate {
                 old_if_available: before,
@@ -241,22 +222,22 @@ fn update_cache_with_event(
                 event,
             }
         },
-        Event::GuildMembersChunk(mut event) => {
-            update_cache!(cache, event);
+        Event::GuildMembersChunk(event) => {
+            update_cache!(&ctx.cache, event);
 
             FullEvent::GuildMembersChunk {
                 chunk: event,
             }
         },
-        Event::GuildRoleCreate(mut event) => {
-            update_cache!(cache, event);
+        Event::GuildRoleCreate(event) => {
+            update_cache!(&ctx.cache, event);
 
             FullEvent::GuildRoleCreate {
                 new: event.role,
             }
         },
-        Event::GuildRoleDelete(mut event) => {
-            let role = if_cache!(update_cache!(cache, event));
+        Event::GuildRoleDelete(event) => {
+            let role = if_cache!(update_cache!(&ctx.cache, event));
 
             FullEvent::GuildRoleDelete {
                 guild_id: event.guild_id,
@@ -264,16 +245,16 @@ fn update_cache_with_event(
                 removed_role_data_if_available: role,
             }
         },
-        Event::GuildRoleUpdate(mut event) => {
-            let before = if_cache!(update_cache!(cache, event));
+        Event::GuildRoleUpdate(event) => {
+            let before = if_cache!(update_cache!(&ctx.cache, event));
 
             FullEvent::GuildRoleUpdate {
                 old_data_if_available: before,
                 new: event.role,
             }
         },
-        Event::GuildStickersUpdate(mut event) => {
-            update_cache!(cache, event);
+        Event::GuildStickersUpdate(event) => {
+            update_cache!(&ctx.cache, event);
 
             FullEvent::GuildStickersUpdate {
                 guild_id: event.guild_id,
@@ -281,7 +262,7 @@ fn update_cache_with_event(
             }
         },
         Event::GuildUpdate(event) => {
-            let before = if_cache!(cache.guild(event.guild.id).map(|g| g.clone()));
+            let before = if_cache!(ctx.cache.guild(event.guild.id).map(|g| g.clone()));
 
             FullEvent::GuildUpdate {
                 old_data_if_available: before,
@@ -294,8 +275,8 @@ fn update_cache_with_event(
         Event::InviteDelete(event) => FullEvent::InviteDelete {
             data: event,
         },
-        Event::MessageCreate(mut event) => {
-            update_cache!(cache, event);
+        Event::MessageCreate(event) => {
+            update_cache!(&ctx.cache, event);
 
             FullEvent::Message {
                 new_message: event.message,
@@ -311,16 +292,16 @@ fn update_cache_with_event(
             deleted_message_id: event.message_id,
             guild_id: event.guild_id,
         },
-        Event::MessageUpdate(mut event) => {
-            let before = if_cache!(update_cache!(cache, event));
+        Event::MessageUpdate(event) => {
+            let before = if_cache!(update_cache!(&ctx.cache, event));
 
             FullEvent::MessageUpdate {
                 old_if_available: before,
                 event,
             }
         },
-        Event::PresenceUpdate(mut event) => {
-            let old_data = if_cache!(update_cache!(cache, event));
+        Event::PresenceUpdate(event) => {
+            let old_data = if_cache!(update_cache!(&ctx.cache, event));
 
             FullEvent::PresenceUpdate {
                 old_data,
@@ -341,17 +322,8 @@ fn update_cache_with_event(
         Event::ReactionRemoveEmoji(event) => FullEvent::ReactionRemoveEmoji {
             removed_reactions: event.reaction,
         },
-        Event::Ready(mut event) => {
-            #[cfg(feature = "cache")]
-            if let Some(total_shards) = update_cache!(cache, event) {
-                extra_event = Some(FullEvent::ShardsReady {
-                    total_shards,
-                });
-            }
-
-            FullEvent::Ready {
-                data_about_bot: event.ready,
-            }
+        Event::Ready(event) => FullEvent::Ready {
+            data_about_bot: event.ready,
         },
         Event::Resumed(event) => FullEvent::Resume {
             event,
@@ -374,8 +346,8 @@ fn update_cache_with_event(
         Event::TypingStart(event) => FullEvent::TypingStart {
             event,
         },
-        Event::UserUpdate(mut event) => {
-            let before = if_cache!(update_cache!(cache, event));
+        Event::UserUpdate(event) => {
+            let before = if_cache!(update_cache!(&ctx.cache, event));
 
             FullEvent::UserUpdate {
                 old_data: before,
@@ -385,16 +357,16 @@ fn update_cache_with_event(
         Event::VoiceServerUpdate(event) => FullEvent::VoiceServerUpdate {
             event,
         },
-        Event::VoiceStateUpdate(mut event) => {
-            let before = if_cache!(update_cache!(cache, event));
+        Event::VoiceStateUpdate(event) => {
+            let before = if_cache!(update_cache!(&ctx.cache, event));
 
             FullEvent::VoiceStateUpdate {
                 old: before,
                 new: event.voice_state,
             }
         },
-        Event::VoiceChannelStatusUpdate(mut event) => {
-            let old = if_cache!(event.update(cache).map(Into::into));
+        Event::VoiceChannelStatusUpdate(event) => {
+            let old = if_cache!(event.update(&ctx.cache).map(Into::into));
 
             FullEvent::VoiceChannelStatusUpdate {
                 old,
@@ -431,32 +403,32 @@ fn update_cache_with_event(
         Event::StageInstanceDelete(event) => FullEvent::StageInstanceDelete {
             stage_instance: event.stage_instance,
         },
-        Event::ThreadCreate(mut event) => {
-            update_cache!(cache, event);
+        Event::ThreadCreate(event) => {
+            update_cache!(&ctx.cache, event);
 
             FullEvent::ThreadCreate {
                 thread: event.thread,
                 newly_created: event.newly_created,
             }
         },
-        Event::ThreadUpdate(mut event) => {
-            let old = if_cache!(update_cache!(cache, event));
+        Event::ThreadUpdate(event) => {
+            let old = if_cache!(update_cache!(&ctx.cache, event));
 
             FullEvent::ThreadUpdate {
                 old,
                 new: event.thread,
             }
         },
-        Event::ThreadDelete(mut event) => {
-            let full_thread_data = if_cache!(update_cache!(cache, event));
+        Event::ThreadDelete(event) => {
+            let full_thread_data = if_cache!(update_cache!(&ctx.cache, event));
 
             FullEvent::ThreadDelete {
                 thread: event.thread,
                 full_thread_data,
             }
         },
-        Event::ThreadListSync(mut event) => {
-            update_cache!(cache, event);
+        Event::ThreadListSync(event) => {
+            update_cache!(&ctx.cache, event);
 
             FullEvent::ThreadListSync {
                 thread_list_sync: event,
@@ -468,22 +440,22 @@ fn update_cache_with_event(
         Event::ThreadMembersUpdate(event) => FullEvent::ThreadMembersUpdate {
             thread_members_update: event,
         },
-        Event::GuildScheduledEventCreate(mut event) => {
-            update_cache!(cache, event);
+        Event::GuildScheduledEventCreate(event) => {
+            update_cache!(&ctx.cache, event);
 
             FullEvent::GuildScheduledEventCreate {
                 event: event.event,
             }
         },
-        Event::GuildScheduledEventUpdate(mut event) => {
-            update_cache!(cache, event);
+        Event::GuildScheduledEventUpdate(event) => {
+            update_cache!(&ctx.cache, event);
 
             FullEvent::GuildScheduledEventUpdate {
                 event: event.event,
             }
         },
-        Event::GuildScheduledEventDelete(mut event) => {
-            update_cache!(cache, event);
+        Event::GuildScheduledEventDelete(event) => {
+            update_cache!(&ctx.cache, event);
 
             FullEvent::GuildScheduledEventDelete {
                 event: event.event,
@@ -510,7 +482,30 @@ fn update_cache_with_event(
         Event::MessagePollVoteRemove(event) => FullEvent::MessagePollVoteRemove {
             event,
         },
-    };
+    }
+}
 
-    (event, extra_event)
+fn get_virtual_event(ctx: &Context, event: &Event) -> Option<FullEvent> {
+    match event {
+        Event::GuildCreate(event) =>
+        {
+            #[cfg(feature = "cache")]
+            if let Some(guilds) = update_cache!(&ctx.cache, event) {
+                return Some(FullEvent::CacheReady {
+                    guilds,
+                });
+            }
+        },
+        Event::Ready(event) =>
+        {
+            #[cfg(feature = "cache")]
+            if let Some(total_shards) = update_cache!(&ctx.cache, event) {
+                return Some(FullEvent::ShardsReady {
+                    total_shards,
+                });
+            }
+        },
+        _ => {},
+    }
+    None
 }
