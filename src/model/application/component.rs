46da08d663e6351ff2c91f40a217f9dcd5770a48
Meditation Mind <115960723+meditationmind@users.noreply.github.com>
2026-01-04 11:14:32 +0900

    Update SelectMenu struct

--- src/model/application/component.rs
+++ src/model/application/component.rs
@@ -650,6 +650,9 @@ pub struct SelectMenu {
     pub min_values: Option<u8>,
     /// The maximum number of selections allowed.
     pub max_values: Option<u8>,
+    /// Whether select menu is required to be filled in a modal. Default is true. Ignored in messages.
+    #[serde(default = "default_true")]
+    pub required: bool,
     /// Whether select menu is disabled.
     #[serde(default)]
     pub disabled: bool,
