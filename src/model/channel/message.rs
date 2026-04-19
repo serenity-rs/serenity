11c7d541f8022b3db126e9b21304da2fee1d7e84
emad <ea@ea.sa>
2026-04-19 04:36:35 +0300

    feat(message): add HAS_SNAPSHOT flag
    Discord sets this flag on forwarded messages that carry a snapshot of
    the referenced message. Bit 14 in the message flags bitfield.

--- src/model/channel/message.rs
+++ src/model/channel/message.rs
@@ -955,6 +955,8 @@ bitflags! {
         /// As of 2023-04-20, bots are currently not able to send voice messages
         /// ([source](https://github.com/discord/discord-api-docs/pull/6082)).
         const IS_VOICE_MESSAGE = 1 << 13;
+        /// This message has a snapshot (via Message Forwarding).
+        const HAS_SNAPSHOT = 1 << 14;
         /// Enables support for sending Components V2.
         ///
         /// Setting this flag is required to use V2 components.
